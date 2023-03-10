7eb2b4e53cc71423fe3d46341ee5dac9a56a2273
Srini Iyer <srini.iyer9@gmail.com>
2023-03-10 15:00:56 -0500

    E2314 - migrate ResponseMap class methods to Response class    - assessments_for    - reviewer_assessments_for

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,41 @@
 # frozen_string_literal: true
 
 class Response < ApplicationRecord
+
+  # return latest versions of the responses
+  def self.assessments_for(team)
+    responses = []
+    if team
+      @array_sort = []
+      @sort_to = []
+      maps = ResponseMap.where(reviewee_id: team.id)
+      maps.each do |map|
+        next if map.response.empty?
+
+        @all_resp = where(map_id: map.map_id).last
+        if map.type.eql?('ReviewResponseMap')
+          # If its ReviewResponseMap then only consider those response which are submitted.
+          @array_sort << @all_resp if @all_resp.is_submitted
+        else
+          @array_sort << @all_resp
+        end
+        # sort all versions in descending order and get the latest one.
+        # @sort_to=@array_sort.sort { |m1, m2| (m1.version_num and m2.version_num) ? m2.version_num <=> m1.version_num : (m1.version_num ? -1 : 1) }
+        @sort_to = @array_sort.sort # { |m1, m2| (m1.updated_at and m2.updated_at) ? m2.updated_at <=> m1.updated_at : (m1.version_num ? -1 : 1) }
+        responses << @sort_to[0] unless @sort_to[0].nil?
+        @array_sort.clear
+        @sort_to.clear
+      end
+      responses = responses.sort { |a, b| a.map.reviewer.fullname <=> b.map.reviewer.fullname }
+    end
+    responses
+  end
+
+  # return latest versions of the response given by reviewer
+  def self.reviewer_assessments_for(team, reviewer)
+    # get_reviewer may return an AssignmentParticipant or an AssignmentTeam
+    map = ResponseMap.where(reviewee_id: team.id, reviewer_id: reviewer.id)
+    where(map_id: map.first.id).sort { |m1, m2| comparator(m1, m2) }.first
+  end
+
 end
--- app/models/response_map.rb
+++ app/models/response_map.rb
@@ -8,35 +8,6 @@ class ResponseMap < ApplicationRecord
     id
   end
 
-  # return latest versions of the responses
-  def self.assessments_for(team)
-    responses = []
-    # stime = Time.now
-    if team
-      @array_sort = []
-      @sort_to = []
-      maps = where(reviewee_id: team.id)
-      maps.each do |map|
-        next if map.response.empty?
-
-        @all_resp = Response.where(map_id: map.map_id).last
-        if map.type.eql?('ReviewResponseMap')
-          # If its ReviewResponseMap then only consider those response which are submitted.
-          @array_sort << @all_resp if @all_resp.is_submitted
-        else
-          @array_sort << @all_resp
-        end
-        # sort all versions in descending order and get the latest one.
-        # @sort_to=@array_sort.sort { |m1, m2| (m1.version_num and m2.version_num) ? m2.version_num <=> m1.version_num : (m1.version_num ? -1 : 1) }
-        @sort_to = @array_sort.sort # { |m1, m2| (m1.updated_at and m2.updated_at) ? m2.updated_at <=> m1.updated_at : (m1.version_num ? -1 : 1) }
-        responses << @sort_to[0] unless @sort_to[0].nil?
-        @array_sort.clear
-        @sort_to.clear
-      end
-      responses = responses.sort { |a, b| a.map.reviewer.fullname <=> b.map.reviewer.fullname }
-    end
-    responses
-  end
 
   def self.comparator(m1, m2)
     if m1.version_num && m2.version_num
@@ -46,14 +17,6 @@ class ResponseMap < ApplicationRecord
     else
       1
     end
-  end
-
-  # return latest versions of the response given by reviewer
-  def self.reviewer_assessments_for(team, reviewer)
-    # get_reviewer may return an AssignmentParticipant or an AssignmentTeam
-    # map = where(reviewee_id: team.id, reviewer_id: reviewer.get_reviewer.id)
-    map = where(reviewee_id: team.id, reviewer_id: reviewer.id)
-    Response.where(map_id: map.first.id).sort { |m1, m2| comparator(m1, m2) }.first
   end
 
   # Placeholder method, override in derived classes if required.
