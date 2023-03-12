0555e1a0f5749a7605fec73d0c2f995a353d38e4
Srini Iyer <srini.iyer9@gmail.com>
2023-03-12 13:55:18 -0400

    E2314 - add DB migrate script for creating response map table

---
+++ db/migrate/20230312174319_create_response_maps.rb
@@ -0,0 +1,16 @@
+class CreateResponseMaps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :response_maps do |t|
+      t.integer :id
+      t.integer :reviewed_object_id
+      t.integer :reviewer_id
+      t.integer :reviewee_id
+      t.string :type
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.boolean :calibrate_to
+
+      t.timestamps
+    end
+  end
+end
