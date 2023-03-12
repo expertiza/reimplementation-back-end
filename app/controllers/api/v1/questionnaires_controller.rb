856e86efa713490d467e4a8e8d682342d1b92148
Aditya Srivastava <asrivas7@vclvm178-115.vcl.ncsu.edu>
2023-03-12 19:31:24 -0400

    questionnaire view crud

--- app/controllers/api/v1/questionnaires_controller.rb
+++ app/controllers/api/v1/questionnaires_controller.rb
@@ -1,3 +1,18 @@
 class Api::V1::QuestionnairesController < ApplicationController
 
+  def view
+    @questionnaire = Questionnaire.find(params[:id])
+  end
+
+  private
+  def questionnaire_params
+    params.permit(:name, :instructor_id, :private, :min_question_score,
+                                          :max_question_score, :type, :display_type, :instruction_loc)
+  end
+
+  def question_params
+    params.require(:question).permit(:txt, :weight, :questionnaire_id, :seq, :type, :size,
+                                     :alternatives, :break_before, :max_label, :min_label)
+  end
+
 end
