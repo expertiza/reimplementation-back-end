8c16f1e3481d251843d405009ec3ee09f89b38e3
Rohan Jigarbhai Shah <rshah29@vclvm177-82.vcl.ncsu.edu>
2023-03-13 09:38:12 -0400

    questions_controller.rb view and show endpoints updated

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,15 +1,20 @@
 class Api::V1::QuestionsController < ApplicationController
   
-  # GET /api/v1/questions
-  def index
+  # GET /api/v1/questions/view
+  def view
     @questions = Question.order(:id)
     render json: @questions
   end
 
   # GET /api/v1/questions/:id
   def show
+    begin
     @question = Question.find(params[:id])
     render json: @question
+    rescue
+      error_msg = "No such Question exists."
+      render json: error_msg
+    end
   end
 
   private
