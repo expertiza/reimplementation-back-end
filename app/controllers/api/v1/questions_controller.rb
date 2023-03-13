ab096a378b24b5a03ae0feffda3b85888f16bd28
Aditya Srivastava <asrivas7@vclvm178-115.vcl.ncsu.edu>
2023-03-12 20:43:47 -0400

    questions show and view methods

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,3 +1,21 @@
 class Api::V1::QuestionsController < ApplicationController
   
+  # GET /api/v1/questions
+  def index
+    @questions = Question.order(:id)
+    render json: @questions
+  end
+
+  # GET /api/v1/questions/:id
+  def show
+    @question = Question.find(params[:id])
+    render json: @question
+  end
+
+  private
+
+  def question_params
+    params.permit(:id, :question)
+  end
+
 end
