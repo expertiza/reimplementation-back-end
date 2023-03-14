80bbe52de053e49a8ee51da1d0f270d893e3b28b
anguyen216 <nguyena.r216@gmail.com>
2023-03-13 23:46:57 -0400

    Add reference codes from expertiza/expertiza for all models

--- app/models/multiple_choice_checkbox.rb
+++ app/models/multiple_choice_checkbox.rb
@@ -1,2 +1,96 @@
 class MultipleChoiceCheckbox < QuizQuestion
+  def edit
+    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
+
+    html = '<tr><td>'
+    html += '<textarea cols="100" name="question[' + id.to_s + '][txt]" '
+    html += 'id="question_' + id.to_s + '_txt">' + txt + '</textarea>'
+    html += '</td></tr>'
+
+    html += '<tr><td>'
+    html += 'Question Weight: '
+    html += '<input type="number" name="question_weights[' + id.to_s + '][txt]" '
+    html += 'id="question_wt_' + id.to_s + '_txt" '
+    html += 'value="' + weight.to_s + '" min="0" />'
+    html += '</td></tr>'
+
+    # for i in 0..3
+    [0, 1, 2, 3].each do |i|
+      html += '<tr><td>'
+
+      html += '<input type="hidden" name="quiz_question_choices[' + id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][iscorrect]" '
+      html += 'id="quiz_question_choices_' + id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_iscorrect" value="0" />'
+
+      html += '<input type="checkbox" name="quiz_question_choices[' + id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][iscorrect]" '
+      html += 'id="quiz_question_choices_' + id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_iscorrect" value="1" '
+      html += 'checked="checked" ' if quiz_question_choices[i].iscorrect
+      html += '/>'
+
+      html += '<input type="text" name="quiz_question_choices[' + id.to_s + '][MultipleChoiceCheckbox][' + (i + 1).to_s + '][txt]" '
+      html += 'id="quiz_question_choices_' + id.to_s + '_MultipleChoiceCheckbox_' + (i + 1).to_s + '_txt" '
+      html += 'value="' + quiz_question_choices[i].txt + '" size="40" />'
+
+      html += '</td></tr>'
+    end
+
+    html.html_safe
+    # safe_join(html)
+  end
+
+  def complete
+    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
+    html = '<label for="' + id.to_s + '">' + txt + '</label><br>'
+    # for i in 0..3
+    [0, 1, 2, 3].each do |i|
+      # txt = quiz_question_choices[i].txt
+      html += '<input name = ' + "\"#{id}[]\" "
+      html += 'id = ' + "\"#{id}" + '_' + "#{i + 1}\" "
+      html += 'value = ' + "\"#{quiz_question_choices[i].txt}\" "
+      html += 'type="checkbox"/>'
+      html += quiz_question_choices[i].txt.to_s
+      html += '</br>'
+    end
+    html
+  end
+
+  def view_completed_question(user_answer)
+    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
+    html = ''
+    quiz_question_choices.each do |answer|
+      html += '<b>' + answer.txt + '</b> -- Correct <br>' if answer.iscorrect
+    end
+    html += '<br>Your answer is:'
+    html += if user_answer[0].answer == 1
+              '<img src="/assets/Check-icon.png"/><br>'
+            else
+              '<img src="/assets/delete_icon.png"/><br>'
+            end
+    user_answer.each do |answer|
+      html += '<b>' + answer.comments.to_s + '</b><br>'
+    end
+    html += '<br><hr>'
+    html.html_safe
+    # safe_join(html)
+  end
+
+  def isvalid(choice_info)
+    valid = 'valid'
+    valid = 'Please make sure all questions have text' if txt == ''
+    correct_count = 0
+    # choice_info.each do |_idx, value|
+    choice_info.each_value do |value|
+      if value[:txt] == ''
+        valid = 'Please make sure every question has text for all options'
+        break
+      end
+      correct_count += 1 if value[:iscorrect] == 1.to_s
+    end
+    # if correct_count == 0
+    if correct_count.zero?
+      valid = 'Please select a correct answer for all questions'
+    elsif correct_count == 1
+      valid = 'A multiple-choice checkbox question should have more than one correct answer.'
+    end
+    valid
+  end
 end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,2 +1,117 @@
 class Questionnaire < ApplicationRecord
+  # for doc on why we do it this way,
+  # see http://blog.hasmanythrough.com/2007/1/15/basic-rails-association-cardinality
+  has_many :questions, dependent: :destroy # the collection of questions associated with this Questionnaire
+  belongs_to :instructor # the creator of this questionnaire
+  has_many :assignment_questionnaires, dependent: :destroy
+  has_many :assignments, through: :assignment_questionnaires
+  has_one :questionnaire_node, foreign_key: 'node_object_id', dependent: :destroy, inverse_of: :questionnaire
+
+  validate :validate_questionnaire
+  validates :name, presence: true
+  validates :max_question_score, :min_question_score, numericality: true
+
+  DEFAULT_MIN_QUESTION_SCORE = 0  # The lowest score that a reviewer can assign to any questionnaire question
+  DEFAULT_MAX_QUESTION_SCORE = 5  # The highest score that a reviewer can assign to any questionnaire question
+  DEFAULT_QUESTIONNAIRE_URL = 'http://www.courses.ncsu.edu/csc517'.freeze
+  QUESTIONNAIRE_TYPES = ['ReviewQuestionnaire',
+                         'MetareviewQuestionnaire',
+                         'Author FeedbackQuestionnaire',
+                         'AuthorFeedbackQuestionnaire',
+                         'Teammate ReviewQuestionnaire',
+                         'TeammateReviewQuestionnaire',
+                         'SurveyQuestionnaire',
+                         'AssignmentSurveyQuestionnaire',
+                         'Assignment SurveyQuestionnaire',
+                         'Global SurveyQuestionnaire',
+                         'GlobalSurveyQuestionnaire',
+                         'Course SurveyQuestionnaire',
+                         'CourseSurveyQuestionnaire',
+                         'Bookmark RatingQuestionnaire',
+                         'BookmarkRatingQuestionnaire',
+                         'QuizQuestionnaire'].freeze
+  has_paper_trail
+
+  def get_weighted_score(assignment, scores)
+    # create symbol for "varying rubrics" feature -Yang
+    round = AssignmentQuestionnaire.find_by(assignment_id: assignment.id, questionnaire_id: id).used_in_round
+    questionnaire_symbol = if round.nil?
+                             symbol
+                           else
+                             (symbol.to_s + round.to_s).to_sym
+                           end
+    compute_weighted_score(questionnaire_symbol, assignment, scores)
+  end
+
+  def compute_weighted_score(symbol, assignment, scores)
+    aq = assignment_questionnaires.find_by(assignment_id: assignment.id)
+    if scores[symbol][:scores][:avg].nil?
+      0
+    else
+      scores[symbol][:scores][:avg] * aq.questionnaire_weight / 100.0
+    end
+  end
+
+  # Does this questionnaire contain true/false questions?
+  def true_false_questions?
+    questions.each { |question| return true if question.type == 'Checkbox' }
+    false
+  end
+
+  def delete
+    assignments.each do |assignment|
+      raise "The assignment #{assignment.name} uses this questionnaire.
+            Do you want to <A href='../assignment/delete/#{assignment.id}'>delete</A> the assignment?"
+    end
+
+    questions.each(&:delete)
+
+    node = QuestionnaireNode.find_by(node_object_id: id)
+    node.destroy if node
+
+    destroy
+  end
+
+  def max_possible_score
+    results = Questionnaire.joins('INNER JOIN questions ON questions.questionnaire_id = questionnaires.id')
+                           .select('SUM(questions.weight) * questionnaires.max_question_score as max_score')
+                           .where('questionnaires.id = ?', id)
+    results[0].max_score
+  end
+
+  # clones the contents of a questionnaire, including the questions and associated advice
+  def self.copy_questionnaire_details(params, instructor_id)
+    orig_questionnaire = Questionnaire.find(params[:id])
+    questions = Question.where(questionnaire_id: params[:id])
+    questionnaire = orig_questionnaire.dup
+    questionnaire.instructor_id = instructor_id
+    questionnaire.name = 'Copy of ' + orig_questionnaire.name
+    questionnaire.created_at = Time.zone.now
+    questionnaire.save!
+    questions.each do |question|
+      new_question = question.dup
+      new_question.questionnaire_id = questionnaire.id
+      new_question.size = '50,3' if (new_question.is_a?(Criterion) || new_question.is_a?(TextResponse)) && new_question.size.nil?
+      new_question.save!
+      advices = QuestionAdvice.where(question_id: question.id)
+      next if advices.empty?
+
+      advices.each do |advice|
+        new_advice = advice.dup
+        new_advice.question_id = new_question.id
+        new_advice.save!
+      end
+    end
+    questionnaire
+  end
+
+  # validate the entries for this questionnaire
+  def validate_questionnaire
+    errors.add(:max_question_score, 'The maximum question score must be a positive integer.') if max_question_score < 1
+    errors.add(:min_question_score, 'The minimum question score must be a positive integer.') if min_question_score < 0
+    errors.add(:min_question_score, 'The minimum question score must be less than the maximum.') if min_question_score >= max_question_score
+
+    results = Questionnaire.where('id <> ? and name = ? and instructor_id = ?', id, name, instructor_id)
+    errors.add(:name, 'Questionnaire names must be unique.') if results.present?
+  end
 end
