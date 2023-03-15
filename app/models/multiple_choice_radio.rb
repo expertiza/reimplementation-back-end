5f25bca648bb9d6ed55df1612bee7f06416da00f
Aileen M Jacob <amjacob2@vclvm177-19.vcl.ncsu.edu>
2023-03-15 14:52:56 -0400

    Reimplement complete and view_completed_question method

--- app/models/multiple_choice_radio.rb
+++ app/models/multiple_choice_radio.rb
@@ -35,43 +35,10 @@ class MultipleChoiceRadio < QuizQuestion
   end
 
   def complete
-    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
-    html = '<label for="' + id.to_s + '">' + txt + '</label><br>'
-    # for i in 0..3
-    [0, 1, 2, 3].each do |i|
-      # txt = quiz_question_choices[i].txt
-      html += '<input name = ' + "\"#{id}\" "
-      html += 'id = ' + "\"#{id}" + '_' + "#{i + 1}\" "
-      html += 'value = ' + "\"#{quiz_question_choices[i].txt}\" "
-      html += 'type="radio"/>'
-      html += quiz_question_choices[i].txt.to_s
-      html += '</br>'
-    end
-    html
+   
   end
 
   def view_completed_question(user_answer)
-    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
 
-    html = ''
-    quiz_question_choices.each do |answer|
-      html += if answer.iscorrect
-                '<b>' + answer.txt + '</b> -- Correct <br>'
-              else
-                answer.txt + '<br>'
-              end
-    end
-
-    html += '<br>Your answer is: '
-    html += '<b>' + user_answer.first.comments.to_s + '</b>'
-    html += if user_answer.first.answer == 1
-              '<img src="/assets/Check-icon.png"/>'
-            else
-              '<img src="/assets/delete_icon.png"/>'
-            end
-    html += '</b>'
-    html += '<br><br><hr>'
-    html.html_safe
-    # safe_join(html)
   end
 end
--- app/models/quiz_question.rb
+++ app/models/quiz_question.rb
@@ -19,9 +19,43 @@ class QuizQuestion < Question
     html.html_safe
   end
 
-  def complete; end
+def complete
+    quiz_question_choices = self.quiz_question_choices
+    html = '<label for="' + id.to_s + '">' + txt + '</label><br>'
+    [0, 1, 2, 3].each do |i|
+      html += '<input name = ' + "\"#{id}\" "
+      html += 'id = ' + "\"#{id}" + '_' + "#{i + 1}\" "
+      html += 'value = ' + "\"#{quiz_question_choices[i].txt}\" "
+      html += 'type="radio"/>'
+      html += quiz_question_choices[i].txt.to_s
+      html += '</br>'
+    end
+    html
+  end
 
-  def view_completed_question; end
+  def view_completed_question(user_answer)
+    quiz_question_choices = self.quiz_question_choices
+
+    html = ''
+    quiz_question_choices.each do |answer|
+      html += if answer.iscorrect
+                '<b>' + answer.txt + '</b> -- Correct <br>'
+              else
+                answer.txt + '<br>'
+              end
+    end
+
+    html += '<br>Your answer is: '
+    html += '<b>' + user_answer.first.comments.to_s + '</b>'
+    html += if user_answer.first.answer == 1
+              '<img src="/assets/Check-icon.png"/>'
+            else
+              '<img src="/assets/delete_icon.png"/>'
+            end
+    html += '</b>'
+    html += '<br><br><hr>'
+    html.html_safe
+  end
 
   def isvalid(choice_info)
     @valid = 'valid'
