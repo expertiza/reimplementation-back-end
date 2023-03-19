0366462e8b51eaabab572b8da0d93a949c05c198
anguyen216 <nguyena.r216@gmail.com>
2023-03-19 14:19:46 -0400

    Fix the inputs of isvalid tests for QuizQuestion model

--- spec/requests/model/quiz_question_spec.rb
+++ spec/requests/model/quiz_question_spec.rb
@@ -32,7 +32,7 @@ describe QuizQuestion do
   describe '#isvalid' do
     context 'when the question and its choices have valid text' do
       it 'returns "valid"' do
-        questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: 'question text', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
+        questions = { '1' => { txt: 'question text', iscorrect: true }, '2' => { txt: 'question text', iscorrect: false }, '3' => { txt: 'question text', iscorrect: false }, '4' => { txt: 'question text', iscorrect: false } }
         expect(quiz_question.isvalid(questions)).to eq('valid')
       end
     end
@@ -42,7 +42,7 @@ describe QuizQuestion do
     context 'when the question itself does not have txt' do
       it 'returns "Please make sure all questions have text"' do
         allow(no_text_question).to receive(:txt).and_return('')
-        questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: 'question text', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
+        questions = { '1' => { txt: 'question text', iscorrect: true }, '2' => { txt: 'question text', iscorrect: false }, '3' => { txt: 'question text', iscorrect: false }, '4' => { txt: 'question text', iscorrect: false } }
         expect(no_text_question.isvalid(questions)).to eq('Please make sure all questions have text')
       end
     end
@@ -50,7 +50,7 @@ describe QuizQuestion do
   describe '#isvalid' do
     context 'when a choice does not have text' do
       it 'returns "Please make sure every question has text for all options"' do
-        questions = { '1' => { txt: 'question text', iscorrect: '1' }, '2' => { txt: '', iscorrect: '1' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
+        questions = { '1' => { txt: 'question text', iscorrect: true }, '2' => { txt: '', iscorrect: true }, '3' => { txt: 'question text', iscorrect: false }, '4' => { txt: 'question text', iscorrect: false } }
         expect(quiz_question.isvalid(questions)).to eq('Please make sure every question has text for all options')
       end
     end
@@ -58,7 +58,7 @@ describe QuizQuestion do
   describe '#isvalid' do
     context 'when no choices are correct' do
       it 'returns "Please select a correct answer for all questions"' do
-        questions = { '1' => { txt: 'question text', iscorrect: '0' }, '2' => { txt: 'question text', iscorrect: '0' }, '3' => { txt: 'question text', iscorrect: '0' }, '4' => { txt: 'question text', iscorrect: '0' } }
+        questions = { '1' => { txt: 'question text', iscorrect: false }, '2' => { txt: 'question text', iscorrect: false }, '3' => { txt: 'question text', iscorrect: false }, '4' => { txt: 'question text', iscorrect: false } }
         expect(quiz_question.isvalid(questions)).to eq('Please select a correct answer for all questions')
       end
     end
