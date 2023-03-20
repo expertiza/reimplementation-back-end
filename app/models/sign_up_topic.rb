6f73da93d3430da94ee5b0e3e709f40eeebdb1a6
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 10:03:41 -0400

    create skeleton methods for sign up topic

--- app/models/sign_up_topic.rb
+++ app/models/sign_up_topic.rb
@@ -1,2 +1,30 @@
 class SignUpTopic < ApplicationRecord
+  has_many: :sign_up_team
+
+  validates :name, :max_choosers, presence: true
+  validates :topic_identifier, length: { maximum: 10 }
+
+  def self.find_available_slots?()
+    p "TBD: Query for finding avilable slots goes here."
+  end
+
+  def create_topic()
+    p "Create topic"
+  end
+
+  def update_topic()
+    p "Update topic"
+  end
+
+  def delete_topic()
+    p "Delete topic"
+  end
+
+  def format_for_display()
+    p "Format for display"
+  end
+
+  def update_waitlisted_users()
+    p "Update waitlisted users"
+  end
 end
