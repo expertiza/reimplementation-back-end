e4a1edda44962fa3a5388dc05222212e9e519f28
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 21:04:34 -0400

    rspec basic setup

--- spec/models/sign_up_topic_spec.rb
+++ spec/models/sign_up_topic_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe SignUpTopic, type: :model do
   it "requires presence of name" do
-    expect(SignUpTopic.new).not_be_valid
+    expect(SignUpTopic.new).not_to be_valid
   end
 end
