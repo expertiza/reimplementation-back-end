b02d20fafe7509a2cb78dcec14efe7a93194975e
ipimpal <ipimpal@ncsu.edu>
2023-03-21 12:48:57 -0400

    Update topic to sign_up_topic

--- app/controllers/api/v1/sign_up_topics_controller.rb
+++ app/controllers/api/v1/sign_up_topics_controller.rb
@@ -18,16 +18,16 @@ class Api::V1::SignUpTopicsController < ApplicationController
     #need data for test to pass
     #render json: {message: "The topic: has been created successfully. "}, status: :created
     #topic = ['a',1]
-    topic = SignUpTopic.where(topic_name: params[:topic][:topic_name], assignment_id: params[:topic][:assignment_id]).first
+    topic = SignUpTopic.where(topic_name: params[:sign_up_topic][:topic_name], assignment_id: params[:sign_up_topic][:assignment_id]).first
     if topic.nil?
       @sign_up_topic = SignUpTopic.new #(sign_up_topic_params)
-      @sign_up_topic.topic_identifier = params[:topic][:topic_identifier]
-      @sign_up_topic.topic_name = params[:topic][:topic_name]
-      @sign_up_topic.max_choosers = params[:topic][:max_choosers]
-      @sign_up_topic.category = params[:topic][:category]
-      @sign_up_topic.assignment_id = params[:topic][:assignment_id]
-      @assignment = Assignment.find(params[:topic][:assignment_id])
-      @sign_up_topic.micropayment = params[:topic][:micropayment] if @assignment.microtask?
+      @sign_up_topic.topic_identifier = params[:sign_up_topic][:topic_identifier]
+      @sign_up_topic.topic_name = params[:sign_up_topic][:topic_name]
+      @sign_up_topic.max_choosers = params[:sign_up_topic][:max_choosers]
+      @sign_up_topic.category = params[:sign_up_topic][:category]
+      @sign_up_topic.assignment_id = params[:sign_up_topic][:assignment_id]
+      @assignment = Assignment.find(params[:sign_up_topic][:assignment_id])
+      @sign_up_topic.micropayment = params[:sign_up_topic][:micropayment] if @assignment.microtask?
       if @sign_up_topic.save
         #undo_link "The topic: \"#{@sign_up_topic.topic_name}\" has been created successfully. "
         render json: {message: "The topic: \"#{@sign_up_topic.topic_name}\" has been created successfully. "}, status: :created
@@ -36,9 +36,9 @@ class Api::V1::SignUpTopicsController < ApplicationController
         render json: {message: @sign_up_topic.errors}, status: :unprocessable_entity
       end
     else
-      topic.topic_identifier = params[:topic][:topic_identifier]
+      topic.topic_identifier = params[:sign_up_topic][:topic_identifier]
       #update_max_choosers(topic)
-      topic.category = params[:topic][:category]
+      topic.category = params[:sign_up_topic][:category]
       # topic.assignment_id = params[:id]
       topic.save
       render json: {message: "The topic: \"#{@sign_up_topic.topic_name}\" has been created previously and was updated successfully. "}, status: :created
