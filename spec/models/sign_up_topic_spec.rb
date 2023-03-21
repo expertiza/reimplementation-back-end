0732c65613a19d2d62a2de6164de34fd1cc66e7c
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 21:08:58 -0400

    add few more tests

--- spec/models/sign_up_topic_spec.rb
+++ spec/models/sign_up_topic_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe SignUpTopic, type: :model do
   it "requires presence of name" do
     expect(SignUpTopic.new).not_to be_valid
   end
+
+  it "requries presence of max choosers" do
+    expect(SignUpTopic.new).not_to be_valid
+  end
+
+  it "creates valid sign up topic" do
+    expect(SignUpTopic.new(name: "temp", max_choosers: 10).to be_valid
+  end
 end
