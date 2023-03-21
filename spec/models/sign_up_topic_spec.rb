eab9ad8b035fa15a157bb0d6b81db9bfa6a3859c
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 22:38:36 -0400

    add test for create method

--- spec/models/sign_up_topic_spec.rb
+++ spec/models/sign_up_topic_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe SignUpTopic, type: :model do
     expect(SignUpTopic.new(name: "temp", max_choosers: 10)).to be_valid
   end
 
+  describe "test helper methods" do
+    it "create record via helper method" do
+      temp = SignUpTopic.new(name: "temp", max_choosers: 10)
+      temp.category = "category"
+      temp.topic_identifier = "id"
+      temp.description = "desc"
+      temp2 = SignUpTopic.create_topic("temp", 10, "category", "id", "desc")
+      expect(temp2.name).to eq(temp.name)
+      expect(temp2.category).to eq(temp.category)
+      expect(temp2.max_choosers).to eq(temp.max_choosers)
+      expect(temp2.topic_identifier).to eq(temp.topic_identifier)
+      expect(temp2.description).to eq(temp.description)
+    end
+  end
+
 end
