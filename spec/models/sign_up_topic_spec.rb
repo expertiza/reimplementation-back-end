da05c7f57d363f67786fe7eef3b887f1586bab0b
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 21:12:11 -0400

    add another test for using create helper

--- spec/models/sign_up_topic_spec.rb
+++ spec/models/sign_up_topic_spec.rb
@@ -12,4 +12,9 @@ RSpec.describe SignUpTopic, type: :model do
   it "creates valid sign up topic" do
     expect(SignUpTopic.new(name: "temp", max_choosers: 10)).to be_valid
   end
+
+  it "creates a model through helper" do
+    new_record = SignUpTopic.create_topic("name", 1, "category", "topic_identifier", "description")
+    expect(new_record).to be_valid
+  end
 end
