77ec6a410d0f8b61a6e018fcc68fd63036217cab
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-03-20 21:31:00 -0400

    fix class vs instance methods

--- spec/models/sign_up_topic_spec.rb
+++ spec/models/sign_up_topic_spec.rb
@@ -13,8 +13,10 @@ RSpec.describe SignUpTopic, type: :model do
     expect(SignUpTopic.new(name: "temp", max_choosers: 10)).to be_valid
   end
 
-  it "creates a model through helper" do
-    new_record = SignUpTopic.create_topic("name", 1, "category", "topic_identifier", "description")
-    expect(new_record).to be_valid
+  describe "model helper" do
+    it "creates a model through helper" do
+      new_record = SignUpTopic.create_topic("name", 1, "category", "topic_identifier", "description")
+      expect(new_record).to be_valid
+    end
   end
 end
