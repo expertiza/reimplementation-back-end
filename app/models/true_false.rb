ad5bc8c67de831aaabdba7181fdd194cdda74e25
Joe Johnson <jdjohns4@ncsu.edu>
2023-03-21 10:53:46 -0400

    converted multichoicecheckbox and truefalse to use quiz_question_choices instance var

--- app/models/true_false.rb
+++ app/models/true_false.rb
@@ -5,14 +5,14 @@ class TrueFalse < QuizQuestion
     html += '<tr><td>'
     html += '<input type="radio" name="quiz_question_choices[' + id.to_s + '][TrueFalse][1][iscorrect]" '
     html += 'id="quiz_question_choices_' + id.to_s + '_TrueFalse_1_iscorrect_True" value="True" '
-    html += 'checked="checked" ' if @quiz_question_choices[0].iscorrect
+    html += 'checked="checked" ' if self.quiz_question_choices[0].iscorrect
     html += '/>True'
     html += '</td></tr>'
 
     html += '<tr><td>'
     html += '<input type="radio" name="quiz_question_choices[' + id.to_s + '][TrueFalse][1][iscorrect]" '
     html += 'id="quiz_question_choices_' + id.to_s + '_TrueFalse_1_iscorrect_True" value="False" '
-    html += 'checked="checked" ' if @quiz_question_choices[1].iscorrect
+    html += 'checked="checked" ' if self.quiz_question_choices[1].iscorrect
     html += '/>False'
     html += '</td></tr>'
 
@@ -20,7 +20,7 @@ class TrueFalse < QuizQuestion
   end
 
   def complete
-    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
+    quiz_question_choices = self.quiz_question_choices
     html = '<label for="' + id.to_s + '">' + txt + '</label><br>'
     (0..1).each do |i|
       html += '<input name = ' + "\"#{id}\" "
@@ -38,7 +38,7 @@ class TrueFalse < QuizQuestion
   end
 
   def view_completed_question(user_answer)
-    quiz_question_choices = QuizQuestionChoice.where(question_id: id)
+    quiz_question_choices = self.quiz_question_choices
     html = ''
     html += 'Correct Answer is: <b>'
     html += if quiz_question_choices[0].iscorrect
