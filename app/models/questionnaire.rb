4b08fd461895e7f16248badcfb048ad1c1ef8adf
amisha-w <amishabw@gmail.com>
2023-03-22 22:04:12 -0400

    added implementation for cloning questionnaire with details

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -48,4 +48,57 @@ class Questionnaire < ApplicationRecord
     questions_weight_sum = questions.sum { |question| question.weight}
     max_score = questions_weight_sum * max_question_score
   end
+
+  # Clones the given
+  def self.copy_questionnaire_details(params, instructor_id)
+    questionnaire_id = params[:id]
+    orig_questionnaire = Questionnaire.find(questionnaire_id)
+
+    #Get associated questions for the questionnaire
+    questions = Question.where(questionnaire_id: questionnaire_id)
+
+    #1: Clone Questionnaire
+    cloned_questionnaire = get_cloned_questionnaire(orig_questionnaire)
+
+    #2: Clone Questions within Questionnaire
+    questions.each do |orig_question|
+      cloned_question = get_cloned_question(orig_question, cloned_questionnaire.id)
+
+      #3: Clone Advices for each question
+      advices = QuestionAdvice.where(question_id: orig_question.id)
+      advices.each do |advice|
+        get_cloned_advice(advice, cloned_question.id)
+      end
+    end
+
+    questionnaire
+  end
+
+  def get_cloned_questionnaire(orig_questionnaire)
+    #Create cloned object
+    cloned_questionnaire = orig_questionnaire.clone
+    cloned_questionnaire.instructor_id = instructor_id
+    cloned_questionnaire.name = 'Copy of ' + orig_questionnaire.name
+    cloned_questionnaire.created_at = Time.zone.now
+    cloned_questionnaire.save!
+
+    cloned_questionnaire
+  end
+
+  def get_cloned_question(orig_question, questionnaire_id)
+    cloned_question = orig_question.clone
+    cloned_question.questionnaire_id = questionnaire_id
+    cloned_question.size = '50,3' if (cloned_question.is_a?(Criterion) || cloned_question.is_a?(TextResponse)) && cloned_question.size.nil?
+    cloned_question.save!
+
+    cloned_question
+  end
+
+  def get_cloned_advice(orig_advice, question_id)
+    cloned_advice = orig_advice.dup
+    cloned_advice.question_id = question_id
+    cloned_advice.save!
+
+    cloned_advice
+  end
 end
