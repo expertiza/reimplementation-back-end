754b4f07a88087a14c3d2c3e86bd18c1610bf2a5
Colleen Britt <colleencbritt@gmail.com>
2023-04-07 13:03:55 -0400

    Added comments to checkbox, dropdown, multiple choice, and other question models and the upload file model

--- app/models/dropdown.rb
+++ app/models/dropdown.rb
@@ -2,10 +2,12 @@ class Dropdown < UnscoredQuestion
   include ActionView::Helpers
   validates :alternatives, presence: true
 
+  # This method returns what to display if an instructor (etc.) is creating or editing a questionnaire (questionnaires_controller.rb).
   def edit(_count)
     render partial: 'questionnaire/edit/edit_dropdown'
   end
 
+  # This method returns what to display if an instructor (etc.) is viewing a questionnaire.
   def view_question_text
     html = "<TD align=\"left\"> #{txt} </TD>"
     html += "<TD align=\"left\">#{type}</TD>"
@@ -14,6 +16,7 @@ class Dropdown < UnscoredQuestion
     safe_join(['<TR>'.html_safe, '</TR>'.html_safe], html.html_safe)
   end
 
+  # Returns what to display for the complete dropbox question.
   def complete(count, answer = nil)
     html = "<p style=\"width: 80%;\"><label for=\"responses_#{count.to_s}\"\">#{txt}&nbsp;&nbsp;</label>"
     html += "<input id=\"responses_#{count.to_s}_score\" name=\"responses[#{count.to_s}][score]\" type=\"hidden\" value=\"\" style=\"min-width: 100px;\">"
@@ -25,6 +28,7 @@ class Dropdown < UnscoredQuestion
     html.html_safe
   end
 
+  # Returns what to display for the complete alternative question.
   def complete_for_alternatives(alternatives, answer)
     html = ''
     alternatives.each do |alternative|
@@ -35,6 +39,7 @@ class Dropdown < UnscoredQuestion
     html
   end
 
+  # Returns what to display if a student is viewing a filled-out questionnaire.
   def view_completed_question(count, answer)
     html = "<b>#{count.to_s}. #{txt}</b>"
     html += "<BR>&nbsp&nbsp&nbsp&nbsp#{answer.comments.to_s}"
--- app/models/scored_question.rb
+++ app/models/scored_question.rb
@@ -2,14 +2,19 @@ class ScoredQuestion < ChoiceQuestion
   validates :weight, presence: true # user must specify a weight for a question
   validates :weight, numericality: true # the weight must be numeric
 
+  # This method returns what to display if an instructor (etc.) is creating or editing a questionnaire (questionnaires_controller.rb).
   def edit; end
 
+  # This method returns what to display if an instructor (etc.) is viewing a questionnaire
   def view_question_text; end
 
+  # Returns what to display for the complete question.
   def complete; end
 
+  # Returns what to display if a student is viewing a filled-out questionnaire.
   def view_completed_question; end
 
+  # Calculates the score by adding weight to the answer.
   def self.compute_question_score(response_id)
     answer = Answer.find_by(question_id: id, response_id: response_id)
     weight * answer.answer
