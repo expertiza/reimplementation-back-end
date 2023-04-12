80d9798c1a28ce6650213afdb55e5fafdb4f4e8e
kajadhav <kajadhav@ncsu.edu>
2023-04-11 22:03:02 -0400

    Feat: Add AccountRequest model

---
+++ app/models/account_request.rb
@@ -0,0 +1,16 @@
+class AccountRequest < ApplicationRecord
+    belongs_to :role
+    belongs_to :institution
+  
+    before_save { self.email = email.downcase }
+    before_save { name }
+    
+    validates :name, presence: true, length: { maximum: 50, message: 'is too long' }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, presence: true, length: { maximum: 255, message: 'is too long' },
+                      format: { with: VALID_EMAIL_REGEX, message: 'format is wrong' },
+                      uniqueness: { case_sensitive: false, message: 'has already existed in Expertiza' }
+  
+    validates :fullname, presence: true, length: { maximum: 100, message: 'is too long' }
+  end
+  
---
+++ db/migrate/20230412013310_add_keys_to_account_requests.rb
@@ -0,0 +1,6 @@
+class AddKeysToAccountRequests < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :account_requests, :role, null: false, foreign_key: true
+    add_reference :account_requests, :institution, null: false, foreign_key: true
+  end
+end
