dc4061cf4bc24da99fdfaf9e0593f292945fe2ca
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-12 11:44:13 -0400

    add waitlist empty methods

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -1,4 +1,20 @@
 class Waitlist < ApplicationRecord
   belongs_to :signup_topic
   belongs_to :signed_up_team
+
+  def self.count_waitlisted_team()
+    # TODO: Return the count of waitlisted teams.
+  end
+
+  def self.remove_teams_from_waitlist()
+    # TODO: Remove teams from the waitlist.
+  end
+
+  def self.add_teams_to_waitlist()
+    # TODO: Add teams to the waitlist.
+  end
+
+  def self.get_waitlisted_teams()
+    # TODO: Get all the waitlisted teams
+  end
 end
