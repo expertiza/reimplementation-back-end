89ea94fef7b97b53c8f6938857ffc153ade377f4
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-12 17:05:19 -0400

    add remaining waitlist methods and tests

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -6,8 +6,20 @@ class Waitlist < ApplicationRecord
     return Waitlist.where(signup_topic_id: topic_id).count
   end
 
-  def self.remove_teams_from_waitlist()
-    # TODO: Remove teams from the waitlist.
+  def self.remove_teams_from_waitlist(topic_id, team_ids)
+    items_to_delete = Waitlist.where(signup_topic_id: topic_id, signed_up_team_id: team_ids)
+
+    deleted_item_count = items_to_delete.size
+    items_to_delete.destroy_all
+    return deleted_item_count
+  end
+
+  def self.promote_teams_from_waitlist(topic_id, count=1)
+    teams_in_waitlist = Waitlist.where(signup_topic_id: topic_id).limit(count).order('created_at asc').pluck(:signed_up_team_id);
+
+    Waitlist.where(signed_up_team_id: teams_in_waitlist).destroy_all
+
+    return teams_in_waitlist
   end
 
   def self.get_waitlisted_teams(topic_id)
--- spec/models/waitlist_spec.rb
+++ spec/models/waitlist_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Waitlist, type: :model do
   end
 
   describe "This tests functionality of the waitlist" do
-
+    # Helper method to use add an entry to the waitlist table using the active records created by the sign up topic and signed up team factories.
     def add_entry_to_waitlist()
       waitlist = Waitlist.new(
         signup_topic_id: topic["id"], 
@@ -23,7 +23,13 @@ RSpec.describe Waitlist, type: :model do
       waitlist.save
       return waitlist
     end
+    
+    # This test uses the factories to create a signed_up_team and signup_topic which then gets added to the 
+    it "Add teams to waitlist" do
+      expect(add_entry_to_waitlist()).to be_valid
+    end
 
+    # This test adds a waitlist entry and then calls the helper method to validate the count of teams currently in the waitlist for the given topic.
     it "Returns count of teams waitlisted for given topic" do
       expect(add_entry_to_waitlist()).to be_valid
 
@@ -33,12 +39,11 @@ RSpec.describe Waitlist, type: :model do
     end
 
     it "Removes teams from waitlist" do
-      # TODO: remove a team from waitlist
-    end
-
-    # This test uses the factories to create a signed_up_team and signup_topic which then gets added to the 
-    it "Add teams to waitlist" do
       expect(add_entry_to_waitlist()).to be_valid
+
+      delete_count = Waitlist.remove_teams_from_waitlist(topic["id"], [team["id"]])
+
+      expect(delete_count).to eq(1)
     end
 
     # This test adds a team to the waitlist and then queries for it to check for query response.
@@ -47,6 +52,17 @@ RSpec.describe Waitlist, type: :model do
 
       teams = Waitlist.get_waitlisted_teams(topic["id"])
       expect(teams).to include(team["id"])
+    end
+
+    it "Promotes waitlisted teams" do
+      expect(add_entry_to_waitlist()).to be_valid
+
+      actual_promoted_teams = Waitlist.promote_teams_from_waitlist(topic["id"])
+
+      expect(actual_promoted_teams).to include(team["id"])
+
+      waitlist_count = Waitlist.count_waitlisted_teams(team["id"])
+      expect(waitlist_count).to eq(0)
     end
   end
 end
