2df5aef7795f296b66c693ab4f8cfeca0df57d30
Saketh Vangala <sakethvangala36323@gmail.com>
2023-04-14 15:58:57 -0400

    review comments

--- app/models/signed_up_team.rb
+++ app/models/signed_up_team.rb
@@ -3,28 +3,27 @@ class SignedUpTeam < ApplicationRecord
   belongs_to :team 
 
   # Get participants in the team signed up for a given topic.
-  def find_team_participants()
+  def get_team_participants()
     return team.get_team_participants()
   end 
 
   #This method is responsible for creating a signed_up_team for a topic by checking the condition if topic is available to choose.
-  def self.create_signed_up_team(topic_identifier,team_id)
-    signup_topic = SignupTopic.where(id: topic_identifier).first
+  def self.create_signed_up_team(topic_id, team_id)
+    signup_topic = SignupTopic.find(topic_id)
 
-    if signup_topic.find_if_topic_available() == false
+    if signup_topic.is_available() == false
       return false
     end
 
-    signed_up_team = SignedUpTeam.new(signup_topic_id: topic_identifier,team_id: team_id)
-    signed_up_team.save
+    signed_up_team = SignedUpTeam.create!({:signup_topic_id => topic_id, :team_id => team_id})
 
     return true
   end
 
   #This method is responsible for deleting a signed_up_team for a topic and delegating any changes required in topic
   def self.delete_signed_up_team(team_id)
-    signed_up_team = SignedUpTeam.where(id: team_id).first
-    topic_release_status = signed_up_team.signup_topic.release_topic(signed_up_team.id)
+    signed_up_team = SignedUpTeam.find(team_id)
+    topic_release_status = signed_up_team.signup_topic.release_team(signed_up_team.id)
 
     if topic_release_status == true
       signed_up_team.destroy
