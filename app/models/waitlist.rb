a7842cc5a80e059c555b831127c9d41db1748d33
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-14 13:57:13 -0400

    update all tests for positive functionality checks

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -2,32 +2,22 @@
 class Waitlist
   # Return the count of teams in the waitlist for the given topic.
   def self.count_waitlisted_teams(topic_id)
-    return Waitlist.where(signup_topic_id: topic_id).count
-  end
-  
-  # Remove chosen teams from the waitlist for the given topic id.
-  # NOTE: This is required during the un-register process where teams can drop a topic while they are on the waitlist.
-  def self.remove_teams_from_waitlist(topic_id, team_ids)
-    items_to_delete = Waitlist.where(signup_topic_id: topic_id, signed_up_team_id: team_ids)
-
-    deleted_item_count = items_to_delete.size
-    items_to_delete.destroy_all
-    return deleted_item_count
+    return SignedUpTeam.where(signup_topic_id: topic_id, is_waitlisted: true).count
   end
   
   # Choose the first N teams in the waitlist for promotion and delete them from the waitlist.
   def self.promote_teams_from_waitlist(topic_id, count=1)
-    promotable_teams = Waitlist.where(signup_topic_id: topic_id)
-                          .limit(count)
-                          .order('created_at asc')
-                          .pluck(:signed_up_team_id);
+    promotable_teams = SignedUpTeam.where(signup_topic_id: topic_id, is_waitlisted: true).limit(count).order('created_at asc');
 
-    Waitlist.where(signed_up_team_id: promotable_teams).destroy_all
-    return promotable_teams
-  end
+    promoted_ids = promotable_teams.pluck(:id)
+    
+    promotable_teams.update_all({:is_waitlisted => false})
+
+    return promoted_ids
+end
 
   # Returns the IDs of the teams which are on the waitlist for the chosen topic.
   def self.get_waitlisted_teams(topic_id)
-    return Waitlist.where(signup_topic_id: topic_id).pluck(:signed_up_team_id)
+    return SignedUpTeam.where(signup_topic_id: topic_id).pluck(:id)
   end
 end
