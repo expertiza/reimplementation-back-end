9f64c9210b068404f82d2e430c90d766fb188c6d
Saketh Vangala <sakethvangala36323@gmail.com>
2023-04-14 21:01:11 -0400

    Add tests for signed_up_team

--- spec/models/signed_up_team_spec.rb
+++ spec/models/signed_up_team_spec.rb
@@ -20,12 +20,22 @@ RSpec.describe SignedUpTeam, type: :model do
       expect(signed_up_team.get_team_participants()).to eq(true)
     end
 
-    it "Signs up a team for the topic" do
+    it "Signs up a team for the topic if the topic is available" do
       expect(SignedUpTeam.create_signed_up_team(topic["id"],team["id"])).to eq(true)
+    end
+
+    it 'creates a signed up team if the topic is available' do
+      expect { SignedUpTeam.create_signed_up_team(topic['id'], team['id']) }.to change(SignedUpTeam, :count).by(1)
     end
 
     it "Deletes the signed_up_team for the topic assigned" do
       expect(SignedUpTeam.delete_signed_up_team(signed_up_team["id"])).to eq(true)
     end
+
+    it 'deletes the signed up team for a topic and delegates any required changes' do
+      expect(signed_up_team).to be_valid
+      expect { SignedUpTeam.delete_signed_up_team(signed_up_team["id"]) }.to change(SignedUpTeam, :count).by(-1)
+    end
+    
   end
 end
