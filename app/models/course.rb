f101202c40b4ba5c3116daf7a8e85469a8645aaa
ameyagv <ameyav1234@gmail.com>
2023-04-14 21:33:12 -0400

    added method signature for path and get_tas in course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,17 @@ class Course < ApplicationRecord
   belongs_to :institution, foreign_key: 'institution_id'
   validates :name, presence: true
   validates :directory_path, presence: true
+  has_many :ta_mappings, dependent: :destroy
+  has_many :tas, through: :ta_mappings
 
+  #returns the submission directory for the course
+  def path
+
+  end
+
+  #returns tas associated with the course
+  def get_tas
+
+  end
 
 end
