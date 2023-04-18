3ed545061de22b77d92642e1c7f0cc69f8d18245
rohitgeddam <rohitgeddam2018@gmail.com>
2023-04-18 15:11:17 -0400

    add more tests for InvitationController

--- spec/requests/api/v1/invitation_controller_spec.rb
+++ spec/requests/api/v1/invitation_controller_spec.rb
@@ -2,8 +2,9 @@ require 'swagger_helper'
 require 'rails_helper'
 
 RSpec.describe 'Invitations API', type: :request do
-  let(:user1) { create :user }
-  let(:user2) { create :user }
+  let(:user1) { create :user, name: "rohitgeddam" }
+  let(:user2) { create :user, name: "superman" }
+  let(:invalid_user) { build :user, name: "INVALID"}
   let(:assignment) { create(:assignment) }
 
   path '/api/v1/invitations' do
@@ -40,10 +41,9 @@ RSpec.describe 'Invitations API', type: :request do
         required: %w[assignment_id from_id to_id]
       }
 
-      response(201, 'Create an invitation') do
+      response(201, 'Create an invitation with valid parameters') do
         let(:invitation) { {to_id: user1.id, from_id: user2.id, assignment_id: assignment.id} }
         after do |example|
-          p example
           example.metadata[:response][:content] = {
             'application/json' => {
               example: JSON.parse(response.body, symbolize_names: true)
@@ -52,6 +52,67 @@ RSpec.describe 'Invitations API', type: :request do
         end
         run_test!
       end
+
+      response(422, 'Create an invitation with invalid to user parameters') do
+        let(:invitation) { {to_id: invalid_user.id, from_id: user2.id, assignment_id: assignment.id} }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(422, 'Create an invitation with invalid from user parameters') do
+        let(:invitation) { {to_id: user1.id, from_id: invalid_user.id, assignment_id: assignment.id} }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(422, 'Create an invitation with invalid assignment parameters') do
+        let(:invitation) { {to_id: user1.id, from_id: user2.id, assignment_id: nil} }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(422, 'Create an invitation with invalid reply_status parameters') do
+        let(:invitation) { { to_id: user1.id, from_id: user2.id, assignment_id: assignment.id, reply_status: "I" } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
+      response(422, 'Create an invitation with same to user and from user parameters') do
+        let(:invitation) { { to_id: user1.id, from_id: user1.id, assignment_id: assignment.id } }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+
 
     end
 
