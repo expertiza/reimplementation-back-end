d475c6772b6b9bc6efa2057050405bb2e63f73cd
Varun2000 <varundanda2000@gmail.com>
2023-04-20 00:19:49 -0400

    made correction for get_team_participants

--- app/models/signup_topic.rb
+++ app/models/signup_topic.rb
@@ -43,7 +43,7 @@ class SignupTopic < ApplicationRecord
 
   # Method used to retrieve participants in signed up team of particular topic.
   def get_team_participants(team_id)
-    self.signed_up_teams.find(team_id).team
+    self.signed_up_teams.find(team_id).get_team_participants
   end
 
   # Method used to return number of available slots for teams to sign up for the topic.
