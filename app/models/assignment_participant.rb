77ca040cdb28d03f7f82cbe417d5fc5ff2696b7d
Devashish Vachhani <dvachha@vclvm178-114.vcl.ncsu.edu>
2023-04-22 11:10:17 -0400

    scaffold for AssignmentParticipant model

--- app/models/assignment_participant.rb
+++ app/models/assignment_participant.rb
@@ -1,6 +1,2 @@
-class AssignmentParticipant < Participant
-    belongs_to  :assignment, class_name: 'Assignment', foreign_key: 'parent_id'
-    belongs_to :user
-    def handle
-    end
+class AssignmentParticipant < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_22_150739) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_22_150940) do
+  create_table "assignment_participants", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "assignments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "directory_path"
