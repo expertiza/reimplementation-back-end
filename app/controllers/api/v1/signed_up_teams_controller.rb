cf9c30bfdfc441d49ac919a8061ec98df018ec2d
ipimpal <ipimpal@ncsu.edu>
2023-04-23 17:44:45 -0400

    Update SignedUpTeams

--- app/controllers/api/v1/signed_up_teams_controller.rb
+++ app/controllers/api/v1/signed_up_teams_controller.rb
@@ -1,24 +1,29 @@
 class Api::V1::SignedUpTeamsController < ApplicationController
 
   def index
-    puts params[:topic_id]
+    # puts params[:topic_id]
     @sign_up_topic = SignUpTopic.find(params[:topic_id])
     @signed_up_team = SignedUpTeam.find_team_participants(@sign_up_topic.assignment_id)
     render json: @signed_up_team
   end
-  def create(topic_id, team_id)
-    @sign_up_topic = SignUpTopic.where(assignment_id: topic_id).first
-    puts @sign_up_topic
-    @signed_up_team = SignedUpTeam.new
-    @signed_up_team.topic_id = @sign_up_topic.id
-    @signed_up_team.team_id = team_id
-    if @signed_up_team.save
-      render json: {message: "Signed up team successful!"}, status: :created
-    else
-      render json: {message: @signed_up_team.errors}, status: :unprocessable_entity
-    end
+
+
+  # Implemented by signed_up_team.rb (Model) --> create_signed_up_team
+  # This implementation was for the purpose of our understanding
+  def create #(topic_id, team_id)
+    # @sign_up_topic = SignUpTopic.where(assignment_id: topic_id).first
+    # puts @sign_up_topic
+    # @signed_up_team = SignedUpTeam.new
+    # @signed_up_team.topic_id = @sign_up_topic.id
+    # @signed_up_team.team_id = team_id
+    # if @signed_up_team.save
+    #   render json: {message: "Signed up team successful!"}, status: :created
+    # else
+    #   render json: {message: @signed_up_team.errors}, status: :unprocessable_entity
+    # end
   end
 
+  #Update signed_up_team using parameters.
   def update
     @signed_up_team = SignedUpTeam.find(params[:id])
     if @signed_up_team.update(signed_up_teams_params)
@@ -28,24 +33,27 @@ class Api::V1::SignedUpTeamsController < ApplicationController
     end
   end
 
+  #Sign up using
   def sign_up
     team_id = params[:team_id]
     topic_id = params[:topic_id]
-    create(topic_id, team_id)
+    SignedUpTeam.create_signed_up_team(topic_id, team_id)
   end
 
   #Method for signing up as student
   def sign_up_student
     user_id = params[:user_id]
     topic_id = params[:topic_id]
-    @teams_user = TeamsUser.where(user_id: user_id).first
-    team_id = @teams_user.team_id
-    create(topic_id, team_id)
+    team_id = SignedUpTeam.get_team_participants
+    # @teams_user = TeamsUser.where(user_id: user_id).first
+    # team_id = @teams_user.team_id
+    SignedUpTeam.create_signed_up_team(topic_id, team_id)
+    # create(topic_id, team_id)
   end
 
   def destroy
     @signed_up_team = SignedUpTeam.find(params[:id])
-    if @signed_up_team.drop_team
+    if @signed_up_team.delete_signed_up_team
       render json: {message: 'Signed up teams was deleted successfully!'}, status: :no_content
     else
       render json: @signed_up_team.errors, status: :unprocessable_entity
--- app/models/signed_up_team.rb
+++ app/models/signed_up_team.rb
@@ -1,78 +1,109 @@
 class SignedUpTeam < ApplicationRecord
-  def drop_team
-    true
+  belongs_to :signup_topic
+  belongs_to :team
+
+  # This method is to get participants in the team signed up for a given topic. It calls on the parent Team Class to fetch the participants details.
+  def get_team_participants
+    return team.get_team_participants()
+  end
+
+  #This is a class method responsible for creating a SignedUpTeam instance with given topic_id and team_id by checking the condition if topic is available to choose.
+  def self.create_signed_up_team(topic_id, team_id)
+    signup_topic = SignupTopic.find(topic_id)
+
+    if signup_topic.is_available() == false
+      return false
+    end
+
+    signed_up_team = SignedUpTeam.create!({:signup_topic_id => topic_id, :team_id => team_id})
+
+    return true
+  end
+
+  #This is a class method responsible for deleting a SignedUpTeam instance for a topic and delegating any changes required in topic
+  def self.delete_signed_up_team(team_id)
+    signed_up_team = SignedUpTeam.find(team_id)
+    topic_release_status = signed_up_team.signup_topic.release_team(signed_up_team.id)
+
+    if topic_release_status == true
+      signed_up_team.destroy
+      return true
+    else
+      return false
+    end
+
   end
   def self.find_team_participants(assignment_id)
     #assignment_id
     { data:[
-        {
-          "id": 123,
-          "topic_identifier": "E2333",
-          "topic_name": "Github metrics integration",
-          "max_choosers": 1,
-          "available_slots": 0,
-          "num_waitlisted": 1,
-          "signed_up_teams": [
-            {
-              "signed_up_team_id": 123,
-              "is_waitlisted": false,
-              "team_members": [
-                {
-                  "name": "John Doe"
-                },
-                {
-                  "name": "Jane Doe"
-                }
-              ]
-            },
-            {
-              "signed_up_team_id": 124,
-              "is_waitlisted": true,
-              "team_members": [
-                {
-                  "name": "John Doe"
-                },
-                {
-                  "name": "Jane Doe"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "id": 124,
-          "topic_identifier": "E2334",
-          "topic_name": "Github metrics integration",
-          "max_choosers": 1,
-          "available_slots": 0,
-          "num_waitlisted": 1,
-          "signed_up_teams": [
-            {
-              "signed_up_team_id": 123,
-              "is_waitlisted": false,
-              "team_members": [
-                {
-                  "name": "John Doe"
-                },
-                {
-                  "name": "Jane Doe"
-                }
-              ]
-            },
-            {
-              "signed_up_team_id": 124,
-              "is_waitlisted": true,
-              "team_members": [
-                {
-                  "name": "John Doe"
-                },
-                {
-                  "name": "Jane Doe"
-                }
-              ]
-            }
-          ]
-        }
-      ]}
+      {
+        "id": 123,
+        "topic_identifier": "E2333",
+        "topic_name": "Github metrics integration",
+        "max_choosers": 1,
+        "available_slots": 0,
+        "num_waitlisted": 1,
+        "signed_up_teams": [
+          {
+            "signed_up_team_id": 123,
+            "is_waitlisted": false,
+            "team_members": [
+              {
+                "name": "John Doe"
+              },
+              {
+                "name": "Jane Doe"
+              }
+            ]
+          },
+          {
+            "signed_up_team_id": 124,
+            "is_waitlisted": true,
+            "team_members": [
+              {
+                "name": "John Doe"
+              },
+              {
+                "name": "Jane Doe"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "id": 124,
+        "topic_identifier": "E2334",
+        "topic_name": "Github metrics integration",
+        "max_choosers": 1,
+        "available_slots": 0,
+        "num_waitlisted": 1,
+        "signed_up_teams": [
+          {
+            "signed_up_team_id": 123,
+            "is_waitlisted": false,
+            "team_members": [
+              {
+                "name": "John Doe"
+              },
+              {
+                "name": "Jane Doe"
+              }
+            ]
+          },
+          {
+            "signed_up_team_id": 124,
+            "is_waitlisted": true,
+            "team_members": [
+              {
+                "name": "John Doe"
+              },
+              {
+                "name": "Jane Doe"
+              }
+            ]
+          }
+        ]
+      }
+    ]}
   end
 end
