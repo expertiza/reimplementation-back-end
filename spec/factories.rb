2610af49fee23aed83bbe1322392e21a28d5aeec
rohitgeddam <rohitgeddam2018@gmail.com>
2023-04-23 09:53:12 -0400

    Update factories.rb
    Fix Rubocop issues

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
 
   factory :user do
-    sequence(:name) { |n| "#{Faker::Name.name}".delete(" \t\r\n").downcase }
-    sequence(:email) { |n| "#{Faker::Internet.email}"}
-    password { "blahblahblah" }
+    sequence(:name) { |n| Faker::Name.name.to_s.delete(" \t\r\n").downcase }
+    sequence(:email) { |n| Faker::Internet.email.to_s}
+    password { 'password' }
     sequence(:fullname) { |n| "#{Faker::Name.name}#{Faker::Name.name}".downcase  }
     role factory: :role
   end
@@ -13,13 +13,13 @@ FactoryBot.define do
   end
 
   factory :assignment do
-    name { (Assignment.last ? ('assignment' + (Assignment.last.id + 1).to_s) : 'final2').to_s }
+    name { (Assignment.last ? "assignment#{(Assignment.last.id + 1).to_s}" : 'final2').to_s }
   end
 
   factory :invitation do
     from_user factory: :user
     to_user factory: :user
     assignment factory: :assignment
-    reply_status { "W" }
+    reply_status { 'W' }
   end
 end
