94822b50c223d3f52cb527a8c899dec2308824fe
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-24 14:03:32 -0400

    remove unused method and update comment in waitlist

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -5,7 +5,7 @@ class Waitlist
     return SignedUpTeam.where(signup_topic_id: topic_id, is_waitlisted: true).count
   end
   
-  # Choose the first N teams in the waitlist for promotion and delete them from the waitlist.
+  # Choose the first N teams in the waitlist for promotion and delete them from the waitlist. The teams are sorted in ascending order based on the creation date in order to prioritize the teams that were put on the waitlist ahead of others.
   def self.promote_teams_from_waitlist(topic_id, count=1)
     promotable_teams = SignedUpTeam.where(signup_topic_id: topic_id, is_waitlisted: true).limit(count).order('created_at asc');
 
@@ -14,10 +14,5 @@ class Waitlist
     promotable_teams.update_all({:is_waitlisted => false})
 
     return promoted_ids
-end
-
-  # Returns the IDs of the teams which are on the waitlist for the chosen topic.
-  def self.get_waitlisted_teams(topic_id)
-    return SignedUpTeam.where(signup_topic_id: topic_id).pluck(:id)
   end
 end
