f2251f6480e6840e67f646baccca558083f95750
sankettangade <sankettangade99@gmail.com>
2023-04-24 23:15:24 -0400

    Added duties_controller.rb and duty.rb changes

--- app/controllers/api/v1/duties_controller.rb
+++ app/controllers/api/v1/duties_controller.rb
@@ -1,16 +1,66 @@
 class Api::V1::DutiesController < ApplicationController
+
+  before_action :set_assignment
+  before_action :set_duty, only: %i[show edit update destroy]
+  before_action :authorize_user, except: [:index, :show]
+
   def index
+    @duties = @assignment.duties
   end
 
   def show
   end
 
+  def new
+    @duty = @assignment.duties.build
+  end
+
+  def edit
+  end
+
   def create
+    @duty = @assignment.duties.build(duty_params)
+
+    if @duty.save
+      redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully created.'
+    else
+      redirect_to_create_page_and_show_error
+    end
   end
 
   def update
+    if @duty.update(duty_params)
+      redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully updated.'
+    else
+      redirect_to_create_page_and_show_error
+    end
   end
 
   def destroy
+    @duty.destroy
+    redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully deleted.'
   end
-end
+
+    private
+
+  def set_assignment
+    @assignment = Assignment.find(params[:assignment_id])
+  end
+
+  def set_duty
+    @duty = @assignment.duties.find(params[:id])
+  end
+
+  def authorize_user
+    redirect_to root_path, alert: 'You are not authorized to perform this action.' unless current_user_has_ta_privileges?
+  end
+
+  def redirect_to_create_page_and_show_error
+    flash[:error] = @duty.errors.full_messages.join('. ')
+    redirect_to action: :new, assignment_id: @assignment.id
+  end
+
+  def duty_params
+    params.require(:duty).permit(:max_members_for_duty, :name)
+  end
+  end
--- app/models/duty.rb
+++ app/models/duty.rb
@@ -1,28 +1,18 @@
 class Duty < ApplicationRecord
   belongs_to :assignment
+  validates :name, presence: true,
+                   format: { with: /\A[^`!@#\$%\^&*+_=]+\z/,
+                             message: 'Please enter a valid role name' },
+                   length: {
+                     minimum: 3,
+                     message: 'Role name is too short (minimum is 3 characters)'
+                   }
+  validates :name, uniqueness: { case_sensitive: false, scope: :assignment_id,
+                                 message: 'The role "%{value}" is already present for this assignment' }
+  validates :max_members_for_duty, numericality: { only_integer: true, greater_than_or_equal_to: 1,
+                                                   message: 'Value for max members for role is invalid' }
 
-  validates :name,
-            presence: true,
-            format: { with: /\A[^`!@#\$%\^&*+_=]+\z/,
-                      message: 'Please enter a valid role name' },
-            length: {
-              minimum: 3,
-              message: 'Role name is too short (minimum is 3 characters)'
-            },
-            uniqueness: {
-              case_sensitive: false, scope: :assignment,
-              message: 'The role "%{value}" is already present for this assignment'
-            }
-
-  validates_numericality_of :max_members_for_duty,
-                            only_integer: true,
-                            greater_than_or_equal_to: 1,
-                            message: 'Value for max members for role is invalid'
-
-  # check if the duty selected is available for selection in that particular team. 
-  # Checks whether current duty count in the team is less than the max_members_for_duty 
-  # set for that particular duty
   def can_be_assigned?(team)
-    max_members_for_duty > team.participants.where(duty_id: id).count
+    max_members_for_duty > team.participants.select { |team_member| team_member.duty_id == id }.count
   end
 end
