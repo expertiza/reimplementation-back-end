120e94ae8c37a0ddcf36b18ffa6bb43cb948e2a9
Kunal Govind Patil <kunal@Kunals-MacBook-Air.local>
2023-04-24 20:37:01 -0400

    route mod

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
-      get 'badges/index'
-      get 'badges/show'
-      get 'badges/create'
-      get 'badges/update'
-      get 'badges/destroy'
-      get 'duties/index'
-      get 'duties/show'
-      get 'duties/create'
-      get 'duties/update'
-      get 'duties/destroy'
+      resources :badges
+      resources :duties
       resources :institutions
       resources :roles
       resources :users do
