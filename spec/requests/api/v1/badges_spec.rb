d7fa23f007ad48a2a1a216ebcf68554fcb23be83
Kunal Govind Patil <kunal@Kunals-MacBook-Air.local>
2023-04-24 20:37:26 -0400

    controller code fix

--- spec/requests/api/v1/badges_spec.rb
+++ spec/requests/api/v1/badges_spec.rb
@@ -5,37 +5,42 @@ RSpec.describe "Api::V1::Badges", type: :request do
 
   describe "GET #index" do
     it "returns http success" do
-      get "/api/v1/badges/index"
+      get "/api/v1/badges"
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/api/v1/badges/show"
+      badge = Badge.create(name: "Test Badge", description: "This is a test badge")
+      get "/api/v1/badges/#{badge.id}"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
+  describe "POST /create" do
     it "returns http success" do
-      get "/api/v1/badges/create"
+      post "/api/v1/badges", params: { badge: { name: "Test Badge", description: "This is a test badge" } }
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe "PATCH /update" do
+    it "returns http success" do
+      badge = Badge.create(name: "Test Badge", description: "This is a test badge")
+      patch "/api/v1/badges/#{badge.id}", params: { badge: { name: "New Test Badge Name" } }
+      follow_redirect!
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /update" do
+  describe "DELETE /destroy" do
     it "returns http success" do
-      get "/api/v1/badges/update"
-      expect(response).to have_http_status(:success)
+      badge = Badge.create(name: "Test Badge", description: "This is a test badge")
+      delete "/api/v1/badges/#{badge.id}"
+      expect(response).to have_http_status(:redirect)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/badges/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
 
 end
