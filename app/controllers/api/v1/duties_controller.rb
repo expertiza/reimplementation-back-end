5d9b9cc5cf3ff7eff3f31eb57154af7305838d9c
sankettangade <sankettangade99@gmail.com>
2023-04-25 23:18:11 -0400

    duties_controller test cases

--- app/controllers/api/v1/duties_controller.rb
+++ app/controllers/api/v1/duties_controller.rb
@@ -1,28 +1,26 @@
-class Api::V1::DutiesController < ApplicationController
+# frozen_string_literal: true
 
-  before_action :set_assignment
-  before_action :authorize_user, except: [:index, :show]
+class Api::V1::DutiesController < ApplicationController
   before_action :set_duty, only: %i[show edit update destroy]
 
   def index
-    @duties = @assignment.duties
+    @duties = Duty.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
-    @duty = @assignment.duties.build
+    @duty = Duty.new
+    @id = params[:id]
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
-    @duty = @assignment.duties.build(duty_params)
+    @duty = Duty.new(duty_params)
 
     if @duty.save
-      redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully created.'
+      redirect_to redirect_to_url, notice: 'Role was successfully created.'
     else
       redirect_to_create_page_and_show_error
     end
@@ -30,7 +28,7 @@ class Api::V1::DutiesController < ApplicationController
 
   def update
     if @duty.update(duty_params)
-      redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully updated.'
+      redirect_to redirect_to_url, notice: 'Role was successfully updated.'
     else
       redirect_to_create_page_and_show_error
     end
@@ -38,30 +36,29 @@ class Api::V1::DutiesController < ApplicationController
 
   def destroy
     @duty.destroy
-    redirect_to edit_assignment_path(@assignment), notice: 'Role was successfully deleted.'
+    redirect_to redirect_to_url, notice: 'Role was successfully deleted.'
   end
 
-
-
-    private
-  def set_assignment
-    @assignment = Assignment.find(params[:assignment_id])
-  end
+  private
 
   def set_duty
-    @duty = @assignment.duties.find(params[:id])
+    @duty = Duty.find(params[:id])
   end
 
-  def authorize_user
-    redirect_to root_path, alert: 'You are not authorized to perform this action.' unless current_user_has_ta_privileges?
+  def redirect_to_edit_assignment_path
+    redirect_to edit_assignment_path(duty_params[:assignment_id])
   end
 
   def redirect_to_create_page_and_show_error
-    flash[:error] = @duty.errors.full_messages.join('. ')
-    redirect_to action: :new, assignment_id: @assignment.id
+    # flash[:error] = @duty.errors.full_messages.join('. ')
+    redirect_to action: :new, id: duty_params[:assignment_id]
   end
 
   def duty_params
-    params.require(:duty).permit(:max_members_for_duty, :name)
+    params.require(:duty).permit(:assignment_id, :max_members_for_duty, :name)
   end
-  end
+end
+
+def redirect_to_url
+  "/api/v1/duties"
+end
