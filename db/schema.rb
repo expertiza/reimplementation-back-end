7498f01bee459c9c4134f2558f640b1f53704f6e
Surya Prakash Susarla <susarla.surya.prakash.1998@gmail.com>
2023-04-26 16:33:47 -0400

    revert schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_202838) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_15_185139) do
   create_table "assignments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "directory_path"
@@ -80,36 +80,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_202838) do
     t.index ["parent_id"], name: "fk_rails_4404228d2f"
   end
 
-  create_table "signed_up_teams", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.integer "preference_priority_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "signup_topic_id", null: false
-    t.bigint "team_id", null: false
-    t.boolean "is_waitlisted", default: false
-    t.index ["signup_topic_id"], name: "index_signed_up_teams_on_signup_topic_id"
-    t.index ["team_id"], name: "index_signed_up_teams_on_team_id"
-  end
-
-  create_table "signup_topics", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.integer "max_choosers"
-    t.string "category"
-    t.string "topic_identifier"
-    t.string "description"
-    t.string "link"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "assignment_id", null: false
-    t.index ["assignment_id"], name: "index_signup_topics_on_assignment_id"
-  end
-
-  create_table "teams", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
@@ -118,24 +88,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_202838) do
     t.string "email"
     t.integer "parent_id"
     t.string "mru_directory_path"
-    t.boolean "email_on_review", default: false
-    t.boolean "email_on_submission", default: false
-    t.boolean "email_on_review_of_review", default: false
-    t.boolean "is_new_user", default: true
-    t.boolean "master_permission_granted", default: false
+    t.boolean "email_on_review"
+    t.boolean "email_on_submission"
+    t.boolean "email_on_review_of_review"
+    t.boolean "is_new_user"
+    t.boolean "master_permission_granted"
     t.string "handle"
     t.string "persistence_token"
     t.string "timezonepref"
-    t.boolean "copy_of_emails", default: false
+    t.boolean "copy_of_emails"
     t.integer "institution_id"
-    t.boolean "etc_icons_on_homepage", default: false
+    t.boolean "etc_icons_on_homepage"
     t.integer "locale"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "roles", "roles", column: "parent_id", on_delete: :cascade
-  add_foreign_key "signed_up_teams", "signup_topics"
-  add_foreign_key "signed_up_teams", "teams"
-  add_foreign_key "signup_topics", "assignments"
 end
