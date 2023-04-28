235938f208f4514841d727a629f0d2e95a133097
Kunal Govind Patil <kunal@Kunals-Air.lan>
2023-04-28 01:47:19 -0400

    fixed duties test

--- spec/requests/api/v1/duties_spec.rb
+++ spec/requests/api/v1/duties_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'api/v1/duties', type: :request do
   path '/api/v1/duties' do
 
     get('list duties') do
+      tags 'Duties'
       response(200, 'successful') do
 
         after do |example|
@@ -19,8 +20,21 @@ RSpec.describe 'api/v1/duties', type: :request do
     end
 
     post('create duty') do
-      response(200, 'successful') do
+      tags 'Duties'
+      description 'Create a new duty'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :duty, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          max_members_for_duty: { type: :integer },
+          assignment_id: { type: :integer }
+        },
+        required: [ 'name', 'max_members_for_duty', 'assignment_id']
+      }
 
+      response(200, 'successful') do
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -38,6 +52,7 @@ RSpec.describe 'api/v1/duties', type: :request do
     parameter name: 'id', in: :path, type: :string, description: 'id'
 
     get('show duty') do
+      tags 'Duties'
       response(200, 'successful') do
         let(:id) { '123' }
 
@@ -53,8 +68,23 @@ RSpec.describe 'api/v1/duties', type: :request do
     end
 
     patch('update duty') do
+      tags 'Duties'
+      description 'Update a duty'
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :string
+      parameter name: :duty, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          max_members_for_duty: { type: :integer },
+          assignment_id: { type: :integer }
+        }
+      }
+
       response(200, 'successful') do
         let(:id) { '123' }
+        let(:duty) { { name: 'Updated Duty Name', max_members_for_duty: 2, assignment_id: 456 } }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -63,26 +93,13 @@ RSpec.describe 'api/v1/duties', type: :request do
             }
           }
         end
-        run_test!
-      end
-    end
 
-    put('update duty') do
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
         run_test!
       end
     end
 
     delete('delete duty') do
+      tags 'Duties'
       response(200, 'successful') do
         let(:id) { '123' }
 
