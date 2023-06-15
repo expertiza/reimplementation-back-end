5e8dc9492c34faf157513c28289205e1cf357a49
Ankur Mundra <20452032+mundra-ankur@users.noreply.github.com>
2023-06-15 18:42:57 -0400

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,15 @@
 class Question < ApplicationRecord
-  belongs_to :questionnaire # each question belongs to a specific questionnaire
+  before_create :set_seq
+  belongs_to :questionnaire # Each question belongs to a specific questionnaire
   
   validates :seq, presence: true, numericality: true # sequence must be numeric
   validates :txt, length: { minimum: 0, allow_nil: false, message: "can't be nil" } # user must define text content for a question
   validates :question_type, presence: true # user must define type for a question
   validates :break_before, presence: true
 
-  
+  def set_seq
+    self.seq = questionnaire.questions.size + 1
+  end
 
   def as_json(options = {})
       super(options.merge({
