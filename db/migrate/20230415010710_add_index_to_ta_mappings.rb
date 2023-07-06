381355c9a6f05bcba7b05ce5f2dd2cd26281bebf
ameyagv <ameyav1234@gmail.com>
2023-07-05 23:48:36 -0400

    combined migration files for ta_mappings

--- db/migrate/20230415010710_add_index_to_ta_mappings.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTaMappings < ActiveRecord::Migration[7.0]
-  def change
-    add_index :ta_mappings, :ta_id, name: "fk_ta_mapping_users"
-  end
-end
--- db/migrate/20230415011209_create_ta_mappings.rb
+++ db/migrate/20230415011209_create_ta_mappings.rb
@@ -2,9 +2,10 @@ class CreateTaMappings < ActiveRecord::Migration[7.0]
   def change
     create_table :ta_mappings do |t|
       t.references :course, null: false, foreign_key: true
-      t.integer :ta_id
+      t.references :ta, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end
+    add_index :ta_mappings, :ta_id, name: "fk_ta_mapping_users"
   end
 end
