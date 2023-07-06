bffef3d4deeab98180626074e5ce1e3ee1ff817b
ameyagv <ameyav1234@gmail.com>
2023-07-05 21:39:07 -0400

    combined course migration index and keys

--- db/migrate/20230415003243_create_courses.rb
+++ db/migrate/20230415003243_create_courses.rb
@@ -4,10 +4,12 @@ class CreateCourses < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :directory_path
       t.text :info
-      t.boolean :private, default:false
-      t.integer :instructor_id
+      t.boolean :private, default: false
 
       t.timestamps
     end
+    add_reference :courses, :instructor, foreign_key: { to_table: :users }, null: false
+    add_reference :courses, :institution, foreign_key: true, null: false
+    add_index :courses, :instructor, name: "fk_course_users"
   end
 end
