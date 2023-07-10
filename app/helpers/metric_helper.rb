8256ac30e3334a83499ec375293da8fbd281418f
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-07-10 01:45:30 -0400

    Update metric_helper.rb
    changed instance variables to local variables

--- app/helpers/metric_helper.rb
+++ app/helpers/metric_helper.rb
@@ -6,20 +6,20 @@ module MetricHelper
   # word count.
 
   def volume_of_review_comments(reviewer_id)
-    @comments_in_round, @counter_in_round = get_all_review_comments(reviewer_id)
-    num_rounds = @comments_in_round.count - 1 # ignore nil element (index 0)
+    comments_in_round, counter_in_round = get_all_review_comments(reviewer_id)
+    num_rounds = comments_in_round.count - 1 # ignore nil element (index 0)
 
     comments = ""
     comment_count = 0
-    @comments_in_round.each { |comment| comments += comment unless comment == nil}
-    @counter_in_round.each { |count| comment_count += count unless count == nil}
+    comments_in_round.each { |comment| comments += comment unless comment == nil}
+    counter_in_round.each { |count| comment_count += count unless count == nil}
 
     overall_avg_vol = (Lingua::EN::Readability.new(comments).num_words / (comment_count.zero? ? 1 : comment_count)).round(0)
     review_comments_volume = []
     review_comments_volume.push(overall_avg_vol)
     (1..num_rounds).each do |round|
-      num = Lingua::EN::Readability.new(@comments_in_round[round]).num_words
-      den = (@counter_in_round[round].zero? ? 1 : @counter_in_round[round])
+      num = Lingua::EN::Readability.new(comments_in_round[round]).num_words
+      den = (counter_in_round[round].zero? ? 1 : counter_in_round[round])
       avg_vol_in_round = (num / den).round(0)
       review_comments_volume.push(avg_vol_in_round)
     end
@@ -30,26 +30,26 @@ module MetricHelper
   # as well as a count of the total number of comments. Returns the
   # above information both for totals and in a list per-round.
   def get_all_review_comments(reviewer_id)
-    @comments_in_round = []
-    @counter_in_round = []
+    comments_in_round = []
+    counter_in_round = []
     question_ids = Question.get_questions_with_comments_for_assignment(id)
 
     ReviewResponseMap.where(reviewed_object_id: id, reviewer_id: reviewer_id).find_each do |response_map|
       (1..num_review_rounds + 1).each do |round|
-        @comments_in_round[round] = ''
-        @counter_in_round[round] = 0
+        comments_in_round[round] = ''
+        counter_in_round[round] = 0
         last_response_in_current_round = response_map.response.select { |r| r.round == round }.last
         next if last_response_in_current_round.nil?
 
         last_response_in_current_round.scores.each do |answer|
-          @comments_in_round[round] += (answer.comments ||= '')
+          comments_in_round[round] += (answer.comments ||= '')
         end
         additional_comment = last_response_in_current_round.additional_comment
-        @comments_in_round[round] += additional_comment
-        @counter_in_round[round] += 1
+        comments_in_round[round] += additional_comment
+        counter_in_round[round] += 1
       end
     end
-    [@comments_in_round, @counter_in_round]
+    [comments_in_round, counter_in_round]
   end
 
 end
