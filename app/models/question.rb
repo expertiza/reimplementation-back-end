56e3b6a37e730ab1d6c090874369806703b01fcb
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-08 13:51:17 -0400

    removed get_questions_with_comments_for_assignment

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,6 @@
 class Question < ApplicationRecord
     belongs_to :questionnaire # each question belongs to a specific questionnaire
     has_many :answers, dependent: :destroy
-  
-    def self.get_questions_with_comments_for_assignment(assignment_id)
-      question_ids = []
-      questionnaires = Assignment.find(assignment_id).questionnaires.select { |questionnaire| questionnaire.type == 'ReviewQuestionnaire' }
-      questionnaires.each do |questionnaire|
-        questions = questionnaire.questions.select { |question| question.is_a?(ScoredQuestion) || question.instance_of?(TextArea) }
-        questions.each { |question| question_ids << question.id }
-      end
-      question_ids
-    end
 
     def scorable?
       false
