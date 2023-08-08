8b5eca2510c98c54a6e50589638958c277227385
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-08 13:57:50 -0400

    removed test for get_all_responses

--- spec/models/response_map_spec.rb
+++ spec/models/response_map_spec.rb
@@ -1,18 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ResponseMap, type: :model do
-  let(:team) {Team.new}
-  let(:participant) { Participant.new(id: 1) }
-  let(:assignment) { Assignment.new(id: 1, name: 'Test Assignment') }
-
-  describe ".get_all_responses" do
-    it "returns all responses by a particular reviewer" do
-      response_map = ReviewResponseMap.create!(assignment: assignment, reviewer: participant, reviewee: team)
-
-      response1 = Response.create!(response_map: response_map)
-      response2 = Response.create!(response_map: response_map)
-
-      expect(response_map.get_all_responses).to eq([response1, response2])
-    end
-  end
 end
