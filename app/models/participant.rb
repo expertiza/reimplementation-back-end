a4ac23140f0d18a5ab037d72d1040913503e0457
Ameya Vaichalkar <57044378+ameyagv@users.noreply.github.com>
2023-08-08 12:29:29 -0400

    fixed method fullname in Participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,7 +2,7 @@ class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :assignment, foreign_key: 'assignment_id', inverse_of: false
 
-  def fullname(ip_address = nil)
-    user.fullname(ip_address)
+  def fullname
+    user.fullname
   end
 end
