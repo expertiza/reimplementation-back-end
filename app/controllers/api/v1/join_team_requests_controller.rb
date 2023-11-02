6aeb4340d3d4737639c695c5a0d8782c7a1950ac
papatil <papatil@ncsu.edu>
2023-11-02 14:30:08 -0400

    Removed commeneted TODO lines

--- app/controllers/api/v1/join_team_requests_controller.rb
+++ app/controllers/api/v1/join_team_requests_controller.rb
@@ -1,13 +1,10 @@
 class Api::V1::JoinTeamRequestsController < ApplicationController
   # Constants used to indicate status for the request
-  # The datatype for status is string
-  PENDING = 'P'
-  DECLINED = 'D'
-  ACCEPTED = 'A'
+  PENDING = 'PENDING'
+  DECLINED = 'DECLINED'
+  ACCEPTED = 'ACCEPTED'
 
-
-  # TODO Once the check_team_status is implemented below, uncomment this line
-  # before_action :check_team_status, only: [:create]
+  before_action :check_team_status, only: [:create]
   before_action :find_request, only: %i[show update destroy decline]
 
   def action_allowed?
@@ -40,11 +37,10 @@ class Api::V1::JoinTeamRequestsController < ApplicationController
 
     participant = Participant.where(user_id: @current_user.id, assignment_id: params[:assignment_id]).first
 
-    # TODO Uncomment this code once team model and controller is implemented such that teams have participants
-    # team = Team.find(params[:team_id])
-    # if team.participants.include?(participant)
-    #   render json: { error: 'You already belong to the team' }, status: :unprocessable_entity
-    # else
+    team = Team.find(params[:team_id])
+    if team.participants.include?(participant)
+      render json: { error: 'You already belong to the team' }, status: :unprocessable_entity
+    else
     if participant
       @join_team_request.participant_id = participant.id
       if @join_team_request.save
@@ -56,7 +52,7 @@ class Api::V1::JoinTeamRequestsController < ApplicationController
       render json: { errors: "Participant not found" }, status: :unprocessable_entity
     end
 
-    # end
+    end
   end
 
   # PATCH/PUT api/v1/join_team_requests/1
@@ -83,32 +79,14 @@ class Api::V1::JoinTeamRequestsController < ApplicationController
     end
   end
 
-  def accept
-    @join_team_request.status = ACCEPTED
-
-    if @join_team_request.save
-      render json: { message: 'JoinTeamRequest accepted successfully' }, status: :ok
-    else
-      render json: { errors: @join_team_request.errors.full_messages }, status: :unprocessable_entity
-    end
-  end
-
   private
 
-  # TODO Uncomment and modify this code as necessary when TeamUser controller and Teams model is complete
-  # def check_team_status
-  #
-  #   team_member = TeamsUser.where(['team_id = ? and user_id = ?', params[:team_id], session[:user][:id]])
-  #   team = Team.find(params[:team_id])
-  #
-  #   if team.full?
-  #     render json: { message: 'This team is full.' }, status: :unprocessable_entity
-  #   elsif team_member.any?
-  #     render json: { message: 'You are already a member of this team.' }, status: :unprocessable_entity
-  #   else
-  #     render json: { message: 'Team is available for joining.' }, status: :ok
-  #   end
-  # end
+  def check_team_status
+    team = Team.find(params[:team_id])
+    if team.full?
+      render json: { message: 'This team is full.' }, status: :unprocessable_entity
+    end
+  end
 
   def find_request
     @join_team_request = JoinTeamRequest.find(params[:id])
--- app/models/join_team_request.rb
+++ app/models/join_team_request.rb
@@ -2,4 +2,6 @@ class JoinTeamRequest < ApplicationRecord
   # TODO Uncomment the following line when Team and Team Controller is thoroughly implemented
   # belongs_to :team
   has_one :participant, dependent: :nullify
+  ACCEPTED_STATUSES = %w[ACCEPTED DECLINED PENDING]
+  validates :status, inclusion: { in: ACCEPTED_STATUSES }
 end
