3cb3c0be732bb9fa5f9568d68900bba20ba7b1c5
Rishab Muzhangathu <ramuzhan@ncsu.edu>
2023-11-06 14:49:06 -0500

    Code Refractoring

--- spec/controllers/badges_controller_spec.rb
+++ spec/controllers/badges_controller_spec.rb
@@ -1,130 +1,19 @@
-describe Api::V1::BadgesController do
-describe "index" do
-  it "returns all badges" do
-    # Test scenario 1: When there are no badges in the database
-    # Expected behavior: The response should contain an empty array of badges
-    # Test scenario 2: When there are multiple badges in the database
-    # Expected behavior: The response should contain all the badges in the database
-  end
+require 'swagger_helper'
 
-  it "returns a successful response status" do
+describe Api::V1::BadgesController do
+  describe "index" do
+    it "returns all badges" do
+      request.headers['Authorization'] = 'eyJhbGciOiJSUzI1NiJ9.eyJpZCI6MSwibmFtZSI6ImFkbWluIiwiZnVsbF9uYW1lIjoiYWRtaW4gYWRtaW4iLCJyb2xlIjoiU3VwZXIgQWRtaW5pc3RyYXRvciIsImluc3RpdHV0aW9uX2lkIjoxLCJleHAiOjE2OTkzMjU3Mzl9.e_O2S65agUCih-klyShyfujz0GGJqzz2IcYXEPq_NOWiyd9iFR-gpdUiSqeqWuHA9XcokzEw_41HXSPLRo00xchPXUiUzEwKeZ0wL2ghrPUk07MNyC9IoDfC5ZTIDmAoZ2OgdCPSyUtVRoavkwycyb9hOVh-X6ZLorsdo_t0a7JbL2N6ut_Hcz2zJTPyBD6LNMVqsLflWQl71LGC3PxzI_yevLembFCVPqY2nR41LKmKOxmK52JcAKJx9AO_rLRtZ9vws-HVcrgHphQ-URVNg6JbgqZpaLKV_XV4iP8wlR06YUcNjx2b2hhbweeVe0y-vt6hPO0lt1fj2w1H7sF_sQ'
+      get :index
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to be_empty
+    end
+
+    it "returns a successful response status" do
     # Test scenario 1: When the request is successful
     # Expected behavior: The response status should be 200 (OK)
     # Test scenario 2: When there is an error in retrieving the badges
     # Expected behavior: The response status should indicate an error
-  end
-end
-describe "new" do
-  it "creates a new badge" do
-    # Test body
-  end
-
-  it "returns the created badge as JSON" do
-    # Test body
-  end
-
-  it "returns a status code of 200 (OK)" do
-    # Test body
-  end
-end
-describe "show" do
-  context "when the badge is found" do
-    it "returns the badge as JSON with a status of :ok"
-  end
-
-  context "when the badge is not found" do
-    it "returns an empty JSON response with a status of :ok"
-  end
-end
-describe "POST #create" do
-  context "when valid badge parameters are provided" do
-    it "creates a new badge" do
-    end
-
-    it "returns a JSON response with the created badge and a status of 201" do
     end
   end
-
-  context "when invalid badge parameters are provided" do
-    it "does not create a new badge" do
-    end
-
-    it "returns a JSON response with the badge errors and a status of 422" do
-    end
-  end
-end
-describe "#update" do
-  context "when badge is successfully updated" do
-    it "returns a JSON response with the updated badge and status code 200"
-  end
-
-  context "when badge update fails" do
-    it "returns a JSON response with the error messages and status code 422"
-  end
-end
-describe "#destroy" do
-  it "destroys the badge" do
-    # Test scenario 1: Badge is successfully destroyed
-    # Test scenario 2: Badge is not found and cannot be destroyed
-    # Test scenario 3: Badge is already destroyed and cannot be destroyed again
-  end
-
-  it "renders a JSON response with a success message" do
-    # Test scenario 1: JSON response contains a success message after badge is destroyed
-    # Test scenario 2: JSON response contains a success message when badge is not found
-    # Test scenario 3: JSON response contains a success message when badge is already destroyed
-  end
-
-  it "returns a status code of 200 (OK)" do
-    # Test scenario 1: Status code is 200 when badge is successfully destroyed
-    # Test scenario 2: Status code is 200 when badge is not found
-    # Test scenario 3: Status code is 200 when badge is already destroyed
-  end
-end
-describe "set_badge" do
-  context "when a valid badge ID is provided" do
-    it "finds the corresponding badge" do
-      # Test body
-    end
-  end
-
-  context "when an invalid badge ID is provided" do
-    it "returns an error message" do
-      # Test body
-    end
-  end
-end
-describe "set_return_to" do
-  context "when session[:return_to] is not set" do
-    it "sets session[:return_to] to the value of request.referer" do
-      # Test body
-    end
-  end
-
-  context "when session[:return_to] is already set" do
-    it "does not change the value of session[:return_to]" do
-      # Test body
-    end
-  end
-end
-describe "badge_params" do
-  context "when valid parameters are provided" do
-    it "returns the permitted parameters for a badge" do
-      # Test code here
-    end
-  end
-
-  context "when required parameters are missing" do
-    it "raises an error" do
-      # Test code here
-    end
-  end
-
-  context "when additional parameters are provided" do
-    it "returns only the permitted parameters for a badge" do
-      # Test code here
-    end
-  end
-end
-
 end
