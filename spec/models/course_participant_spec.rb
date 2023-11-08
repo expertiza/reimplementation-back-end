6a0db963213f38393082c4ba40ad157fdb30898a
Janhavi-23 <janhavip1231@gmail.com>
2023-11-07 22:31:00 -0500

    Added test cases for course_participant

--- spec/models/course_participant_spec.rb
+++ spec/models/course_participant_spec.rb
@@ -1,139 +1,137 @@
-describe CourseParticipant do
-describe "#copy" do
-  context "when the assignment participant does not exist" do
-    it "creates a new assignment participant with the given assignment_id" do
-      # Test code here
-    end
+require 'rails_helper'
 
-    it "sets a handle for the new assignment participant" do
-      # Test code here
-    end
+RSpec.describe CourseParticipant, type: :model do
+  let(:course) { build(:course, id: 1, name: 'ECE517') }
+  let(:assignment) { Assignment.new(id: 1, name: 'Test Assignment') }
+  let(:user) { User.new(id:1, name: 'abc')}
 
-    it "returns the newly created assignment participant" do
-      # Test code here
-    end
-  end
+  describe "#copy" do
+    context "when the assignment participant does not exist" do
+      it "creates a new assignment participant with the given assignment_id" do
+        new_participant = CourseParticipant.copy(assignment_id: assignment.id)
+        expect(new_participant).to be_an(AssignmentParticipant)
+      end
 
-  context "when the assignment participant already exists" do
-    it "does not create a new assignment participant" do
-      # Test code here
-    end
+      it "sets a handle for the new assignment participant" do
+        new_participant = CourseParticipant.copy(assignment_id: assignment.id)
+        expect(new_participant.handle).to_not be_nil
+      end
 
-    it "returns nil" do
-      # Test code here
+      it "returns the newly created assignment participant" do
+        new_participant = CourseParticipant.copy(assignment_id: assignment.id)
+        expect(new_participant.assignment_id).to eq(assignment.id)
+      end
     end
-  end
-end
-describe ".import" do
-  context "when user id is not specified" do
-    it "raises an ArgumentError" do
-      # Test code
-    end
-  end
+    context "when the assignment participant already exists" do
+      it "does not create a new assignment participant" do
+        existing_participant = AssignmentParticipant.create(user_id: user.id, parent_id: assignment.id)
+        allow(AssignmentParticipant).to receive(:find_or_create_by).and_return(existing_participant)
 
-  context "when user is not found" do
-    it "raises an ArgumentError if the record does not have enough items" do
-      # Test code
-    end
+        new_participant = CourseParticipant.copy(assignment.id)
+        expect(new_participant).to eq(existing_participant)
+      end
 
-    it "creates a new user with the specified attributes" do
-      # Test code
+      it "returns nil" do
+        # Test code here  
+      end
     end
   end
+  describe ".import" do
+    context "when user id is not specified" do
+      it "raises an ArgumentError" do
+        # Test code  
+      end
+    end
+    context "when user is not found" do
+      it "raises an ArgumentError if the record does not have enough items" do
+        # Test code  
+      end
 
-  context "when course is not found" do
-    it "raises an ImportError" do
-      # Test code
+      it "creates a new user with the specified attributes" do
+        # Test code  
+      end
+    end
+    context "when course is not found" do
+      it "raises an ImportError" do
+        # Test code  
+      end
+    end
+    context "when course participant does not exist" do
+      it "creates a new course participant with the specified user and course" do
+        # Test code  
+      end
     end
   end
-
-  context "when course participant does not exist" do
-    it "creates a new course participant with the specified user and course" do
-      # Test code
+  describe "course_string" do
+    context "when there is no course associated with the assignment" do
+      it "returns a dash surrounded by center tags" do
+        # test body  
+      end
+    end
+    context "when the course associated with the assignment has an empty title" do
+      it "returns a dash surrounded by center tags" do
+        # test body  
+      end
+    end
+    context "when the course associated with the assignment has a title with no printing characters" do
+      it "returns a dash surrounded by center tags" do
+        # test body  
+      end
+    end
+    context "when the course associated with the assignment has a valid title" do
+      it "returns the course name" do
+        # test body  
+      end
     end
   end
-end
-describe "course_string" do
-  context "when there is no course associated with the assignment" do
-    it "returns a dash surrounded by center tags" do
-      # test body
+  describe "#path" do
+    context "when the parent course exists" do
+      it "returns the path of the parent course concatenated with the directory number" do
+        # Test scenario 1  
+      end
+    end
+    context "when the parent course does not exist" do
+      it "raises an error" do
+        # Test scenario 2  
+      end
     end
   end
-
-  context "when the course associated with the assignment has an empty title" do
-    it "returns a dash surrounded by center tags" do
-      # test body
+  describe ".export" do
+    context "when personal_details option is true" do
+      it "exports user's name, fullname, and email" do
+        # Test scenario here  
+      end
+    end
+    context "when role option is true" do
+      it "exports user's role name" do
+        # Test scenario here  
+      end
+    end
+    context "when parent option is true" do
+      it "exports user's parent name" do
+        # Test scenario here  
+      end
+    end
+    context "when email_options option is true" do
+      it "exports user's email_on_submission, email_on_review, and email_on_review_of_review" do
+        # Test scenario here  
+      end
+    end
+    context "when handle option is true" do
+      it "exports part's handle" do
+        # Test scenario here  
+      end
     end
   end
-
-  context "when the course associated with the assignment has a title with no printing characters" do
-    it "returns a dash surrounded by center tags" do
-      # test body
+  describe '.export_fields' do
+    context 'when options are empty' do
+      it 'returns the export fields for User with default options' do
+        # Test code  
+      end
     end
-  end
-
-  context "when the course associated with the assignment has a valid title" do
-    it "returns the course name" do
-      # test body
-    end
-  end
-end
-describe "#path" do
-  context "when the parent course exists" do
-    it "returns the path of the parent course concatenated with the directory number" do
-      # Test scenario 1
-    end
-  end
-
-  context "when the parent course does not exist" do
-    it "raises an error" do
-      # Test scenario 2
-    end
-  end
-end
-describe ".export" do
-  context "when personal_details option is true" do
-    it "exports user's name, fullname, and email" do
-      # Test scenario here
-    end
-  end
-
-  context "when role option is true" do
-    it "exports user's role name" do
-      # Test scenario here
-    end
-  end
-
-  context "when parent option is true" do
-    it "exports user's parent name" do
-      # Test scenario here
-    end
-  end
-
-  context "when email_options option is true" do
-    it "exports user's email_on_submission, email_on_review, and email_on_review_of_review" do
-      # Test scenario here
-    end
-  end
-
-  context "when handle option is true" do
-    it "exports part's handle" do
-      # Test scenario here
-    end
-  end
-end
-describe '.export_fields' do
-  context 'when options are empty' do
-    it 'returns the export fields for User with default options' do
-      # Test code
-    end
-  end
-
-  context 'when options are provided' do
-    it 'returns the export fields for User with the provided options' do
-      # Test code
-    end
-  end
-end
-
+    context 'when options are provided' do
+      it 'returns the export fields for User with the provided options' do
+        # Test code  
+      end
+    end  end
 end
