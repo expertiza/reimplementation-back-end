dd6562d30c038fc66e25e08895d0f0be88bb7c52
parallels <scjaje@gmail.com>
2023-11-29 15:07:06 -0500

    Create show route test

--- spec/controllers/student_quizzes_controller_spec.rb
+++ spec/controllers/student_quizzes_controller_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Api::V1::StudentQuizzesController, type: :controller do
   let(:student) { create(:student) }       # Creates a student user
   let(:instructor) { create(:instructor) } # Creates an instructor user
   let(:auth_token) { 'mocked_auth_token' } # mock a token
+  let(:questionnaire) do # mock questionnaires
+    course = create(:course, instructor: instructor)
+    assignment = create(:assignment, course: course, instructor: instructor)
+    create(:questionnaire, assignment: assignment, instructor: instructor)
+  end
 
   before do
     allow_any_instance_of(Api::V1::StudentQuizzesController)
@@ -21,8 +26,9 @@ RSpec.describe Api::V1::StudentQuizzesController, type: :controller do
     before do
       create(:institution)
       course = create(:course, instructor: instructor)
-      puts "Instructor role_id: #{instructor.role_id}"  # Debugging output
       assignment = create(:assignment, course: course, instructor: instructor)
+      # The 3 below refers to how many quizzes you want to create. In this case we create 3 and make sure
+      # the index returns a count of 3.
       create_list(:questionnaire, 3, assignment: assignment)
 
       get :index
@@ -38,5 +44,19 @@ RSpec.describe Api::V1::StudentQuizzesController, type: :controller do
     end
   end
 
-  # Additional tests for other actions can be added here...
+  describe 'GET #show' do
+    before do
+      get :show, params: { id: questionnaire.id }
+    end
+
+    it 'returns a success response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns the requested questionnaire data' do
+      json_response = JSON.parse(response.body)
+      expect(json_response['id']).to eq(questionnaire.id)
+    end
+  end
+
 end
