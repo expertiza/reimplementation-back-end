bfc5247f8a91f33a8276beeafb5a1f8255bc1c20
Sachin R <32716246+sachindoddaguni@users.noreply.github.com>
2023-12-02 14:22:08 -0500

    Adding rspec test cases for find_by_login method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,40 +2,38 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # describe ".find_by_login" do
-  #   context "when login is an email" do
-  #     it "returns the user with the matching email" do
-  #       # Test scenario 1
-  #       # Given an existing user with email "test@example.com"
-  #       # When calling .find_by_login("test@example.com")
-  #       # Then it should return the user with email "test@example.com"
-  #
-  #       # Test scenario 2
-  #       # Given no user with email "test@example.com" exists
-  #       # When calling .find_by_login("test@example.com")
-  #       # Then it should return nil
-  #     end
-  #   end
-  #
-  #   context "when login is not an email" do
-  #     it "returns the user with the matching name" do
-  #       # Test scenario 3
-  #       # Given an existing user with name "john_doe"
-  #       # When calling .find_by_login("john_doe@example.com")
-  #       # Then it should return the user with name "john_doe"
-  #
-  #       # Test scenario 4
-  #       # Given multiple users with name "john_doe" exist
-  #       # When calling .find_by_login("john_doe@example.com")
-  #       # Then it should return the first user with name "john_doe"
-  #
-  #       # Test scenario 5
-  #       # Given no user with name "john_doe" exists
-  #       # When calling .find_by_login("john_doe@example.com")
-  #       # Then it should return nil
-  #     end
-  #   end
-  # end
+  describe '.find_by_login' do
+    context 'when login is an email' do
+      let!(:user) { create(:user, name: 'testname', email: 'test@test.com') }
+      it 'returns the user with the matching email' do
+        result = User.find_by_login('test@test.com')
+        expect(result).to eq(user)
+      end
+
+      it 'returns nil when user with email doesnt exist' do
+        result = User.find_by_login('test@unknown.com')
+        expect(result).to eq(nil)
+      end
+    end
+
+    context 'when login is not an email' do
+      let!(:user) { create(:user, name: 'testname', email: 'test@test.com') }
+      let!(:user2) { build(:user, name: 'testname', email: 'test@test2.com') }
+      it 'returns the user with the matching name' do
+        result = User.find_by_login('testname')
+        expect(result).to eq(user)
+      end
+      it 'return first user with the matching name' do
+        user2.save
+        result = User.find_by_login('testname')
+        expect(result).to eq(user)
+      end
+      it 'return nil when no user with matching name' do
+        result = User.find_by_login('unknown')
+        expect(result).to eq(nil)
+      end
+    end
+  end
 
   # describe '.search_users' do
   #   context 'when searching by name' do
