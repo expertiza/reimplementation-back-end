08ea560f8a9de023d90bfc13a79321e63b6d5b37
Rishi Dange <radange@ncsu.edu>
2023-12-02 15:32:18 -0500

    Pushing models and tests for scale, dropdown and criterion type questions

--- app/models/criterion.rb
+++ app/models/criterion.rb
@@ -1,74 +1,64 @@
 # frozen_string_literal: true
 
 class Criterion < ScoredQuestion
-
   attr_accessor :formatted_question_type
+
   def edit(count)
-    edit_html = "<div class=\"question\">"
-    edit_html += delete_link(count) + sequence_input(count)
-    edit_html += text_area_field(count) + disabled_type_field
-    edit_html += weight_input(count) + size_input(count)
-    edit_html += labels_input(count) + "</div>"
-    edit_html.html_safe
+    {
+      question_id: id,
+      delete_link: delete_link(count),
+      sequence_input: sequence_input(count),
+      text_area_field: text_area_field(count),
+      disabled_type_field: disabled_type_field,
+      weight_input: weight_input(count),
+      size_input: size_input(count),
+      labels_input: labels_input(count)
+    }.to_json
   end
 
   def view_question_text
-    if max_label && min_label
-      "<TR><TD align=\"left\">#{txt}</TD><TD align=\"left\">#{formatted_question_type}</TD><td align=\"center\">#{weight}</td><TD align=\"center\">(#{min_label}) 0 to 10 (#{max_label})</TD></TR>"
-    else
-      "<TR><TD align=\"left\">#{txt}</TD><TD align=\"left\">#{formatted_question_type}</TD><td align=\"center\">#{weight}</td><TD align=\"center\">0 to 10</TD></TR>"
-    end
+    {
+      formatted_text: formatted_text,
+      type: formatted_question_type,
+      weight: weight,
+      score_range: score_range
+    }.to_json
   end
 
   def complete(dropdown_or_scale)
-    html = "<label>#{txt}</label>"
-    html += if dropdown_or_scale == 'dropdown'
-              dropdown_options
-            else
-              scale_options
-            end
-    html.html_safe
+    options = dropdown_or_scale == 'dropdown' ? dropdown_options : scale_options
+    { label: txt, options: options }.to_json
   end
 
   def advices_criterion_question(count)
-    html = "<ul class=\"advices\">"
-    question_advices.each do |advice|
-      html << "<li>Advice #{count}: #{advice.txt}</li>"
-      count += 1
-    end
-    html += "</ul>"
-    html.html_safe
+    advices = question_advices.map { |advice| { advice_count: count, advice_text: advice.txt } }
+    { advices: advices }.to_json
   end
 
   def dropdown_criterion_question(answer)
-    html = "<select>"
-    html += "<option value=\"\">Select an option</option>"
-    alternatives.split('|').each do |alt|
-      selected = (alt == answer) ? 'selected="selected"' : ''
-      html += "<option value=\"#{alt}\" #{selected}>#{alt}</option>"
-    end
-    html += "</select>"
-    html.html_safe
+    options = alternatives.split('|').map { |alt| { value: alt, selected: (alt == answer) } }
+    { dropdown_options: options }.to_json
   end
 
   def scale_criterion_question
-    html = "<label>#{txt}</label><br>"
-    html += "<input type=\"range\" min=\"0\" max=\"10\" value=\"#{answer || 0}\">"
-    html.html_safe
+    { label: txt, range: { min: 0, max: 10, value: answer || 0 } }.to_json
   end
 
   def view_completed_question(count, answer, questionnaire_max)
-    completed_question_html = "<div class=\"question\">"
-    completed_question_html += "<div class=\"question-text\">#{txt}</div>"
-    completed_question_html += "<div class=\"question-type\">#{formatted_question_type}</div>"
-    completed_question_html += "<div class=\"question-weight\">#{weight}</div>"
-    completed_question_html += "<div class=\"question-score\">(#{min_label}) #{answer} to #{questionnaire_max} (#{max_label})</div>"
-    completed_question_html += "</div>"
-    completed_question_html.html_safe
+    {
+      question: formatted_text,
+      type: formatted_question_type,
+      weight: weight,
+      score: "(#{min_label}) #{answer} to #{questionnaire_max} (#{max_label})"
+    }.to_json
   end
 
-  # Helper methods
   private
+
+  def formatted_text
+    "<div class=\"question\">#{delete_link(count)}#{sequence_input(count)}"\
+      "#{text_area_field(count)}#{disabled_type_field}#{weight_input(count)}#{size_input(count)}#{labels_input(count)}</div>".html_safe
+  end
 
   def delete_link(count)
     "<a href=\"javascript:void(0);\" onclick=\"delete_question(#{count})\">delete</a>"
@@ -95,22 +85,20 @@ class Criterion < ScoredQuestion
   end
 
   def labels_input(count)
-    "<input type=\"text\" name=\"question[#{count}][min_label]\" value=\"#{min_label}\">
-     <input type=\"text\" name=\"question[#{count}][max_label]\" value=\"#{max_label}\">"
+    "<input type=\"text\" name=\"question[#{count}][min_label]\" value=\"#{min_label}\">"\
+      "<input type=\"text\" name=\"question[#{count}][max_label]\" value=\"#{max_label}\">"
+  end
+
+  def score_range
+    max_label && min_label ? "(#{min_label}) 0 to 10 (#{max_label})" : '0 to 10'
   end
 
   def dropdown_options
-    html = "<select>"
-    html += "<option value=\"\">Select an option</option>"
-    alternatives.split('|').each do |alt|
-      html << "<option value=\"#{alt}\">#{alt}</option>"
-    end
-    html += "</select>"
-    html.html_safe
+    options = alternatives.split('|').map { |alt| { value: alt } }
+    { dropdown_options: options }.to_json
   end
 
   def scale_options
-    html = "<label>0</label><input type=\"range\" min=\"0\" max=\"10\" value=\"0\"><label>10</label>"
-    html.html_safe
+    { range: { min: 0, max: 10, value: 0 } }.to_json
   end
 end
--- app/models/dropdown.rb
+++ app/models/dropdown.rb
@@ -1,54 +1,33 @@
 # frozen_string_literal: true
 
 class Dropdown < UnscoredQuestion
-
   def edit(count)
-    html = '<div>'
-    html += "<label for='question_#{id}'>Question #{count}:</label>"
-    html += "<input type='text' id='question_#{id}' name='question[#{id}]' value='#{txt}' />"
-    html += '</div>'
-    html
+    {
+      question_id: id,
+      label: "Question #{count}:",
+      input_type: 'text',
+      input_id: "question_#{id}",
+      input_name: "question[#{id}]",
+      input_value: txt
+    }.to_json
   end
 
   def view_question_text
-    if txt.nil? || type.nil? || weight.nil?
-      'Invalid input values'
-    end
-
-    html = "<TR><TD align='left'> #{txt} </TD><TD align='left'>#{type}</TD><td align='center'>#{weight}</TD><TD align='center'>&mdash;</TD></TR>"
-    html
+    # Update with your specific logic for generating JSON
+    { text: txt, type: type, weight: weight, score_range: 'N/A' }.to_json
   end
 
-  def complete(count, answer = nil)    #initial ans = nil
-    html = ''
-    html += '<select>'
-    if count
-      (1..count).each do |option|
-        selected = (option == answer.to_i) ? 'selected' : ''
-        html += "<option value='#{option}' #{selected}>#{option}</option>"
-      end
-    end
-    html += '</select>'
-    html
+  def complete(count, answer = nil)
+    options = (1..count).map { |option| { value: option, selected: (option == answer.to_i) } }
+    { dropdown_options: options }.to_json
   end
 
   def complete_for_alternatives(alternatives, answer)
-    html = '<select>'
-    alternatives.each do |option|
-      selected = (option == answer) ? 'selected' : ''
-      html += "<option value='#{option}' #{selected}>#{option}</option>"
-    end
-    html += '</select>'
-    html
+    options = alternatives.map { |alt| { value: alt, selected: (alt == answer) } }
+    { dropdown_options: options }.to_json
   end
 
   def view_completed_question
-    html = ''
-    if count && answer
-      html = "Selected option: #{answer} (out of #{count})"
-    else
-      html = "Question not answered."
-    end
-    return html
+    { selected_option: (count && answer) ? "#{answer} (out of #{count})" : 'Question not answered.' }.to_json
   end
 end
