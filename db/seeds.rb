a4580aab63a04797836b7ce1c14563edb7789881
Deana Franks <dlfranks@ncsu.edu>
2024-02-27 23:03:43 -0500

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+#seeds file
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,69 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# institution = Institution.create!(
+#   name: 'NC State'
+# )
+
+# db/seeds.rb
+
+# Find or create the Institution
+institution = Institution.find_or_create_by(name: 'NC State')
+
+# Find or create the admin user
+admin = User.find_or_create_by(name: 'admin') do |user|
+  user.update!(
+    password_digest: BCrypt::Password.create('admin'), # Hashed password
+    full_name: 'Admin Admin',
+    email: 'admin.admin@example.com',
+    mru_directory_path: '/path/to/directory',
+    email_on_review: true,
+    email_on_submission: false,
+    email_on_review_of_review: true,
+    is_new_user: true,
+    master_permission_granted: false,
+    handle: 'admin123',
+    persistence_token: 'token123',
+    timeZonePref: 'UTC',
+    copy_of_emails: false,
+    etc_icons_on_homepage: true,
+    locale: 1,
+    role_id: 2,
+    institution: institution
+  )
+end
+
+# Find or create the instructor user
+instructor = User.find_or_create_by(name: 'instructor') do |user|
+  user.update!(
+    password_digest: BCrypt::Password.create('instructor'), # Hashed password
+    full_name: 'Instructor Abc',
+    email: 'instructor.ins@example.com',
+    mru_directory_path: '/path/to/directory',
+    email_on_review: true,
+    email_on_submission: false,
+    email_on_review_of_review: true,
+    is_new_user: true,
+    master_permission_granted: false,
+    handle: 'instruct',
+    persistence_token: 'token123',
+    timeZonePref: 'UTC',
+    copy_of_emails: false,
+    etc_icons_on_homepage: true,
+    locale: 1,
+    role_id: 3,
+    institution: institution
+  )
+end
+
+# Find or create the course
+OODD = Course.find_or_create_by(name: 'Object Oriented Design and Development') do |course|
+  course.update!(
+    directory_path: '/programming101',
+    info: 'This is an introductory course on Design Patterns.',
+    private: false,
+    instructor: instructor,
+    institution: institution
+  )
+end
