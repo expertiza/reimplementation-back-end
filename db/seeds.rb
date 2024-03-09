e6f446f20d17f2b5691d1a1bbda7c971f9e89f1b
Ali Qureshi <qureshi.ali@outlook.com>
2024-03-08 23:19:23 -0500

    Updating setup script with working seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# Create an admin user
-admin_user = User.create!(
-  name: 'admin',
-  email: 'admin2@example.com',
-  password: 'password123',
-  full_name: 'admin admin',
-  institution_id: 1
-)
+begin
+    #Create an instritution
+    Institution.create!(
+      name: 'North Carolina State University',
+    )
+    
+    # Create an admin user
+    User.create!(
+      name: 'admin',
+      email: 'admin2@example.com',
+      password: 'password123',
+      full_name: 'admin admin',
+      institution_id: 1,
+      role_id: 1
+    )
+rescue ActiveRecord::RecordInvalid => e
+    puts 'The db has already been seeded'
+end
