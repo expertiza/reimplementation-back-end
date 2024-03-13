1b6174bbe0930d03bbf4d231fdcf104723b3a962
Deana Franks <dlfranks@ncsu.edu>
2024-03-12 23:39:32 -0400

    Added associations to TeamsUser, SignUpTopic, and SignedUpTeam

--- app/models/assignment_questionnaire.rb
+++ app/models/assignment_questionnaire.rb
@@ -1,4 +1,5 @@
 class AssignmentQuestionnaire < ApplicationRecord
   belongs_to :assignment
   belongs_to :questionnaire
+  belongs_to :sign_up_topic
 end
--- app/models/signed_up_team.rb
+++ app/models/signed_up_team.rb
@@ -1,2 +1,4 @@
 class SignedUpTeam < ApplicationRecord
+  belongs_to :topic, class_name: 'SignUpTopic'
+  belongs_to :team, class_name: 'Team'
 end
