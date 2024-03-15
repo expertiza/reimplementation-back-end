0a75f19412ef79300734cd84803fa6a408ee9dd1
vishwagandhi1610 <vgandhi@ncsu.edu>
2024-03-15 19:37:46 +0000

    added question_advice.rb and criterion.rb model and criterion_rspec.rb

---
+++ app/models/question_advice.rb
@@ -0,0 +1,22 @@
+class QuestionAdvice < ApplicationRecord
+  belongs_to :question
+  def self.export_fields(_options)
+    QuestionAdvice.columns.map(&:name)
+  end
+
+  def self.export(csv, parent_id, _options)
+    questionnaire = Questionnaire.find(parent_id)
+    questionnaire.questions.each do |question|
+      QuestionAdvice.where(question_id: question.id).each do |advice|
+        csv << advice.attributes.values
+      end
+    end
+  end
+
+  def self.to_json_by_question_id(question_id)
+    question_advices = QuestionAdvice.where(question_id: question_id).order(:id)
+    question_advices.map do |advice|
+      { score: advice.score, advice: advice.advice }
+    end
+  end
+end
