1ca2dd4568f50d8429d6773d7bc7dc2e090a2ba7
Deana Franks <dlfranks@ncsu.edu>
2024-03-21 01:52:54 -0400

    Removed instance variables in the EmailObject.

--- app/mailers/email_object.rb
+++ app/mailers/email_object.rb
@@ -2,9 +2,9 @@ class EmailObject
   attr_accessor :subject, :body, :from, :to
 
   def initialize(to = nil, from = nil, subject = nil, body = nil)
-    @subject = subject
-    @body = body
-    @from = from
-    @to = to
+    self.subject = subject
+    self.body = body
+    self.from = from
+    self.to = to
   end
 end
