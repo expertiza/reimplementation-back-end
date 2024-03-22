fc2052aac01df0369ec21a1e6a2539e73120af29
prathyu99 <prathyu13579@gmail.com>
2024-03-22 18:10:21 -0400

    added assignment id to questionnaire controller and added required associations in question model classes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,28 +1,38 @@
 class Question < ApplicationRecord
-  before_create :set_seq
+  before_validation :set_seq, on: :create
   belongs_to :questionnaire # each question belongs to a specific questionnaire
   has_many :answers, dependent: :destroy
-  
-  validates :seq, presence: true, numericality: true # sequence must be numeric
+  accepts_nested_attributes_for :answers # Allows nested attributes for answers
+
+
   validates :txt, length: { minimum: 0, allow_nil: false, message: "can't be nil" } # user must define text content for a question
   validates :question_type, presence: true # user must define type for a question
-  validates :break_before, presence: true
+  validates :break_before, inclusion: { in: [true, false] }
+  validates :correct_answer, presence: true
+
 
   def scorable?
     false
   end
-    
-  def set_seq
-    self.seq = questionnaire.questions.size + 1
-  end
 
   def as_json(options = {})
-      super(options.merge({
-                            only: %i[txt weight seq question_type size alternatives break_before min_label max_label created_at updated_at],
-                            include: {
-                              questionnaire: { only: %i[name id] }
-                            }
-                          })).tap do |hash|
-      end
+    super(options.merge({
+                          only: %i[txt weight seq question_type size alternatives break_before min_label max_label created_at updated_at],
+                          include: {
+                            questionnaire: { only: %i[name id] }
+                          }
+                        })).tap do |hash|
+    end
   end
+
+
+  private
+
+  def set_seq
+    if questionnaire.present?
+      max_seq = questionnaire.questions.maximum(:seq)
+      self.seq = max_seq.to_i + 1
+    end
+  end
+
 end
