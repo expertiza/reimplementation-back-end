ca807535f35e25a0c46e76d58a53b0a8b9bfdcfb
Ashaka Mehta <ashakamehta8@gmail.com>
2024-03-23 18:53:12 -0400

    add action_allowed method in submitted content controller

---
+++ app/controllers/api/v1/submitted_content_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SubmittedContentController < ApplicationController
+    require 'mimemagic'
+    require 'mimemagic/overlay'
+  
+    include AuthorizationHelper
+  
+    def action_allowed?
+      case params[:action]
+      when 'edit'
+        current_user_has_student_privileges? &&
+          are_needed_authorizations_present?(params[:id], 'reader', 'reviewer')
+      when 'submit_file', 'submit_hyperlink'
+        current_user_has_student_privileges? &&
+          one_team_can_submit_work?
+      else
+        current_user_has_student_privileges?
+      end
+    end
+end  
