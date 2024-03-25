344578343582dfd024554737a4b7677764f63235
Ashaka Mehta <ashakamehta8@gmail.com>
2024-03-24 21:03:19 -0400

    remove unwanted changes from sign up sheet, sign upmtem, signed up team models

--- app/models/signed_up_team.rb
+++ app/models/signed_up_team.rb
@@ -1,14 +1,4 @@
 class SignedUpTeam < ApplicationRecord
-  belongs_to :topic, class_name: 'SignUpTopic'
-  belongs_to :team, class_name: 'Team'
-
-  # the below has been added to make is consistent with the database schema
   validates :topic_id, :team_id, presence: true
   scope :by_team_id, ->(team_id) { where('team_id = ?', team_id) }
-
-  def self.find_team_users(assignment_id, user_id)
-    TeamsUser.joins('INNER JOIN teams ON teams_users.team_id = teams.id')
-             .select('teams.id as t_id')
-             .where('teams.parent_id = ? and teams_users.user_id = ?', assignment_id, user_id)
-  end
 end
