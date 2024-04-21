3688a1343d4053ce0267f62a87f6205ef7321e3c
hasini <hasinichenchala2@gmail.com>
2024-04-21 01:03:44 -0400

    AddingUsedInRoundToAQ Migration File

---
+++ db/migrate/20240420163053_add_used_in_round_to_assignment_questionnaires.rb
@@ -0,0 +1,9 @@
+class AddUsedInRoundToAssignmentQuestionnaires < ActiveRecord::Migration[7.0]
+  def self.up
+    add_column 'assignment_questionnaires', 'used_in_round', :integer
+  end
+
+  def self.down
+    remove_column 'assignment_questionnaires', 'used_in_round'
+  end
+end
