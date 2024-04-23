6856970b5b71db1286bd5022e7151ae04c0c5543
Jeff Riehle <jriehle@ncsu.edu>
2024-04-23 00:19:10 -0400

    added show_calibration_results_for_student

--- app/models/assignment_questionnaire.rb
+++ app/models/assignment_questionnaire.rb
@@ -2,4 +2,10 @@ class AssignmentQuestionnaire < ApplicationRecord
   belongs_to :assignment
   belongs_to :questionnaire
   belongs_to :sign_up_topic, class_name: 'SignUpTopic', foreign_key: 'topic_id'
+  
+  def self.get_questions_by_assignment_id(assignment_id)
+    AssignmentQuestionnaire.find_by(['assignment_id = ? and questionnaire_id IN (?)',
+                                     Assignment.find(assignment_id).id, ReviewQuestionnaire.select('id')])
+                           .questionnaire.questions.reject { |q| q.is_a?(QuestionnaireHeader) }
+  end
 end
