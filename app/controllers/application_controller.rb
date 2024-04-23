a408451d01f4778a52e79684062c5e7ae14e7ff4
hasini <hasinichenchala2@gmail.com>
2024-04-23 13:05:09 -0400

    Adding Auth helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::API
   include JwtToken
+  def are_needed_authorizations_present?(id, *authorizations)
+    participant = Participant.find_by(id: id)
+    return false if participant.nil?
+
+    authorization = participant.authorization
+    !authorizations.include?(authorization)
+  end
+
+  def current_user_id?(user_id)
+    current_user.try(:id) == user_id
+  end
 end
