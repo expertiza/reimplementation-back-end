4e169430e901eb269108f51872f99453cd603764
dinesh-pasupuleti <78731527+dinesh-pasupuleti@users.noreply.github.com>
2024-07-25 11:57:51 -0400

    redesigned signup  models

---
+++ app/models/sign_up_sheet.rb
@@ -0,0 +1,115 @@
+class SignUpSheet < ApplicationRecord
+    # Team lazy initialization method 
+    def self.signup_team(assignment_id, user_id, topic_id = nil)
+      # Find the team ID for the given assignment and user
+      team_id = Team.find_team_users(assignment_id, user_id)&.first&.t_id
+      # If the team doesn't exist, create a new team and assign the team ID
+      if team_id.nil?
+        team = AssignmentTeam.create_team_with_users(assignment_id, [user_id])
+        team_id = team.id
+      end
+      # Confirm the signup topic if a topic ID is provided
+      @signup_topic = SignUpTopic.find_by(id: topic_id)
+      unless @signup_topic.nil?
+        confirmation_status = @signup_topic.sign_team_up(team_id)
+      end
+      # Log the signup topic save status
+      ExpertizaLogger.info "The signup topic save status:#{confirmation_status} for assignment #{assignment_id} by #{user_id}"
+      confirmation_status
+    end
+  
+    def self.add_signup_topic(assignment_id)
+        # Retrieves and organizes due dates for all sign-up topics in a given assignment.
+        @review_rounds = Assignment.find(assignment_id).num_review_rounds
+        @topics = SignUpTopic.where(assignment_id: assignment_id)
+        @duedates = {}
+        
+        # Return the empty hash if there are no topics
+        return @duedates if @topics.nil?
+      
+        # Process each topic to retrieve and organize its due dates
+        @topics.each_with_index do |topic, i|
+          @duedates[i] = duedate = {}
+          duedate['id'] = topic.id
+          duedate['topic_identifier'] = topic.topic_identifier
+          duedate['topic_name'] = topic.topic_name
+      
+          # Process due dates for each review round
+          (1..@review_rounds).each do |round|
+            process_review_round(assignment_id, duedate, round, topic)
+          end
+      
+          # Process the metareview due date
+          deadline_type_subm = DeadlineType.find_by(name: 'metareview').id
+          duedate_subm = TopicDueDate.where(parent_id: topic.id, deadline_type_id: deadline_type_subm).first
+          subm_string = duedate_subm.nil? ? nil : DateTime.parse(duedate_subm['due_at'].to_s).strftime('%Y-%m-%d %H:%M:%S')
+          duedate['submission_' + (@review_rounds + 1).to_s] = subm_string
+        end
+      
+        # Return the organized due dates
+        @duedates
+      end
+      
+  
+    def self.has_teammate_ads?(topic_id)
+      # Checks if there are any teams advertising for partners for a specific topic that are not yet full
+      teams = Team.joins('INNER JOIN signed_up_teams ON signed_up_teams.team_id = teams.id')
+                  .select('teams.*')
+                  .where('teams.advertise_for_partner = 1 and signed_up_teams.topic_id = ?', topic_id).to_a
+      teams.reject!(&:full?)
+      teams.any?
+    end
+  
+    class << self
+      private
+  
+      def process_review_round(assignment_id, duedate, round, topic)
+        # Processes and sets the submission and review due dates for a specific review round and topic
+        duedate_rev, duedate_subm = find_topic_duedates(round, topic)
+  
+        if duedate_subm.nil? || duedate_rev.nil?
+          # the topic is new. so copy deadlines from assignment
+          set_of_due_dates = AssignmentDueDate.where(parent_id: assignment_id)
+          set_of_due_dates.each do |due_date|
+            DeadlineHelper.create_topic_deadline(due_date, 0, topic.id)
+          end
+          duedate_rev, duedate_subm = find_topic_duedates(round, topic)
+        end
+        # Format and set the submission and review due dates in the duedate hash
+        duedate['submission_' + round.to_s] = DateTime.parse(duedate_subm['due_at'].to_s).strftime('%Y-%m-%d %H:%M:%S')
+        duedate['review_' + round.to_s] = DateTime.parse(duedate_rev['due_at'].to_s).strftime('%Y-%m-%d %H:%M:%S')
+      end
+  
+      def find_topic_duedates(round, topic)
+        # Finds and returns the submission and review due dates for a specific review round and topic.
+        deadline_type_subm = DeadlineType.find_by(name: 'submission').id
+        duedate_subm = TopicDueDate.where(parent_id: topic.id, deadline_type_id: deadline_type_subm, round: round).first
+        deadline_type_rev = DeadlineType.find_by(name: 'review').id
+        duedate_rev = TopicDueDate.where(parent_id: topic.id, deadline_type_id: deadline_type_rev, round: round).first
+        [duedate_rev, duedate_subm]
+      end
+    end
+  
+    def self.import(row_hash, session, _id = nil)
+      # Imports users to a sign-up topic in an assignment based on a row from a CSV file
+      raise 'Not enough items: expect 2 or more columns: Topic Identifier, User Name 1, User Name 2, ...' if row_hash.length < 2
+  
+      imported_topic = SignUpTopic.where(topic_identifier: row_hash[:topic_identifier], assignment_id: session[:assignment_id]).first
+  
+      raise ImportError, 'Topic, ' + row_hash[:topic_identifier].to_s + ', was not found.' if imported_topic.nil?
+  
+      params = 1
+      while row_hash.length > params
+        index = 'user_name_' + params.to_s
+  
+        user = User.find_by(name: row_hash[index.to_sym].to_s)
+        raise ImportError, 'The user, ' + row_hash[index.to_sym].to_s.strip + ', was not found.' if user.nil?
+  
+        participant = AssignmentParticipant.where(parent_id: session[:assignment_id], user_id: user.id).first
+        raise ImportError, 'The user, ' + row_hash[index.to_sym].to_s.strip + ', not present in the assignment.' if participant.nil?
+  
+        signup_team(session[:assignment_id], user.id, imported_topic.id)
+        params += 1
+      end
+    end
+  end
--- app/models/sign_up_topic.rb
+++ app/models/sign_up_topic.rb
@@ -1,4 +1,232 @@
 class SignUpTopic < ApplicationRecord
-  has_many :signed_up_teams, foreign_key: 'sign_up_topic_id', dependent: :destroy
+  has_many :signed_up_teams, foreign_key: 'topic_id', dependent: :destroy
+  has_many :teams, through: :signed_up_teams # list all teams choose this topic, no matter in waitlist or not
+  has_many :due_dates, class_name: 'TopicDueDate', foreign_key: 'parent_id', dependent: :destroy
+  has_many :bids, foreign_key: 'topic_id', dependent: :destroy
+  has_many :assignment_questionnaires, class_name: 'AssignmentQuestionnaire', foreign_key: 'topic_id', dependent: :destroy
   belongs_to :assignment
+
+  has_paper_trail
+
+  # the below relations have been added to make it consistent with the database schema
+  validates :topic_name, :assignment_id, :max_choosers, presence: true
+  validates :topic_identifier, length: { maximum: 10 }
+
+  def self.import(row_hash, session, _id = nil)
+    # Imports a topic from a row in a CSV file.
+    # If the topic does not exist, it creates a new one with the provided attributes.
+    # If the topic already exists, it updates the max choosers and topic identifier.
+  
+    # Check if the row has the minimum required length
+    if row_hash.length < 3
+      raise ArgumentError, 'The CSV File expects the format: Topic identifier, Topic name, Max choosers, Topic Category (optional), Topic Description (Optional), Topic Link (optional).'
+    end
+  
+    # Find an existing topic with the same name and assignment ID
+    topic = SignUpTopic.where(topic_name: row_hash[:topic_name], assignment_id: session[:assignment_id]).first
+  
+    if topic.nil?
+      # If the topic doesn't exist, define its attributes and create a new topic
+      attributes = ImportTopicsHelper.define_attributes(row_hash)
+      ImportTopicsHelper.create_new_sign_up_topic(attributes, session)
+    else
+      # If the topic exists, update its max choosers and topic identifier
+      topic.max_choosers = row_hash[:max_choosers]
+      topic.topic_identifier = row_hash[:topic_identifier]
+      topic.save
+    end
+  end
+  
+
+  def self.find_slots_filled(assignment_id)
+    # retrieves the number of filled slots for each topic in a specific assignment, excluding the waitlisted team
+    SignUpTopic.find_by_sql(['SELECT topic_id as topic_id, COUNT(t.max_choosers) as count FROM sign_up_topics t JOIN signed_up_teams u ON t.id = u.topic_id WHERE t.assignment_id = ? and u.is_waitlisted = false GROUP BY t.id', assignment_id])
+  end
+
+  def self.find_slots_waitlisted(assignment_id)
+    # retrieves the number of waitlisted slots for each topic in a specific assignment
+    SignUpTopic.find_by_sql(['SELECT topic_id as topic_id, COUNT(t.max_choosers) as count FROM sign_up_topics t JOIN signed_up_teams u ON t.id = u.topic_id WHERE t.assignment_id = ? and u.is_waitlisted = true GROUP BY t.id', assignment_id])
+  end
+
+  def self.find_waitlisted_topics_for_team(assignment_id, team_id)
+    # retrieves the IDs of the topics that a specific team is waitlisted for in a specific assignment
+    SignedUpTeam.find_by_sql(['SELECT u.id FROM sign_up_topics t, signed_up_teams u WHERE t.id = u.topic_id and u.is_waitlisted = true and t.assignment_id = ? and u.team_id = ?', assignment_id.to_s, team_id.to_s])
+  end
+
+  def slot_available?
+    topic_id = self.id
+    # Retrieve the SignUpTopic record based on the given topic_id
+    topic = SignUpTopic.find(topic_id)
+    
+    # Find the number of students who have selected the topic and are not waitlisted
+    no_of_students_who_selected_the_topic = SignedUpTeam.where(topic_id: topic_id, is_waitlisted: false)
+  
+    # Check if no students have selected the topic yet
+    if no_of_students_who_selected_the_topic.nil?
+      return true
+    else
+      # Check if the number of students who selected the topic is less than the maximum allowed
+      if topic.max_choosers > no_of_students_who_selected_the_topic.size
+        return true # There are available slots for this topic
+      else
+        return false # All slots for this topic are filled
+      end
+    end
+  end  
+
+  def self.assign_to_first_waiting_team(next_wait_listed_team)
+    # get the team_id from the next waitlisted team
+    team_id = next_wait_listed_team.team_id
+    team = Team.find(team_id)
+    assignment_id = team.parent_id
+    # removing it from the waitlist
+    next_wait_listed_team.is_waitlisted = false
+    next_wait_listed_team.save
+    # cancels all other waitlist entries for this team in the same assignment
+    Waitlist.cancel_all_waitlists(team_id, assignment_id)
+  end
+  
+
+  def update_waitlisted_users(max_choosers)
+    num_of_users_promotable = max_choosers.to_i - self.max_choosers.to_i
+    num_of_users_promotable.times do
+      next_wait_listed_team = SignedUpTeam.where(topic_id: id, is_waitlisted: true).first
+      # if slot exist, then confirm the topic for this team and delete all waitlists for this team
+      SignUpTopic.assign_to_first_waiting_team(next_wait_listed_team) if next_wait_listed_team
+    end
+  end
+
+  def self.has_suggested_topic?(assignment_id)
+    sign_up_topics = SignUpTopic.where(assignment_id: assignment_id, private_to: nil)
+    all_topics = SignUpTopic.where(assignment_id: assignment_id)
+    # returns true if there are suggested topics (non-private topics), false otherwise
+    sign_up_topics.size != all_topics.size
+  end
+  
+
+  def users_on_waiting_list
+    # Finds all teams signed up for the current topic (identified by 'id') that are waitlisted
+    waitlisted_signed_up_teams = SignedUpTeam.where(topic_id: id, is_waitlisted: 1)
+    # Initializes an empty array to store the waitlisted users
+    waitlisted_users = []
+    # Checks if there are any waitlisted teams
+    if waitlisted_signed_up_teams.present?
+      # Iterates over each waitlisted team
+      waitlisted_signed_up_teams.each do |waitlisted_signed_up_team|
+        # Finds the assignment team using the team_id from the waitlisted signed-up team
+        assignment_team = AssignmentTeam.find(waitlisted_signed_up_team.team_id)
+        # Adds the users of the assignment team to the waitlisted_users array
+        waitlisted_users << assignment_team.users
+      end
+    end
+    # Flattens the array of arrays into a single array and returns it
+    waitlisted_users.flatten
+  end
+  
+  
+
+  def format_for_display
+    topic_display = ''
+    topic_display += topic_identifier.to_s + ' - '
+    topic_display + topic_name
+  end
+
+  def self.new_topic_from_suggestion(suggestion)
+    signuptopic = SignUpTopic.new
+    signuptopic.topic_identifier = 'S' + Suggestion.where('assignment_id = ? and id <= ?', suggestion.assignment_id, suggestion.id).size.to_s
+    signuptopic.topic_name = suggestion.title
+    signuptopic.assignment_id = suggestion.assignment_id
+    signuptopic.max_choosers = 1
+    # return this model based on these checks
+    if signuptopic.save && suggestion.update_attribute('status', 'Approved')
+      return signuptopic
+    else
+      return 'failed'
+    end
+  end
+
+  def longest_waiting_team(topic_id)
+    # Find and return the team that has been waiting the longest for the specified topic.
+
+    # Find the first waitlisted user (team) for the given topic by querying the SignedUpTeam table.
+    # Check for records where the topic_id matches the specified topic_id and is_waitlisted is true.
+    first_waitlisted_user = SignedUpTeam.where(topic_id: topic_id, is_waitlisted: true).first   
+    # If a waitlisted user (team) is found, return it as the team that has been waiting the longest.
+    unless first_waitlisted_user.nil?
+      return first_waitlisted_user
+    end 
+    # If no waitlisted user is found, return nil to indicate that there are no teams waiting.
+    return nil
+  end
+
+  def reassign_topic(team_id)
+    # Reassigns topic when a team is dropped from a topic.
+    # Get the topic ID for reassignment.
+    topic_id = self.id
+    # Fetch the record in SignedUpTeam where topic_id matches the topic that needs reassignment
+    # and team_id matches the provided team_id. Retrieve the first matching record.
+    signup_record = SignedUpTeam.where(topic_id: topic_id, team_id:  team_id).first
+    # If the signup record is not marked as waitlisted (is_waitlisted is either false or nil),
+    # proceed with reassignment.
+    unless signup_record.try(:is_waitlisted)
+      # Find the longest waiting team for the same topic.
+      longest_waiting_team  = longest_waiting_team(topic_id)
+      # If a longest waiting team is found, proceed with reassignment.
+      unless longest_waiting_team.nil?
+        # Assign the topic to the longest waiting team.
+        # Update the is_waitlisted boolean to false for the longest waiting team.
+        longest_waiting_team.is_waitlisted = false
+        longest_waiting_team.save
+        # Drop all waitlisted records for that team.
+        SignedUpTeam.drop_off_waitlists(longest_waiting_team.team_id)
+      end
+    end
+    # Delete the entry for the team that was either previously assigned or waiting.
+    SignedUpTeam.drop_signup_record(topic_id, team_id)
+  end
+
+  # Method to handle the process when a user signs up
+  def sign_team_up(team_id)
+    topic_id = self.id
+    team = Team.find(team_id)
+    # Fetch all topics for the user within the team for the assignment
+    user_signup = SignedUpTeam.find_user_signup_topics(team.parent_id, team_id)
+    # Check if the user is already signed up and waitlisted for the topic
+    if !user_signup.empty?
+      return false unless user_signup.first&.is_waitlisted == true
+    end
+    # Create a new SignedUpTeam instance with the provided topic and team details
+    signup = SignedUpTeam.new(topic_id: topic_id, team_id: team_id)
+    @signed_topic = SignUpTopic.find_by(id: topic_id)
+    if @signed_topic.slot_available?
+      # Assign the topic to the team if a slot is available and drop off the team from all waitlists
+      SignUpTopic.assign_topic_to_team(signup, topic_id)
+      # Once assigned, drop all the waitlisted topics for this team
+      result = SignedUpTeam.drop_off_waitlists(team_id)
+    else
+      # Save the team as waitlisted if no slots are available
+      result = SignUpTopic.save_waitlist_entry(signup, team_id)
+    end
+    result
+  end
+
+  # Method to assign a topic to the team and update the waitlist status
+  def self.assign_topic_to_team(signup, topic_id)
+    # Set the team's waitlist status to false as they are assigned a topic
+    signup.update(is_waitlisted: false)
+    # Update the topic_id in the signed_up_teams table for the user
+    signed_up_team = SignedUpTeam.find_by(topic_id: topic_id)
+    signed_up_team.update(topic_id: topic_id) if signed_up_team
+  end
+
+  # Method to save the user as waitlisted if no slots are available
+  def self.save_waitlist_entry(signup, team_id)
+    signup.is_waitlisted = true
+    # Save the user's waitlist status
+    result = signup.save
+    # Log the creation of the sign-up sheet for the waitlisted user
+    ExpertizaLogger.info(LoggerMessage.new('SignUpSheet', '', "Sign up sheet created for waitlisted with teamId #{team_id}"))
+    result
+  end
+
 end
