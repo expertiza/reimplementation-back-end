a374d3b5aa4d6dbc618dc2984633939f7c2dde7b
jphulse <162621611+jphulse@users.noreply.github.com>
2024-10-23 21:26:17 -0400

    Updated grades_controller.rb to match the good parts of grades_controller_dev's copy
    The other dev branch is effectively dead, and this is the new branch so this should be considered a SETUP commit

--- app/controllers/api/v1/grades_controller.rb
+++ app/controllers/api/v1/grades_controller.rb
@@ -1,4 +1,4 @@
-class GradesController < ApplicationController
+class Api::V1::GradesController < ApplicationController
   helper :file
   helper :submitted_content
   helper :penalty
@@ -8,8 +8,7 @@ class GradesController < ApplicationController
   include GradesHelper
   include AuthorizationHelper
   include Scoring
-
-  # Determines if an action is allowed for users to view my scores and view team or if they are a TA
+# Determines if an action is allowed for users to view my scores and view team or if they are a TA
   def action_allowed?
     case params[:action]
     when 'view_my_scores'
@@ -17,30 +16,24 @@ class GradesController < ApplicationController
     when 'view_team'
       view_team_allowed?
     else
-      current_user_has_ta_privileges?
+      user_ta_privileges?
     end
-  end
-
-
-  def controller_locale
-    locale_for_student
   end
 
   # the view grading report provides the instructor with an overall view of all the grades for
   # an assignment. It lists all participants of an assignment and all the reviews they received.
   # It also gives a final score, which is an average of all the reviews and greatest difference
   # in the scores of all the reviews.
+
+  # Concerns: No list of all participants (Is that a requirement?) whilst mentioned in the documentation.
+  # Does not list any reviews for the participants.
+  # Instead it provides an (assumed) graph of scores, their averages for each question, and penalties.
+  # Too many functions we cannot locally access.
   def view
+    # Finds the assignment
     @assignment = Assignment.find(params[:id])
-    questionnaires = @assignment.questionnaires
-    if @assignment.varying_rubrics_by_round?
-      @questions = retrieve_questions questionnaires, @assignment.id
-    else
-      @questions = {}
-      questionnaires.each do |questionnaire|
-        @questions[questionnaire.symbol] = questionnaire.questions
-      end
-    end
+    # Extracts the questionnaires
+    @questions = filter_questionnaires
     @scores = review_grades(@assignment, @questions)
     @num_reviewers_assigned_scores = @scores[:teams].length # After rejecting nil scores need original length to iterate over hash
     averages = vector(@scores)
@@ -50,29 +43,11 @@ class GradesController < ApplicationController
     @show_reputation = false
   end
 
-  def view_my_scores
-    @participant = AssignmentParticipant.find(params[:id])
-    @team_id = TeamsUser.team_id(@participant.parent_id, @participant.user_id)
-    return if redirect_when_disallowed
-
-    @assignment = @participant.assignment
-    questionnaires = @assignment.questionnaires
-    @questions = retrieve_questions questionnaires, @assignment.id
-    # @pscore has the newest versions of response for each response map, and only one for each response map (unless it is vary rubric by round)
-    @pscore = participant_scores(@participant, @questions)
-    make_chart
-    @topic_id = SignedUpTeam.topic_id(@participant.assignment.id, @participant.user_id)
-    @stage = @participant.assignment.current_stage(@topic_id)
-    penalties(@assignment.id)
-    # prepare feedback summaries
-    summary_ws_url = WEBSERVICE_CONFIG['summary_webservice_url']
-    sum = SummaryHelper::Summary.new.summarize_reviews_by_reviewee(@questions, @assignment, @team_id, summary_ws_url, session)
-    @summary = sum.summary
-    @avg_scores_by_round = sum.avg_scores_by_round
-    @avg_scores_by_criterion = sum.avg_scores_by_criterion
-  end
 
   # method for alternative view
+  # Allows the user to view information regarding their team that they have signed up with
+  # This includes information such as the topic if relevant to the assignment, and some info related
+  # to the rubrics or rounds of peer review
   def view_team
     @participant = AssignmentParticipant.find(params[:id])
     @assignment = @participant.assignment
@@ -87,7 +62,7 @@ class GradesController < ApplicationController
     counter_for_same_rubric = 0
     if @assignment.vary_by_topic?
       topic_id = SignedUpTeam.topic_id_by_team_id(@team_id)
-      topic_specific_questionnaire = AssignmentQuestionnaire.where(assignment_id: @assignment.id, topic_id: topic_id).first.questionnaire
+      topic_specific_questionnaire = AssignmentQuestionnaire.where(assignment_id: @assignment.id, topic_id: topic_id).first&.questionnaire
       @vmlist << populate_view_model(topic_specific_questionnaire)
     end
     questionnaires.each do |questionnaire|
@@ -110,9 +85,11 @@ class GradesController < ApplicationController
       end
       @vmlist << populate_view_model(questionnaire)
     end
-    @current_role_name = current_role_name
+    @current_role_name = session[:user].role.name
   end
 
+  # Sets information for editing the grade information, setting the scores
+  # for every question after listing the questions out
   def edit
     @participant = AssignmentParticipant.find(params[:id])
     @assignment = @participant.assignment
@@ -120,26 +97,24 @@ class GradesController < ApplicationController
     @scores = participant_scores(@participant, @questions)
   end
 
+  #  Provides functionality for instructors to perform review on an assignment
+  #  appropriately redirects the instructor to the correct page based on whether
+  #  or not the review already exists within the system.
   def instructor_review
     participant = AssignmentParticipant.find(params[:id])
-    reviewer = AssignmentParticipant.find_or_create_by(user_id: session[:user].id, parent_id: participant.assignment.id)
-    reviewer.set_handle if reviewer.new_record?
-    review_exists = true
-    reviewee = participant.team
-    review_mapping = ReviewResponseMap.find_or_create_by(reviewee_id: reviewee.id, reviewer_id: reviewer.id, reviewed_object_id: participant.assignment.id)
+    review_mapping = find_participant_review_mapping(participant)
     if review_mapping.new_record?
-      review_exists = false
+      redirect_to controller: 'response', action: 'edit', id: review_mapping.map_id, return: 'instructor'
     else
       review = Response.find_by(map_id: review_mapping.map_id)
-    end
-    if review_exists
-      redirect_to controller: 'response', action: 'edit', id: review.id, return: 'instructor'
-    else
-      redirect_to controller: 'response', action: 'new', id: review_mapping.map_id, return: 'instructor'
+      redirect_to controller: 'response', action: 'new', id: review.id, return: 'instructor'
     end
   end
 
   # This method is used from edit methods
+  # Finds all questions in all relevant questionnaires associated with this
+  # assignment, this is a helper method
+  # TODO should be private.
   def list_questions(assignment)
     questions = {}
     questionnaires = assignment.questionnaires
@@ -149,6 +124,13 @@ class GradesController < ApplicationController
     questions
   end
 
+  # patch method to update the information regarding the total score for an
+  # associated with this participant for the current assignment, as long as the total_score
+  # is different from the grade
+  # TODO the bottom of this method where we call flash[:note] = message will work with the valid
+  # TODO path but not in the case of the unless statement evaluating to true, as we never initialize
+  # TODO message then, so error handling is bad here
+  # FIXME potentially obsolete, remove? No API endpoint for this in the base code
   def update
     participant = AssignmentParticipant.find(params[:id])
     total_score = params[:total_score]
@@ -157,12 +139,13 @@ class GradesController < ApplicationController
       message = if participant.grade.nil?
                   'The computed score will be used for ' + participant.user.name + '.'
                 else
-                  'A score of ' + params[:participant][:grade] + '% has been saved for ' + participant.user.name + '.'
+                  'A score of ' + grade_to_string(params) + '% has been saved for ' + participant.user.name + '.'
                 end
     end
     flash[:note] = message
     redirect_to action: 'edit', id: params[:id]
   end
+
 
   def save_grade_and_comment_for_submission
     participant = AssignmentParticipant.find_by(id: params[:participant_id])
@@ -178,35 +161,12 @@ class GradesController < ApplicationController
     redirect_to controller: 'grades', action: 'view_team', id: participant.id
   end
 
-  def bar_chart(scores, width = 100, height = 100, spacing = 1)
-    link = nil
-    GoogleChart::BarChart.new("#{width}x#{height}", ' ', :vertical, false) do |bc|
-      data = scores
-      bc.data 'Line green', data, '990000'
-      bc.axis :y, range: [0, data.max], positions: data.minmax
-      bc.show_legend = false
-      bc.stacked = false
-      bc.width_spacing_options(bar_width: (width - 30) / (data.size + 1), bar_spacing: 1, group_spacing: spacing)
-      bc.data_encoding = :extended
-      link = bc.to_url
-    end
-    link
-  end
+  
 
   private
 
-  def populate_view_model(questionnaire)
-    vm = VmQuestionResponse.new(questionnaire, @assignment, @round)
-    vmquestions = questionnaire.questions
-    vm.add_questions(vmquestions)
-    vm.add_team_members(@team)
-    qn = AssignmentQuestionnaire.where(assignment_id: @assignment.id, used_in_round: 2).size >= 1
-    vm.add_reviews(@participant, @team, @assignment.varying_rubrics_by_round?)
-    vm.calculate_metrics
-    vm
-  end
-
-  def redirect_when_disallowed #Could refactor this to two methods using disalowed and redirect
+  def redirect_when_disallowed
+    # Could refactor this to two methods using disalowed and redirect
     # For author feedback, participants need to be able to read feedback submitted by other teammates.
     # If response is anything but author feedback, only the person who wrote feedback should be able to see it.
     ## This following code was cloned from response_controller.
@@ -214,14 +174,14 @@ class GradesController < ApplicationController
     # ACS Check if team count is more than 1 instead of checking if it is a team assignment
     if @participant.assignment.max_team_size > 1
       team = @participant.team
-      unless team.nil? || (team.user? session[:user])
+      unless team.nil? || (team.users.include? session[:user])
         flash[:error] = 'You are not on the team that wrote this feedback'
         redirect_to '/'
         return true
       end
     else
       reviewer = AssignmentParticipant.where(user_id: session[:user].id, parent_id: @participant.assignment.id).first
-      return true unless current_user_id?(reviewer.try(:user_id))
+      return true unless session[:user_id] == reviewer.try(:user_id)
     end
     false
   end
@@ -235,25 +195,7 @@ class GradesController < ApplicationController
     }
   end
 
-  def make_chart
-    @grades_bar_charts = {}
-    participant_score_types = %i[metareview feedback teammate]
-    if @pscore[:review]
-      scores = []
-      if @assignment.varying_rubrics_by_round?
-        (1..@assignment.rounds_of_reviews).each do |round|
-          responses = @pscore[:review][:assessments].select { |response| response.round == round }
-          scores = scores.concat(score_vector(responses, 'review' + round.to_s))
-          scores -= [-1.0]
-        end
-        @grades_bar_charts[:review] = bar_chart(scores)
-      else
-        charts(:review)
-      end
-    end
-    participant_score_types.each { |symbol| charts(symbol) }
-  end
-
+ 
   def self_review_finished?
     participant = Participant.find(params[:id])
     assignment = participant.try(:assignment)
@@ -281,5 +223,36 @@ def view_team_allowed?
     current_user_is_assignment_participant?(participant.assignment.id)
   else
     true
+  end
+end
+
+# Checks if the rubric varies by round and then returns appropriate
+# questions based on the ruling
+def filter_questionnaires
+  questionnaires = @assignment.questionnaires
+  if @assignment.varying_rubrics_by_round?
+    retrieve_questions(questionnaires, @assignment.id)
+  else
+    questions = {}
+    questionnaires.each do |questionnaire|
+      questions[questionnaire.symbol] = questionnaire.questions
+    end
+    questions
+  end
+end
+
+# Helper method that finds the current user from the session and then determines
+# if that user has the privileges afforded to someone with the role of TA
+# or higher
+def user_ta_privileges?
+  user_id = session[:user_id]
+  user = User.find(user_id)
+  user.role.all_privileges_of?(Role.find_by(name: 'Teaching Assistant'))
+end
+
+def review_grades(assignment, questions)
+  scores = { participants: {}, teams: {} }
+  assignment.participants.each do |participant|
+    scores[:participants][participant.id.to_s.to_sym] = participant_scores(participant, questions)
   end
 end
