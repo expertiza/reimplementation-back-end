13e23163e84a5255547d218b316b86836473c1ff
jphulse <hulsej281@gmail.com>
2024-10-27 15:30:04 -0400

    Added a before method and a test for get('action_allowed')

--- spec/requests/api/v1/grades_controller_spec.rb
+++ spec/requests/api/v1/grades_controller_spec.rb
@@ -1,10 +1,24 @@
 require 'swagger_helper'
 
 RSpec.describe describe 'Grades API', type: :request do
+  let(:user) { create(:user) }
+
+  # Chat GPT assisted on before method
+  before do
+    post '/api/v1/login', params: { email: user.email, password: 'password' }
+    @token = JSON.parse(response.body)['token']
+  end
+
   path '/api/v1/grades/{action}/action_allowed' do
     parameter name: 'action', in: :path, type: :string, description: 'the action the user wishes to perform'
-    get('action_allowed') do
+    let(:action) { 'view_team' }
 
+    get('action_allowed') do
+      tags 'Grades'
+      header 'Authorization', "Bearer #{@token}"
+      response(200, 'successful') do
+        run_test!
+      end
     end
 
   end
