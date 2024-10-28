816a49d19faba132df67a822ea8d40e735518e67
snehilbehar1999 <sbehar@ncsu.edu>
2024-10-28 19:52:15 -0400

    reimplemented topic_due_date

--- app/models/topic_due_date.rb
+++ app/models/topic_due_date.rb
@@ -1,20 +1,13 @@
 class TopicDueDate < DueDate
   def self.next_due_date(assignment_id, topic_id)
-    next_due_date = where('parent_id = ? and due_at >= ?', topic_id, Time.zone.now)
-    puts 'hi', next_due_date
+    next_due_dates = where('parent_id = ? and due_at >= ?', topic_id, Time.zone.now)
 
-    if next_due_date.nil?
-      topic_due_date_size = where(parent_id: topic_id).size
-      following_assignment_due_dates = AssignmentDueDate.where(parent_id: assignment_id)[topic_due_date_size..-1]
-      puts 'hi2', following_assignment_due_dates
-
-      unless following_assignment_due_dates.nil?
-        next_due_date = following_assignment_due_dates.find do |assignment_due_date|
-          assignment_due_date.due_at >= Time.zone.now
-        end
-      end
+    # If there are no due dates for the topic, then check if there are any due dates for the assignment
+    if next_due_dates.any? == false
+      next_due_dates = where('parent_id = ? and due_at >= ?', assignment_id, Time.zone.now)
     end
 
-    next_due_date
+    # Sort the due dates and return the next due date
+    sort_due_dates(next_due_dates).first
   end
 end
