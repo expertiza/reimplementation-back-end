7d327e63b1b03503299f364e3ac679b48b34fb91
grochocinski <williamgrochocinski@gmail.com>
2024-10-29 22:05:36 -0400

    Converted seeded data into factories

--- spec/requests/api/v1/bookmarks_controller_spec.rb
+++ spec/requests/api/v1/bookmarks_controller_spec.rb
@@ -2,60 +2,93 @@ require 'rails_helper'
 require 'factory_bot_rails' # shouldn't be needed
 
 RSpec.describe 'api/v1/bookmarks', type: :request do
-  # Find each user in the users table
-  # We can look it up by the role_id
-  let(:student) { User.find_by(name: 'student') }
-  let(:ta) { User.find_by(name: 'ta') }
-  let(:instructor) { User.find_by(name: 'instructor') }
-  let(:admin) { User.find_by(name: 'admin') }
-  let(:super_admin) { User.find_by(name: 'super_admin') }
-
-  # Create headers for each user so they can sign in
-  let(:student_headers) { authenticated_header(student) }
-  let(:ta_headers) { authenticated_header(ta) }
-  let(:instructor_headers) { authenticated_header(instructor) }
-  let(:admin_headers) { authenticated_header(admin) }
-  let(:super_admin_headers) { authenticated_header(super_admin) }
 
   before do
     # Set the default host to localhost
     host! 'localhost'
   end
-  
-  describe 'GET /api/v1/bookmarks' do
-    it 'lets the student access the list of bookmarks' do
-      get '/api/v1/bookmarks', headers: student_headers
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq([])
-    end
 
-    it 'lets the ta access the list of bookmarks' do
-      get '/api/v1/bookmarks', headers: ta_headers
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq([])
+  describe User do 
+    before(:each) do
+      # Create a student
+      @student = create(:user, role_id: Role.find_by(name: 'Student').id)
+      @student_headers = authenticated_header(@student)
     end
-
-    it 'lets the instructor access the list of bookmarks' do
-      get '/api/v1/bookmarks', headers: instructor_headers
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq([])
+    describe 'GET /api/v1/bookmarks' do
+      it 'lets the student access the list of bookmarks' do
+        get '/api/v1/bookmarks', headers: @student_headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq([])
+      end
     end
+  end
 
-    it 'lets the admin access the list of bookmarks' do
-      get '/api/v1/bookmarks', headers: admin_headers
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq([])
+  describe Ta do
+    before(:each) do
+      # Create a teaching assistant
+      @ta = create(:user, role_id: Role.find_by(name: 'Teaching Assistant').id)
+      @ta_headers = authenticated_header(@ta)
     end
-
-    it 'lets the super admin access the list of bookmarks' do
-      get '/api/v1/bookmarks', headers: super_admin_headers
-      expect(response).to have_http_status(:ok)
-      expect(JSON.parse(response.body)).to eq([])
+    describe 'GET /api/v1/bookmarks' do
+      it 'lets the teaching assistant access the list of bookmarks' do
+        get '/api/v1/bookmarks', headers: @ta_headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq([])
+      end
     end
+  end
 
-    it 'does not let someone without a token access the list of bookmarks' do
-      get '/api/v1/bookmarks'
-      expect(response).to have_http_status(:unauthorized)
+  describe Instructor do
+    before(:each) do
+      # Create an instructor
+      @instructor = create(:user, role_id: Role.find_by(name: 'Instructor').id)
+      @instructor_headers = authenticated_header(@instructor)
+    end
+    describe 'GET /api/v1/bookmarks' do
+      it 'lets the instructor access the list of bookmarks' do
+        get '/api/v1/bookmarks', headers: @instructor_headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+  end
+
+  describe Administrator do
+    before(:each) do
+      # Create an administrator
+      @admin = create(:user, role_id: Role.find_by(name: 'Administrator').id)
+      @admin_headers = authenticated_header(@admin)
+    end
+    describe 'GET /api/v1/bookmarks' do
+      it 'lets the administrator access the list of bookmarks' do
+        get '/api/v1/bookmarks', headers: @admin_headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+  end
+
+  describe SuperAdministrator do
+    before(:each) do
+      # Create a super administrator
+      @super_admin = create(:user, role_id: Role.find_by(name: 'Super Administrator').id)
+      @super_admin_headers = authenticated_header(@super_admin)
+    end
+    describe 'GET /api/v1/bookmarks' do
+      it 'lets the super administrator access the list of bookmarks' do
+        get '/api/v1/bookmarks', headers: @super_admin_headers
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+  end
+
+  describe 'user that has not signed in' do
+    describe 'GET /api/v1/bookmarks' do
+      it 'does not let someone without a token access the list of bookmarks' do
+        get '/api/v1/bookmarks'
+        expect(response).to have_http_status(:unauthorized)
+      end
     end
   end
 end
