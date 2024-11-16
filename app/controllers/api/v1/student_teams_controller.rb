31f416558f26b31213cc8bd0d025af109b4b93b5
moreharsh <moreharsh2001@gmail.com>
2024-11-16 17:13:04 -0500

    E2476 show method updated to give the json response back

--- app/controllers/api/v1/student_teams_controller.rb
+++ app/controllers/api/v1/student_teams_controller.rb
@@ -1,15 +1,26 @@
 class StudentTeamsController < ApplicationController
-      
-    def show
+    
+    # GET /student_teams
+    def index
     end
-  
+
+    # GET /student_teams/:student_id
+    def show
+        @student = AssignmentParticipant.find(params[:student_id])
+        render json: @student, status: :ok
+    end
+
+    # POST /student_teams/:student_id
     def create
     end
     
+    # PATCH /student_teams/:team_id
     def update
     end
 
+    # DELETE /student_teams/:team_id/:student_id
     def destroy
     end
+
 end
   
