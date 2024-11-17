faf99e76a8dede583b7898fb7ce1404af40f08b8
Pierce57 <piercewhelan806@gmail.com>
2024-11-17 11:38:57 -0500

    Adding skeleton methods to 'participants_controller.rb'

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -1,3 +1,33 @@
 class Api::V1::ParticipantsController < ApplicationController
+  # Return a list of participants
+  # GET /participants
+  def index; end
 
+  # Return a specified participant
+  # GET /participant/:id
+  def show; end
+
+  # Copy all participants from a course to an assignment
+  # GET /participants/inherit
+  def inherit; end
+
+  # Copy all participants from an assignment to a course
+  # GET /participants/bequeath
+  def bequeath; end
+
+  # Create a participant
+  # POST /participant
+  def create; end
+
+  # Update the permissions of a participant
+  # PATCH /participant/:id/authorization
+  def update_authorization; end
+
+  # Update the handle of a participant
+  # PATCH /participant/:id/handle
+  def update_handle; end
+
+  # Delete a specified participant
+  # DELETE /participant/:id
+  def destroy; end
 end
