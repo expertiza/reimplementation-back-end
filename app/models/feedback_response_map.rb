14c728ca08b1c4030563dea6b37049bcf8e6dfc0
TiptonG <tgmiddl2@ncsu.edu>
2024-11-18 18:53:16 -0500

    Modularize self.feedback_response_map
    Separate self.feedback_response_map functionality across several private methods to reduce complexity.
    NOTES:
    * These changes are in line with the documentation, with the only exception being that self.get_feedback_authors() requires an argument I did not forsee
    * These changes are untested
    * The variable names might want to be changed in the future, for the time being I stuck largely with what was there already
    * Some of the code might want to be displayed better in the future, the "next if" section on line 126 still vexes me.

--- app/models/feedback_response_map.rb
+++ app/models/feedback_response_map.rb
@@ -35,45 +35,51 @@ class FeedbackResponseMap < ResponseMap
     # map_id in (select id from response_maps where reviewed_object_id = 722 and type = 'ReviewResponseMap'))
     @review_response_map_ids = ReviewResponseMap.where(['reviewed_object_id = ?', id]).pluck('id')
     teams = AssignmentTeam.includes([:users]).where(parent_id: id)
-    @authors = []
-    teams.each do |team|
-      team.users.each do |user|
-        participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
-        @authors << participant
-      end
-    end
+    # @authors = []
+    # teams.each do |team|
+    #   team.users.each do |user|
+    #     participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
+    #     @authors << participant
+    #   end
+    # end
+    @authors = get_feedback_authors(id)
 
     @temp_review_responses = Response.where(['map_id IN (?)', @review_response_map_ids]).order('created_at DESC')
     # we need to pick the latest version of review for each round
     @temp_response_map_ids = []
     if Assignment.find(id).varying_rubrics_by_round?
-      @all_review_response_ids_round_one = []
-      @all_review_response_ids_round_two = []
-      @all_review_response_ids_round_three = []
-      @temp_review_responses.each do |response|
-        next if @temp_response_map_ids.include? response.map_id.to_s + response.round.to_s
+    #   @all_review_response_ids_round_one = []
+    #   @all_review_response_ids_round_two = []
+    #   @all_review_response_ids_round_three = []
+    #   @temp_review_responses.each do |response|
+    #     next if @temp_response_map_ids.include? response.map_id.to_s + response.round.to_s
 
-        @temp_response_map_ids << response.map_id.to_s + response.round.to_s
-        @all_review_response_ids_round_one << response.id if response.round == 1
-        @all_review_response_ids_round_two << response.id if response.round == 2
-        @all_review_response_ids_round_three << response.id if response.round == 3
-      end
+    #     @temp_response_map_ids << response.map_id.to_s + response.round.to_s
+    #     @all_review_response_ids_round_one << response.id if response.round == 1
+    #     @all_review_response_ids_round_two << response.id if response.round == 2
+    #     @all_review_response_ids_round_three << response.id if response.round == 3
+    #   end
+      @all_review_response_ids_rounds = varying_rubrics_report(@temp_review_responses, @temp_response_map_ids)
+      return authors, @all_review_response_ids_rounds[1], @all_review_response_ids_rounds[2], @all_review_response_ids_rounds[3]
     else
-      @all_review_response_ids = []
-      @temp_review_responses.each do |response|
-        unless @temp_response_map_ids.include? response.map_id
-          @temp_response_map_ids << response.map_id
-          @all_review_response_ids << response.id
-        end
-      end
+    #   @all_review_response_ids = []
+    #   @temp_review_responses.each do |response|
+    #     unless @temp_response_map_ids.include? response.map_id
+    #       @temp_response_map_ids << response.map_id
+    #       @all_review_response_ids << response.id
+    #     end
+    #   end
+      @all_review_response_ids = static_rurics_report(@temp_review_responses, @temp_response_map_ids)
+      return authors, @all_review_response_ids
     end
     # @feedback_response_map_ids = ResponseMap.where(["reviewed_object_id IN (?) and type = ?", @all_review_response_ids, type]).pluck("id")
     # @feedback_responses = Response.where(["map_id IN (?)", @feedback_response_map_ids]).pluck("id")
-    if Assignment.find(id).varying_rubrics_by_round?
-      return @authors, @all_review_response_ids_round_one, @all_review_response_ids_round_two, @all_review_response_ids_round_three
-    else
-      return @authors, @all_review_response_ids
-    end
+    # NOTE: ELIMINATING UNNECESSARY CONDITIONAL
+    # if Assignment.find(id).varying_rubrics_by_round?
+    #   return @authors, @all_review_response_ids_round_one, @all_review_response_ids_round_two, @all_review_response_ids_round_three
+    # else
+    #   return @authors, @all_review_response_ids
+    # end
   end
 
   # Send emails for author feedback
@@ -97,4 +103,43 @@ class FeedbackResponseMap < ResponseMap
     defn[:body][:first_name] = user.fullname
     Mailer.sync_message(defn).deliver
   end
+
+
+  private
+    # PRIVATE METHODS FOR USE IN SIMPLIFYING self.feedback_response_report
+    # Used in the first section of self.feedback_response_report to get the authors of the feedback
+    self.get_feedback_authors(id):
+      teams = AssignmentTeam.includes([:users]).where(parent_id: id)
+      @authors = []
+      teams.each do |team|
+        team.users.each do |user|
+          participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
+          @authors << participant
+        end
+      end
+      return authors
+    end
+    # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds vary
+    self.varying_rubrics_report(review_responses, response_map_ids):
+      all_review_response_ids_rounds = [1=>[], 2=>[], 3=>[]]
+      review_responses.each do |response|
+        next if response_map_ids.include? response.map_id.to_s + response.round.to_s
+  
+        response_map_ids << response.map_id.to_s + response.round.to_s
+        all_review_response_ids_rounds[response.round] << response.id
+      end
+      return all_review_response_ids_rounds
+    end
+    # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds do not vary
+    self.static_rurics_report(review_responses, response_map_ids):
+      all_review_response_ids = []
+      review_responses.each do |response|
+        unless response_map_ids.include? response.map_id
+          response_map_ids << response.map_id
+          all_review_response_ids << response.id
+        end
+      end
+      return all_review_response_ids
+    end
+
 end
