41adf6d4005990555733c739fe02468f3989f4d0
Pierce57 <piercewhelan806@gmail.com>
2024-11-29 15:51:28 -0500

    Undoing "The 'show' function can now list out a specific user's participant"

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -18,13 +18,9 @@ class Api::V1::ParticipantsController < ApplicationController
   # params - id
   # GET /participants/:id
   def show
-    participant = Participant.find_by(id: params[:id], user_id: params[:user_id])
+    participant = Participant.find(params[:id])
 
-    if participant
-      render json: participant, status: :ok
-    else
-      render json: { error: 'Participant not found or does not belong to the specified user' }, status: :not_found
-    end
+    render json: participant, status: :ok
   end
 
   # Create a participant
