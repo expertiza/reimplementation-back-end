07caca0c924757a203a33348d47028af3d9ecef5
Pierce57 <piercewhelan806@gmail.com>
2024-11-29 14:19:06 -0500

    Adding 'user_id' and 'assignment_id' validations to the Participant Model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,9 +1,15 @@
 class Participant < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :assignment, foreign_key: 'assignment_id', inverse_of: false
   has_many   :join_team_requests, dependent: :destroy
   belongs_to :team, optional: true
 
+  # Validations
+  validates :user_id, presence: true
+  validates :assignment_id, presence: true
+
+  # Methods
   def fullname
     user.fullname
   end
