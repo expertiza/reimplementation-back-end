735f28b6be8bab0576a8ce79698d7e47d665ea3b
Cam Himes <cshimes@ncsu.edu>
2024-11-30 20:50:14 -0500

    Added logging for the roles controller

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -5,12 +5,14 @@ class Api::V1::RolesController < ApplicationController
   # GET /roles
   def index
     roles = Role.order(:id)
+    ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Fetched all roles.", request)
     render json: roles, status: :ok
   end
 
   # GET /roles/:id
   def show
     role = Role.find(params[:id])
+    ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Fetched role with ID: #{role.id}.", request)
     render json: role, status: :ok
   end
 
@@ -18,8 +20,10 @@ class Api::V1::RolesController < ApplicationController
   def create
     role = Role.new(role_params)
     if role.save
+      ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Created role with ID: #{role.id}.", request)
       render json: role, status: :created
     else
+      ExpertizaLogger.error LoggerMessage.new(controller_name, session[:user].name, "Failed to create role. Errors: #{role.errors.full_messages.join(', ')}", request)
       render json: role.errors, status: :unprocessable_entity
     end
   end
@@ -28,8 +32,10 @@ class Api::V1::RolesController < ApplicationController
   def update
     role = Role.find(params[:id])
     if role.update(role_params)
+      ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Updated role with ID: #{role.id}.", request)
       render json: role, status: :ok
     else
+      ExpertizaLogger.error LoggerMessage.new(controller_name, session[:user].name, "Failed to update role with ID: #{role.id}. Errors: #{role.errors.full_messages.join(', ')}", request)
       render json: role.errors, status: :unprocessable_entity
     end
   end
@@ -39,12 +45,14 @@ class Api::V1::RolesController < ApplicationController
     role = Role.find(params[:id])
     role_name = role.name
     role.destroy
+    ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Deleted role with ID: #{role.id}, Name: #{role_name}.", request)
     render json: { message: "Role #{role_name} with id #{params[:id]} deleted successfully!" }, status: :no_content
   end
 
   def subordinate_roles
     role = current_user.role
     roles = role.subordinate_roles
+    ExpertizaLogger.info LoggerMessage.new(controller_name, session[:user].name, "Fetched subordinate roles for role ID: #{role.id}.", request)
     render json: roles, status: :ok
   end
 
@@ -60,6 +68,7 @@ class Api::V1::RolesController < ApplicationController
   # end
 
   def parameter_missing
+    ExpertizaLogger.error LoggerMessage.new(controller_name, session[:user].name, "Parameter missing.", request)
     render json: { error: 'Parameter missing' }, status: :unprocessable_entity
   end
 end
