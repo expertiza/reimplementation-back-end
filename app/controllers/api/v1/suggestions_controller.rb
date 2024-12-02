e27498ee16899e4b425252409489f0220eae8b74
voltavidTony <anthonyplugins@gmail.com>
2024-12-02 18:20:10 -0500

    Added code comments, restructured 'approve' and 'reject', remove leftover debug print statements, moved remaining privilege check into helper method

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,6 +1,9 @@
+# https://wiki.expertiza.ncsu.edu/index.php?title=CSC/ECE_517_Fall_2024_-_E2477._Reimplement_suggestion_controller.rb_(Design_Document)
 class Api::V1::SuggestionsController < ApplicationController
   include AuthorizationHelper
 
+  # Comment on a suggestion.
+  # A new SuggestionComment record is made.
   def add_comment
     render json: SuggestionComment.create!(
       comment: params[:comment],
@@ -11,25 +14,40 @@ class Api::V1::SuggestionsController < ApplicationController
     render json: e.record.errors, status: :unprocessable_entity
   end
 
+  # Approve a suggestion, even if it was previously rejected.
   def approve
     deny_student('Students cannot approve a suggestion.')
-    transaction do
-      @suggestion = Suggestion.find(params[:id])
-      @suggestion.update_attribute('status', 'Approved')
-      create_topic_from_suggestion!
-      unless @suggestion.user_id.nil?
-        @suggester = User.find(@suggestion.user_id)
-        sign_team_up
-        send_notice_of_approval!
+    @suggestion = Suggestion.find(params[:id])
+    # Only go through the approval process if the suggestion isn't already approved.
+    unless @suggestion.status == 'Approved'
+      # Since this process updates multiple records at once, wrap them in
+      #   a transaction so that either all of them succeed, or none of them.
+      transaction do
+        # The approval process is:
+        # 1. Mark the suggestion as approved
+        # 2. Create a new topic from the suggestion
+        # 3. Sign the suggester's team up (create new team if necessary)
+        # 4. Send the topic approved message
+        @suggestion.update_attribute('status', 'Approved')
+        create_topic_from_suggestion!
+        # If a suggestion is anonymous, the suggester is
+        #  unknown and thus steps 3 and 4 can't be taken.
+        unless @suggestion.user_id.nil?
+          @suggester = User.find(@suggestion.user_id)
+          sign_team_up
+          send_notice_of_approval!
+        end
       end
-      render json: @suggestion, status: :ok
     end
+    render json: @suggestion, status: :ok
   rescue ActiveRecord::RecordNotFound => e
     render json: e, status: :not_found
   rescue ActiveRecord::RecordInvalid => e
     render json: e.record.errors, status: :unprocessable_entity
   end
 
+  # A new Suggestion record is made.
+  # Anonymous suggestions are allowed by nulling user_id.
   def create
     render json: Suggestion.create!(
       title: params[:title],
@@ -43,6 +61,7 @@ class Api::V1::SuggestionsController < ApplicationController
     render json: e.record.errors, status: :unprocessable_entity
   end
 
+  # Delete a suggestion from the records.
   def destroy
     deny_student('Students cannot delete suggestions.')
     Suggestion.find(params[:id]).destroy!
@@ -53,36 +72,37 @@ class Api::V1::SuggestionsController < ApplicationController
     render json: e, status: :unprocessable_entity
   end
 
+  # Get a list of all Suggestion records associated with a particular assignment.
   def index
-    deny_student('Students cannot view all suggestions.')
+    deny_student('Students cannot view all suggestions of an assignment.')
     render json: Suggestion.where(assignment_id: params[:id]), status: :ok
   end
 
+  # Reject a suggestion unless it was already approved.
   def reject
     deny_student('Students cannot reject a suggestion.')
     suggestion = Suggestion.find(params[:id])
-    if suggestion.status == 'Initialized'
-      suggestion.update_attribute('status', 'Rejected')
-      render json: suggestion, status: :ok
+    # Since the approval process makes changes to many records,
+    #   rejecting a previously approved suggestion is not possible.
+    if suggestion.status == 'Approved'
+      render json: { error: 'Suggestion has already been approved.' }, status: :unprocessable_entity
     else
-      render json: { error: 'Suggestion has already been approved or rejected.' }, status: :unprocessable_entity
+      # The only action in the rejection process is to mark the suggestion as rejected.
+      suggestion.update_attribute('status', 'Rejected') if suggestion.status == 'Initialized'
+      render json: suggestion, status: :ok
     end
   rescue ActiveRecord::RecordNotFound => e
     render json: e, status: :not_found
   end
 
+  # Get a single Suggestion record.
   def show
     @suggestion = Suggestion.find(params[:id])
-    puts @suggestion.user_id
-    puts @current_user.id
-    if @suggestion.user_id == @current_user.id || AuthorizationHelper.current_user_has_ta_privileges?
-      render json: {
-        suggestion: @suggestion,
-        comments: SuggestionComment.where(suggestion_id: params[:id])
-      }, status: :ok
-    else
-      render json: { error: 'Students can only view their own suggestions.' }, status: :forbidden
-    end
+    deny_non_owner_student('Students can only view their own suggestions.')
+    render json: {
+      suggestion: @suggestion,
+      comments: SuggestionComment.where(suggestion_id: params[:id])
+    }, status: :ok
   rescue ActiveRecord::RecordNotFound => e
     render json: e, status: :not_found
   end
@@ -90,10 +110,26 @@ class Api::V1::SuggestionsController < ApplicationController
   private
 
   def deny_student(err_msg)
-    render json: { error: err_msg }, status: :forbidden unless AuthorizationHelper.current_user_has_ta_privileges?
+    # TAs and above are allowed to perform every action on every Suggestion and SuggestionComment.
+    return if AuthorizationHelper.current_user_has_ta_privileges?
+
+    # A student account is forbidden access and instead sent an error message.
+    render json: { error: err_msg }, status: :forbidden
+  end
+
+  def deny_non_owner_student(err_msg)
+    # If the student owns the resource, they are allowed to perform
+    #   every action related to Suggestions and SuggestionComments.
+    return if @suggestion.user_id == @current_user.id
+    # TAs and above are allowed to perform every action on every Suggestion and SuggestionComment.
+    return if AuthorizationHelper.current_user_has_ta_privileges?
+
+    # A student account is forbidden access and instead sent an error message.
+    render json: { error: err_msg }, status: :forbidden
   end
 
   def create_topic_from_suggestion!
+    # Convert a suggestion into a fully fledged topic.
     @signuptopic = SignUpTopic.create!(
       topic_identifier: "S#{Suggestion.where(assignment_id: @suggestion.assignment_id).count}",
       topic_name: @suggestion.title,
@@ -103,6 +139,7 @@ class Api::V1::SuggestionsController < ApplicationController
   end
 
   def send_notice_of_approval!
+    # Email the suggester and CC the suggester's teammates that the suggestion was approved
     Mailer.send_topic_approved_email(
       cc: User.joins(:teams_users).where(teams_users: { team_id: @team.id }).where.not(id: @suggester.id).map(&:email),
       subject: "Suggested topic '#{@suggestion.title}' has been approved",
@@ -114,16 +151,22 @@ class Api::V1::SuggestionsController < ApplicationController
   def sign_team_up!
     return unless @suggestion.auto_signup == true
 
+    # Find the suggester's team which is signed up to the topic's assignment.
     @team = Team.where(assignment_id: @signuptopic.assignment_id).joins(:teams_user)
                 .where(teams_user: { user_id: @suggester.id }).first
+    # Create the team if necessary.
     if @team.nil?
       @team = Team.create!(assignment_id: @signuptopic.assignment_id)
       TeamsUser.create!(team_id: @team.id, user_id: @suggester.id)
     end
+    # If the suggester's team has no assignment, then clear its
+    #   waitlist and sign it up to the newly created topic.
     unless SignedUpTeam.exists?(team_id: @team.id, is_waitlisted: false)
       SignedUpTeam.where(team_id: @team.id, is_waitlisted: true).destroy_all
       SignedUpTeam.create!(sign_up_topic_id: @signuptopic.id, team_id: @team.id, is_waitlisted: false)
     end
+    # Since the suggester's team is signed up to the topic, make it private
+    #   to the suggester so no other teams will attempt to sign up to it.
     @signuptopic.update_attribute(:private_to, @suggester.id)
   end
 end
