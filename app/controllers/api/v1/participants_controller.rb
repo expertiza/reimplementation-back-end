a890807e614906fc3ac95a75d96df4c17416f09e
Pierce57 <piercewhelan806@gmail.com>
2024-12-01 22:23:56 -0500

    Adding comments to the private methods in 'participants_controller.rb'

--- app/controllers/api/v1/participants_controller.rb
+++ app/controllers/api/v1/participants_controller.rb
@@ -121,6 +121,10 @@ class Api::V1::ParticipantsController < ApplicationController
 
   private
 
+  # Filters participants based on the provided user and/or assignment
+  # If a user is provided, participants are filtered by user_id
+  # If an assignment is provided, participants are filtered by assignment_id
+  # Returns participants ordered by their IDs
   def filter_participants(user, assignment)
     participants = Participant.all
     participants = participants.where(user_id: user.id) if user
@@ -128,6 +132,8 @@ class Api::V1::ParticipantsController < ApplicationController
     participants.order(:id)
   end
 
+  # Finds a user based on the user_id parameter
+  # Returns the user if found
   def find_user
     user_id = params[:user_id]
     user = User.find_by(id: user_id)
@@ -135,6 +141,8 @@ class Api::V1::ParticipantsController < ApplicationController
     user
   end
 
+  # Finds an assignment based on the assignment_id parameter
+  # Returns the assignment if found
   def find_assignment
     assignment_id = params[:assignment_id]
     assignment = Assignment.find_by(id: assignment_id)
@@ -142,6 +150,8 @@ class Api::V1::ParticipantsController < ApplicationController
     assignment
   end
 
+  # Finds a participant based on the id parameter
+  # Returns the participant if found
   def find_participant
     participant_id = params[:id]
     participant = Participant.find_by(id: participant_id)
@@ -149,6 +159,8 @@ class Api::V1::ParticipantsController < ApplicationController
     participant
   end
 
+  # Validates that the role parameter is present and is one of the following valid roles: reader, reviewer, submitter, mentor
+  # Returns the role if valid
   def validate_role
     valid_roles = %w[reader reviewer submitter mentor]
     role = params[:role]
