f6f5c5c8caa461a9dd1d15b84c3d7a3a549c0caa
voltavidTony <anthonyplugins@gmail.com>
2024-12-02 16:20:16 -0500

    Initial recommended changes

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,9 @@
 class Mailer < ActionMailer::Base
   default from: 'expertiza.mailer@gmail.com'
 
-  def send_topic_approved_message(defn)
-    @body = defn[:body]
-    @topic_name = defn[:body][:approved_topic_name]
-    @proposer = defn[:body][:proposer]
-
-    defn[:to] = 'expertiza.mailer@gmail.com' if Rails.env.development? || Rails.env.test?
-    mail(subject: defn[:subject], to: defn[:to], bcc: defn[:cc]).deliver_now!
+  def send_topic_approved_email(defn)
+    @suggester = defn[:suggester]
+    @topic_name = defn[:topic_name]
+    mail(to: @suggester.email, cc: defn[:cc], subject: defn[:subject]).deliver_now!
   end
 end
