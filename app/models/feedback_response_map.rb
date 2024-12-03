85b09bb39765334b4574faaae72fc56b1f1bff80
Sarah Daniel <ssdaniel1824@gmail.com>
2024-12-03 16:36:28 -0500

    Rubocop offenses
    Commented out unneeded lines, fixed indent and spacing problems, and added a "next" to an "unless" statement at line 166.

--- app/models/feedback_response_map.rb
+++ app/models/feedback_response_map.rb
@@ -39,13 +39,16 @@ class FeedbackResponseMap < ResponseMap
     # reviewed_object_id in (select id from responses where
     # map_id in (select id from response_maps where reviewed_object_id = 722 and type = 'ReviewResponseMap'))
     @review_response_map_ids = ReviewResponseMap.where(['reviewed_object_id = ?', id]).pluck('id')
-    teams = AssignmentTeam.includes([:users]).where(parent_id: id)
+    
+    # INDENTING THIS OUT B/C ITS NOT USED IN THIS METHOD 12/3/24
+    # teams = AssignmentTeam.includes([:users]).where(parent_id: id)
+    
     # @authors = []
     # teams.each do |team|
     #   team.users.each do |user|
     #     participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
     #     @authors << participant
-    #   end
+    # end
     # end
     @authors = get_feedback_authors(id)
 
@@ -72,7 +75,7 @@ class FeedbackResponseMap < ResponseMap
     #     unless @temp_response_map_ids.include? response.map_id
     #       @temp_response_map_ids << response.map_id
     #       @all_review_response_ids << response.id
-    #     end
+    #   end
     #   end
       @all_review_response_ids = static_rubrics_report(@temp_review_responses)
       return @authors, @all_review_response_ids
@@ -109,63 +112,63 @@ class FeedbackResponseMap < ResponseMap
     Mailer.sync_message(defn).deliver
   end
 
-
   private
-    ### PRIVATE METHODS FOR USE IN SIMPLIFYING self.feedback_response_report
-    # Used in the first section of self.feedback_response_report to get the authors of the feedback
-    def self.get_feedback_authors(id)
-      # Get the teams for the assignment
-      teams = AssignmentTeam.includes([:users]).where(parent_id: id)
-      # Initialize the authors array
-      @authors = []
-      # For each team, get the users and add them to the authors array
-      teams.each do |team|
-        team.users.each do |user|
-          participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
-          @authors << participant
-        end
-      end
-      @authors
-    end
-    
-    # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds vary
-    def self.varying_rubrics_report(review_responses)
-      # Create an array of response map ids
-      response_map_ids = []  
-      # Initialize the array of response map ids
-      # This will be a dictionary, where the key is the round number and the value is an array of response ids
-      # If the dictionary does not have a key for a round, that key will be initialized with an empty array
-      all_review_response_ids_rounds = []
-      # For each response, add the response id to the appropriate round array
-      review_responses.each do |response|
-        # Skip (next) if the response is already in the array
-        next if response_map_ids.include? response.map_id.to_s + response.round.to_s 
-        # Otherwise, add the response map to the tracker array and the response id to the appropriate round array
-        response_map_ids << response.map_id.to_s + response.round.to_s
-        # If the round is not already in the dictionary, initialize it with an empty array
-        all_review_response_ids_rounds[response.round] ||= [] # This line creates a new entry only if it does not already exist
-        all_review_response_ids_rounds[response.round] << response.id
 
+  ### PRIVATE METHODS FOR USE IN SIMPLIFYING self.feedback_response_report
+  # Used in the first section of self.feedback_response_report to get the authors of the feedback
+  def self.get_feedback_authors(id)
+    # Get the teams for the assignment
+    teams = AssignmentTeam.includes([:users]).where(parent_id: id)
+    # Initialize the authors array
+    @authors = []
+    # For each team, get the users and add them to the authors array
+    teams.each do |team|
+      team.users.each do |user|
+        participant = AssignmentParticipant.where(parent_id: id, user_id: user.id).first
+        @authors << participant
       end
-      all_review_response_ids_rounds
     end
+    @authors
+  end
 
-    # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds do not vary
-    def self.static_rubrics_report(review_responses)
-      # create an array of response_map_ids
-      response_map_ids = []
-      # Initialize the array of response map ids
-      all_review_response_ids = []
-      # For each response, add the response id to the array
-      review_responses.each do |response|
-        # Skip if the response is already in the array
-        unless response_map_ids.include? response.map_id
-          # Otherwise, add the response map to the tracker array and the response id to the return array
-          response_map_ids << response.map_id
-          all_review_response_ids << response.id
-        end
+  # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds vary
+  def self.varying_rubrics_report(review_responses)
+    # Create an array of response map ids
+    response_map_ids = []
+    # Initialize the array of response map ids
+    # This will be a dictionary, where the key is the round number and the value is an array of response ids
+    # If the dictionary does not have a key for a round, that key will be initialized with an empty array
+    all_review_response_ids_rounds = []
+    # For each response, add the response id to the appropriate round array
+    review_responses.each do |response|
+      # Skip (next) if the response is already in the array
+      next if response_map_ids.include? response.map_id.to_s + response.round.to_s
+
+      # Otherwise, add the response map to the tracker array and the response id to the appropriate round array
+      response_map_ids << response.map_id.to_s + response.round.to_s
+      # If the round is not already in the dictionary, initialize it with an empty array
+      all_review_response_ids_rounds[response.round] ||= [] # This line creates a new entry only if it does not already exist
+      all_review_response_ids_rounds[response.round] << response.id
+
+    end
+    all_review_response_ids_rounds
+  end
+
+  # Used in the conditional of self.feedback_response_report to get the rubric reports if the rounds do not vary
+  def self.static_rubrics_report(review_responses)
+    # create an array of response_map_ids
+    response_map_ids = []
+    # Initialize the array of response map ids
+    all_review_response_ids = []
+    # For each response, add the response id to the array
+    review_responses.each do |response|
+      # Skip if the response is already in the array
+      next unless response_map_ids.include? response.map_id
+        # Otherwise, add the response map to the tracker array and the response id to the return array
+        response_map_ids << response.map_id
+        all_review_response_ids << response.id
       end
-      all_review_response_ids
     end
-
+    all_review_response_ids
+  end
 end
