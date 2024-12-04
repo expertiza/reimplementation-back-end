e0e33dcb816d6036722727cf4561559a06e950b7
Aarya Rajoju <arajoju@ARMBP.local>
2024-12-03 20:15:49 -0500

    updated the db seed file to create more users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,26 @@ begin
       institution_id: 1,
       role_id: 1
     )
+
+    User.create!(
+      name: 'instructor',
+      email: 'instructor@example.com',
+      password: 'password123',
+      full_name: 'instructor instructor',
+      institution_id: 1,
+      role_id: 3
+    )
+
+    User.create!(
+      name: 'student',
+      email: 'student@example.com',
+      password: 'password123',
+      full_name: 'student student',
+      institution_id: 5,
+      role_id: 5
+    )
+
+
 rescue ActiveRecord::RecordInvalid => e
     puts 'The db has already been seeded'
 end
