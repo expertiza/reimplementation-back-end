975646a84236b3d1052d362191a0fd81c598b8e7
Anurag Dilip Gorkar <56599642+AnuragGorkar@users.noreply.github.com>
2024-12-07 18:01:07 +0000

    Seed file formatted

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 begin
-  #Create an instritution
-  Institution.create!(
-    name: 'North Carolina State University',
-  )
-  
-  # Create an admin user
-  User.create!(
-    name: 'admin',
-    email: 'admin2@example.com',
-    password: 'password123',
-    full_name: 'admin admin',
-    institution_id: 1,
-    role_id: 1
-  )
+    #Create an instritution
+    Institution.create!(
+      name: 'North Carolina State University',
+    )
+    
+    # Create an admin user
+    User.create!(
+      name: 'admin',
+      email: 'admin2@example.com',
+      password: 'password123',
+      full_name: 'admin admin',
+      institution_id: 1,
+      role_id: 1
+    )
 rescue ActiveRecord::RecordInvalid => e
-  puts 'The db has already been seeded'
+    puts 'The db has already been seeded'
 end
