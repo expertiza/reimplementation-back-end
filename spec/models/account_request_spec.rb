a922fc220e06c6921f766f664bfdca00f4643028
asaxen24 <asaxen24@ncsu.edu>
2025-02-10 00:08:21 -0500

    testing PR test info workflow step

--- spec/models/account_request_spec.rb
+++ spec/models/account_request_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe AccountRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  expect(1).to eq(2)
 end
