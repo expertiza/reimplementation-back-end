3350e0deeab3f57b1fd36b7f5f9ca7079117b239
asaxen24 <asaxen24@ncsu.edu>
2025-02-10 00:43:08 -0500

    pr comment workflow

--- spec/models/account_request_spec.rb
+++ spec/models/account_request_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe AccountRequest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it "deliberately fails for testing PR comments" do
+    expect(1).to eq(2)  # This will always fail
+  end
 end
