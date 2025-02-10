9dd866e3174405e23270395360e1785ee560d797
asaxen24 <asaxen24@ncsu.edu>
2025-02-10 00:22:20 -0500

    reverted changes

--- spec/models/account_request_spec.rb
+++ spec/models/account_request_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe AccountRequest, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-  expect(1).to eq(2)
+  pending "add some examples to (or delete) #{__FILE__}"
 end
