008b0a4d9c25473e5ae81a029845d3be39685ba4
bhhuynh <brianhhuynh38@gmail.com>
2025-03-23 20:31:43 -0400

    RoleContext implementation complete

--- app/models/Strategies/roles/role_context.rb
+++ app/models/Strategies/roles/role_context.rb
@@ -1,19 +1,51 @@
 # A Context class that is able to execute role strategy methods from clients
 class RoleContext
 
+  # The RoleStrategy currently being used
   attr_reader :strategy
 
-  #
+  # Unimplemented
   def validate_permissions
     raise NotImplementedError
   end
 
+  # Returns the permissions associated with the role of the current strategy,
+  # but will return nil if there is no strategy set
+  #
+  # @return associated permissions for a given role; nil if no strategy
   def get_permissions
-    @strategy.get_permissions
+    @strategy ? return @strategy.get_permissions : return nil
   end
 
-  def set_strategy(RoleStrategy strategy)
+  # Sets a strategy to the RoleContext for use with other functions
+  #
+  # @param [RoleStrategy] strategy that is to be set
+  def set_strategy(strategy)
     @strategy = strategy
+  end
+
+  # Sets a strategy via the role of the participant (param[:authorization]). This
+  # strategy can then be used for other functions that are a part of this. The
+  # function returns a boolean that signifies the success of the function.
+  #
+  # @param [String] role is the authorization string provided to identify roles
+  # @return true if strategy is successful, else false
+  def set_strategy_by_role(role)
+    # Get proper strategy given the role provided
+    case role
+    when 'Student'
+      @strategy = StudentStrategy.new
+    when 'Reviewer'
+      @strategy = ReviewerStrategy.new
+    when 'Teaching Assistant'
+      @strategy = TeacherAssistantStrategy.new
+    when 'Mentor'
+      @strategy = MentorStrategy.new
+    else
+      return false
+    end
+    # Returns true if the strategy is successsfully set
+    return true;
   end
 
 end
