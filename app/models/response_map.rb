77e448591711ea5f0fc3ec1a718ded0589bfe166
Paul <ukpaulbd@gmail.com>
2025-03-24 04:21:53 -0400

    Response Map error fix

--- app/models/response_map.rb
+++ app/models/response_map.rb
@@ -4,7 +4,7 @@ class ResponseMap < ApplicationRecord
   belongs_to :reviewee, class_name: 'User', foreign_key: 'reviewee_id', optional: true
   belongs_to :questionnaire, foreign_key: 'reviewed_object_id', optional: true
   belongs_to :assignment, class_name: 'Assignment', foreign_key: 'reviewed_object_id', inverse_of: false
-  has_many :responses
+  has_many :response, foreign_key: 'map_id', dependent: :destroy, inverse_of: false
   validates :reviewee_id, uniqueness: { scope: :reviewed_object_id, message: "is already assigned to this questionnaire" }
 
   # Gets the score from this response map
