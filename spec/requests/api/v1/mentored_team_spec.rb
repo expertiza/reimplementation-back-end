8f2144a603c2f02dc8942a865ebc881f0b367706
Aditya Singh <78498965+adii711@users.noreply.github.com>
2025-03-24 19:08:48 -0400

    Update mentored_team_spec.rb

--- spec/requests/api/v1/mentored_team_spec.rb
+++ spec/requests/api/v1/mentored_team_spec.rb
@@ -1,48 +1,94 @@
 require 'rails_helper'
 
 RSpec.describe MentoredTeam, type: :model do
-  let(:assignment) { create(:assignment) }
-  let(:team) { create(:mentored_team, parent_id: assignment.id) }
+  let(:team) { create(:mentored_team) }
   let(:user) { create(:user) }
-  let(:mentor) { create(:user, is_mentor: true) }
+  let(:mentor) { create(:user, role: :mentor) }
 
-  describe '#add_member' do
-    context 'when user is not part of the team' do
-      it 'adds the user successfully' do
-        expect(team.add_member(user, assignment.id)).to be true
-        expect(TeamsUser.exists?(team_id: team.id, user_id: user.id)).to be true
-      end
-    end
-
-    context 'when user is already a member' do
-      it 'raises an error' do
-        team.add_member(user, assignment.id)
-        expect { team.add_member(user, assignment.id) }.to raise_error(RuntimeError, /already a member/)
-      end
-    end
-
-    context 'when mentor assignment is invalid' do
-      it 'raises an error if a mentor already exists' do
-        team.add_member(mentor, assignment.id)
-        expect { team.add_member(create(:user, is_mentor: true), assignment.id) }.to raise_error(RuntimeError, /A mentor is already assigned/)
-      end
+  describe '#import_team_members' do
+    it 'imports members successfully from a given list' do
+      members = [create(:user), create(:user)]
+      expect { team.import_team_members(members) }.to change { team.users.count }.by(2)
     end
   end
 
-  describe '#import_team_members' do
-    context 'when team members are valid' do
-      it 'imports members successfully' do
-        row_hash = { teammembers: [user.name] }
-        expect { team.import_team_members(row_hash) }.not_to raise_error
-        expect(TeamsUser.exists?(team_id: team.id, user_id: user.id)).to be true
-      end
+  describe '#find_or_raise_user' do
+    it 'returns the user if found' do
+      expect(team.find_or_raise_user(user.id)).to eq(user)
     end
 
-    context 'when a member does not exist' do
-      it 'raises an ImportError' do
-        row_hash = { teammembers: ['nonexistent_user'] }
-        expect { team.import_team_members(row_hash) }.to raise_error(ImportError, /was not found/)
-      end
+    it 'raises an error if the user is not found' do
+      expect { team.find_or_raise_user(999) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe '#user_not_in_team?' do
+    it 'returns true if user is not in the team' do
+      expect(team.user_not_in_team?(user)).to be true
+    end
+
+    it 'returns false if user is in the team' do
+      team.users << user
+      expect(team.user_not_in_team?(user)).to be false
+    end
+  end
+
+  describe '#mentor_assignment_valid?' do
+    it 'returns true if a mentor can be assigned' do
+      expect(team.mentor_assignment_valid?(mentor)).to be true
+    end
+
+    it 'returns false if an invalid mentor is assigned' do
+      expect(team.mentor_assignment_valid?(user)).to be false
+    end
+  end
+
+  describe '#add_member' do
+    it 'adds a user to the team' do
+      expect { team.add_member(user) }.to change { team.users.count }.by(1)
+    end
+
+    it 'does not add a user who is already in the team' do
+      team.users << user
+      expect { team.add_member(user) }.not_to change { team.users.count }
+    end
+  end
+
+  describe '#can_add_member?' do
+    it 'returns true if the team can add a member' do
+      expect(team.can_add_member?).to be true
+    end
+
+    it 'returns false if the team has reached its limit' do
+      allow(team).to receive(:users).and_return(Array.new(10) { create(:user) })
+      expect(team.can_add_member?).to be false
+    end
+  end
+
+  describe '#add_team_user' do
+    it 'adds a user to the team successfully' do
+      expect { team.add_team_user(user) }.to change { team.users.count }.by(1)
+    end
+  end
+
+  describe '#add_participant_to_team' do
+    it 'adds a participant to the team' do
+      participant = create(:participant)
+      expect { team.add_participant_to_team(participant) }.to change { team.users.count }.by(1)
+    end
+  end
+
+  describe '#assign_mentor_if_needed' do
+    it 'assigns a mentor if no mentor is present' do
+      team.assign_mentor_if_needed(mentor)
+      expect(team.mentor).to eq(mentor)
+    end
+
+    it 'does not assign a mentor if one is already assigned' do
+      existing_mentor = create(:user, role: :mentor)
+      team.mentor = existing_mentor
+      team.assign_mentor_if_needed(mentor)
+      expect(team.mentor).to eq(existing_mentor)
     end
   end
 end
