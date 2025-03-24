e86b7d6b185d8e0c4e9bc6b2887277de387e9c6d
ckoneti <ckoneti@ncsu.edu>
2025-03-24 01:04:58 -0400

    Added initial required methods in feedback_response_map.rb

--- app/models/feedback_response_map.rb
+++ app/models/feedback_response_map.rb
@@ -1,3 +1,29 @@
 class FeedbackResponseMap < ResponseMap
-  
+  belongs_to :review, class_name: 'Response', foreign_key: 'reviewed_object_id'
+
+  # Returns the title used for display
+  def title
+    'Feedback'
+  end
+
+  # Gets the feedback questionnaire associated with the assignment
+  def questionnaire
+    review_response_map.assignment.questionnaires.find_by(type: 'AuthorFeedbackQuestionnaire')
+  end
+
+  # Returns the original contributor (the author who received the review)
+  def contributor
+    review_response_map.reviewee
+  end
+
+  # Returns the team being reviewed in the original review
+  def team
+    review_response_map.reviewee_team
+  end
+
+  # Returns the reviewer who gave the original review
+  def reviewer
+    review_response_map.reviewer
+  end
+
 end
