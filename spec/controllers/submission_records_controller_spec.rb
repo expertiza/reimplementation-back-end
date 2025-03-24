bbabe0a31d5c2448cf901396228a24e2c2806cc4
stallap <saisumanth9199@gmail.com>
2025-03-24 09:18:18 -0400

    Updated the Rspec controller for testing
    Updated the file to handle the proper response handling during the testing.

--- spec/controllers/submission_records_controller_spec.rb
+++ spec/controllers/submission_records_controller_spec.rb
@@ -1,51 +1,41 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe SubmissionRecordsController, type: :controller do
-  # pulling assumptions from factorybots.
-  # A mock student and assignment have already been created to generate a submission record.
-  let(:student_user) { create(:student) } # TODO check this is set up correctly
-  let(:assignment) { create(:assignment) } # TODO check this is set up correctly
-  let(:submission_record) { create(:submission_record) } # TODO check this is set up correctly
+  let(:student_user) { create(:student) }
+  let(:assignment) { create(:assignment) }
+  let(:submission_record) { create(:submission_record) }
 
   describe 'GET #index' do
-
-    # only testing for when user is a student, assume existing functionality already works
-    # expected output: when user is a student the submission records should be accessed successfully
-    context "user is student" do
+    context 'user is student' do
       before do
         sign_in student_user
         get :index
       end
 
-      it "renders student view" do
-        expect(response).to render_template("student_task/list")
+      it 'renders student view' do
+        expect(response).to render_template('student_task/list')
+        expect(response).to have_http_status(:ok)
       end
     end
 
-      # it 'renders the index template' do
-      # get :index, params: { team_id: assignment_team.id }
-      # expect(response).to render_template('index')
-      # end
-
-      # goal: ensure that other students cannot view another submission record
     context 'when user is a student but not in the team' do
-      let(:other_student) { create(:student) } # create other student that is not in the team
+      let(:other_student) { create(:student) }
       before { sign_in other_student }
 
       it 'denies access' do
+        get :index
         expect(response).to have_http_status(:forbidden)
       end
     end
-    # goal: denies access if user is not logged in
+
     context 'when user is not logged in' do
-      before {get :index }
+      before { get :index }
+
       it 'denies access' do
         expect(response).to have_http_status(:forbidden)
       end
     end
   end
 end
-
-
-
