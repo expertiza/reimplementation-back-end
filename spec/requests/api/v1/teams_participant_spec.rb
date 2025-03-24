159590f39047168494cb7b1033be37ad0343047b
Aditya Singh <78498965+adii711@users.noreply.github.com>
2025-03-24 19:09:12 -0400

    Update teams_participant_spec.rb

--- spec/requests/api/v1/teams_participant_spec.rb
+++ spec/requests/api/v1/teams_participant_spec.rb
@@ -1,20 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe TeamsParticipant, type: :model do
-  let(:participant) { create(:participant) }
-  let(:team) { create(:assignment_team) }
-  let!(:teams_participant) { create(:teams_participant, participant: participant, team: team) }
+  let(:assignment) { create(:assignment) }
+  let(:team) { create(:assignment_team, assignment: assignment) }
+  let(:user) { create(:user) }
+  let(:participant) { create(:participant, user: user, assignment: assignment) }
+  let(:teams_participant) { create(:teams_participant, team: team, participant: participant) }
 
-  describe '.team_members' do
-    it 'returns all team members' do
-      members = TeamsParticipant.team_members(team.id)
-      expect(members).to include(participant.user)
+  describe 'associations' do
+    it { should belong_to(:team) }
+    it { should belong_to(:participant) }
+  end
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(teams_participant).to be_valid
+    end
+
+    it 'is invalid without a team' do
+      teams_participant.team = nil
+      expect(teams_participant).not_to be_valid
+    end
+
+    it 'is invalid without a participant' do
+      teams_participant.participant = nil
+      expect(teams_participant).not_to be_valid
     end
   end
 
-  describe '.remove_participant_from_team' do
-    it 'removes a participant from the team' do
-      expect { TeamsParticipant.remove_participant_from_team(participant.id, team.id) }.to change { TeamsParticipant.count }.by(-1)
+  describe '#team_members' do
+    it 'returns team members for a given team' do
+      teams_participant
+      expect(TeamsParticipant.team_members(team.id)).to include(participant)
+    end
+  end
+
+  describe '#participant?' do
+    it 'returns true if user is a participant' do
+      expect(teams_participant.participant?(user)).to be true
+    end
+
+    it 'returns false if user is not a participant' do
+      another_user = create(:user)
+      expect(teams_participant.participant?(another_user)).to be false
     end
   end
 end
