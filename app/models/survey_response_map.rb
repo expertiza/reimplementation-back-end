ab16f47a96d9bc6a26a1463510046696954cbe3f
Dennis Christman <dennis.christman@gmail.com>
2025-04-12 13:17:36 -0400

    Added code shared among all subclasses

--- app/models/survey_response_map.rb
+++ app/models/survey_response_map.rb
@@ -2,9 +2,16 @@
 #   AssignmentSurveyResponseMap
 #   CourseSurveyResponseMap
 #   GlobalSurveyResponseMap
-
+# Includes only code shared between all subclasses
 class SurveyResponseMap < ResponseMap
+    belongs_to :survey_deployment, foreign_key: 'reviewee_id'
+    belongs_to :reviewer, class_name: 'Participant', foreign_key: 'reviewer_id'
+
     def survey?
         true
+    end
+
+    def contributor
+        nil
     end
 end
--- spec/models/survey_response_map_spec.rb
+++ spec/models/survey_response_map_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe SurveyResponseMap, type: :model do
             expect(survey_response_map.survey?).to be true
         end
     end
+
+    describe '#contributor' do
+        it 'returns nil' do
+        survey_response_map = SurveyResponseMap.new
+        expect(survey_response_map.contributor).to be_nil
+        end
+    end
 end
