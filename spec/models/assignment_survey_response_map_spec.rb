1aad1038336d61b60600dfb3d1f3fe9dd9861488
Dennis Christman <dennis.christman@gmail.com>
2025-04-16 14:46:33 -0400

    Created model file and tests

---
+++ spec/models/assignment_survey_response_map_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe AssignmentSurveyResponseMap, type: :model do
+    describe '#questionnaire' do
+      it 'returns the questionnaire associated with the reviewed_object_id' do
+        questionnaire = instance_double('Questionnaire')
+        allow(Questionnaire).to receive(:find_by).with(id: 1).and_return(questionnaire)
+        assignment_survey_response_map = AssignmentSurveyResponseMap.new(reviewed_object_id: 1)
+        expect(assignment_survey_response_map.questionnaire).to eq(questionnaire)
+      end
+    end
+  
+    describe '#survey_parent' do
+      it 'returns the associated assignment' do
+        assignment = double('Assignment')
+        assignment_survey_response_map = AssignmentSurveyResponseMap.new
+        allow(assignment_survey_response_map).to receive(:assignment).and_return(assignment)
+        expect(assignment_survey_response_map.survey_parent).to eq(assignment)
+      end
+    end
+  end
