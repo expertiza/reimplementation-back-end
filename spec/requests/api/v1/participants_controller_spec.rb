ec42e9899a7e223098fe705c3c69be1cebc20e5d
bhhuynh <brianhhuynh38@gmail.com>
2025-04-19 06:52:45 -0400

    Fixed incorrect html status codes and error messages

--- spec/requests/api/v1/participants_controller_spec.rb
+++ spec/requests/api/v1/participants_controller_spec.rb
@@ -329,23 +329,23 @@ RSpec.describe 'Participants API', type: :request do
         end
       end
 
-      response '400', 'Missing user_id in request body' do
+      response '404', 'Missing user_id in request body' do
         let(:authorization) { 'mentor' }
         let(:participant) { { assignment_id: assignment1.id } }
 
         run_test! do |response|
-          expect(response.status).to eq(400)
-          expect(JSON.parse(response.body)['error']).to include('user_id')
+          expect(response.status).to eq(404)
+          expect(JSON.parse(response.body)['error']).to eq('User not found')
         end
       end
 
-      response '400', 'Missing assignment_id in request body' do
+      response '404', 'Missing assignment_id in request body' do
         let(:authorization) { 'mentor' }
         let(:participant) { { user_id: studentb.id } }
 
         run_test! do |response|
-          expect(response.status).to eq(400)
-          expect(JSON.parse(response.body)['error']).to include('assignment_id')
+          expect(response.status).to eq(404)
+          expect(JSON.parse(response.body)['error']).to eq('Assignment not found')
         end
       end
       
