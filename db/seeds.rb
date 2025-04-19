b29158481f75bccad2da72d4790d1e144153e1e4
Jing Huang <jinghuang@Jings-MacBook-Air.local>
2025-04-18 23:00:06 -0400

    edited seed.rb file to generate student task related data for the frontend to use.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,127 +1,177 @@
 begin
-    #Create an instritution
-    inst_id = Institution.create!(
-      name: 'North Carolina State University',
-    ).id
-    
-    # Create an admin user
-    User.create!(
-      name: 'admin',
-      email: 'admin2@example.com',
-      password: 'password123',
-      full_name: 'admin admin',
+  # Create an instritution
+  inst_id = Institution.create!(
+    name: 'North Carolina State University'
+  ).id
+
+  # Create an admin user
+  User.create!(
+    name: 'admin',
+    email: 'admin2@example.com',
+    password: 'password123',
+    full_name: 'admin admin',
+    institution_id: 1,
+    role_id: 1
+  )
+
+  # Generate Random Users
+  num_students = 4
+  num_assignments = 8
+  num_teams = 16
+  num_courses = 2
+  num_instructors = 2
+
+  puts "creating instructors"
+  instructor_user_ids = []
+  num_instructors.times do
+    instructor_user_ids << User.create(
+      name: Faker::Internet.unique.username,
+      email: Faker::Internet.unique.email,
+      password: "password",
+      full_name: Faker::Name.name,
       institution_id: 1,
-      role_id: 1
+      role_id: 3
+    ).id
+  end
+
+  puts "creating courses"
+  course_ids = []
+  num_courses.times do |i|
+    course_ids << Course.create(
+      instructor_id: instructor_user_ids[i],
+      institution_id: inst_id,
+      directory_path: Faker::File.dir(segment_count: 2),
+      name: Faker::Company.industry,
+      info: "A fake class",
+      private: false
+    ).id
+  end
+
+  puts "Creating assignments with specific names"
+  assignment_names = [
+    "Program 1",
+    "Program 3",
+    "Program 4",
+    "Program 5",
+    "OSS Program 1",
+    "OSS Program 2",
+    "Program 6",
+    "Program 7"
+  ]
+
+  puts "creating assignments"
+  assignment_ids = []
+  num_assignments.times do |i|
+    assignment_ids << Assignment.create(
+      name: assignment_names[i],
+      instructor_id: instructor_user_ids[i % num_instructors],
+      course_id: course_ids[i % num_courses],
+      has_teams: true,
+      private: false
+    ).id
+  end
+
+  puts "creating teams"
+  team_ids = []
+  num_teams.times do |i|
+    team_ids << Team.create(
+      assignment_id: assignment_ids[i % num_assignments]
+    ).id
+  end
+
+  puts "creating students"
+  student_user_ids = []
+  # num_students.times do
+  #   student_user_ids << User.create(
+  #     name: Faker::Internet.unique.username,
+  #     email: Faker::Internet.unique.email,
+  #     password: "password",
+  #     full_name: Faker::Name.name,
+  #     institution_id: 1,
+  #     role_id: 5,
+  #   ).id
+  # end
+  #
+  # Create first student with specific name "niki"
+  student_user_ids << User.create!(
+    name: "niki",
+    email: "niki@example.com",
+    password: "password",
+    full_name: "Niki Jones",
+    institution_id: 1,
+    role_id: 5
+  ).id
+
+  # Create remaining (num_students - 1) students with Faker
+  (num_students - 1).times do
+    student_user_ids << User.create(
+      name: Faker::Internet.unique.username,
+      email: Faker::Internet.unique.email,
+      password: "password",
+      full_name: Faker::Name.name,
+      institution_id: 1,
+      role_id: 5
+    ).id
+  end
+
+  puts "assigning students to teams"
+  teams_users_ids = []
+  # num_students.times do |i|
+  #  teams_users_ids << TeamsUser.create(
+  #    team_id: team_ids[i%num_teams],
+  #    user_id: student_user_ids[i]
+  #  ).id
+  # end
+
+  num_students.times do |i|
+    puts "Creating TeamsUser with team_id: #{team_ids[i % num_teams]}, user_id: #{student_user_ids[i]}"
+    teams_user = TeamsUser.create(
+      team_id: team_ids[i % num_teams],
+      user_id: student_user_ids[i]
     )
-    
-
-    #Generate Random Users
-    num_students = 48
-    num_assignments = 8
-    num_teams = 16
-    num_courses = 2
-    num_instructors = 2
-    
-    puts "creating instructors"
-    instructor_user_ids = []
-    num_instructors.times do
-      instructor_user_ids << User.create(
-        name: Faker::Internet.unique.username,
-        email: Faker::Internet.unique.email,
-        password: "password",
-        full_name: Faker::Name.name,
-        institution_id: 1,
-        role_id: 3,
-      ).id
+    if teams_user.persisted?
+      teams_users_ids << teams_user.id
+      puts "Created TeamsUser with ID: #{teams_user.id}"
+    else
+      puts "Failed to create TeamsUser: #{teams_user.errors.full_messages.join(', ')}"
     end
+  end
 
-    puts "creating courses"
-    course_ids = []
-    num_courses.times do |i|
-      course_ids << Course.create(
-        instructor_id: instructor_user_ids[i],
-        institution_id: inst_id,
-        directory_path: Faker::File.dir(segment_count: 2),
-        name: Faker::Company.industry,
-        info: "A fake class",
-        private: false
-      ).id
-    end
+  # puts "assigning participant to students, teams, courses, and assignments"
+  # participant_ids = []
+  # num_students.times do |i|
+  #   participant_ids << Participant.create(
+  #     user_id: student_user_ids[i],
+  #     assignment_id: assignment_ids[i%num_assignments],
+  #     team_id: team_ids[i%num_teams],
+  #   ).id
+  # end
+  #
+  puts "assigning participants to all students for all assignments"
+  participant_ids = []
 
-    puts "creating assignments"
-    assignment_ids = []
-    num_assignments.times do |i|
-      assignment_ids << Assignment.create(
-        name: Faker::Verb.base,
-        instructor_id: instructor_user_ids[i%num_instructors],
-        course_id: course_ids[i%num_courses],
-        has_teams: true,
-        private: false
-      ).id
-    end
+  # Array of possible stages
+  stages = ["Not started", "In progress", "Submitted", "Reviewed", "Finished"]
 
-
-    puts "creating teams"
-    team_ids = []
-    num_teams.times do |i|
-      team_ids << Team.create(
-        assignment_id: assignment_ids[i%num_assignments]
-      ).id
-    end
-
-    puts "creating students"
-    student_user_ids = []
-    num_students.times do
-      student_user_ids << User.create(
-        name: Faker::Internet.unique.username,
-        email: Faker::Internet.unique.email,
-        password: "password",
-        full_name: Faker::Name.name,
-        institution_id: 1,
-        role_id: 5,
-      ).id
-    end
-
-    puts "assigning students to teams"
-    teams_users_ids = []
-    #num_students.times do |i|
-    #  teams_users_ids << TeamsUser.create(
-    #    team_id: team_ids[i%num_teams],
-    #    user_id: student_user_ids[i]
-    #  ).id
-    #end
-
-    num_students.times do |i|
-      puts "Creating TeamsUser with team_id: #{team_ids[i % num_teams]}, user_id: #{student_user_ids[i]}"
-      teams_user = TeamsUser.create(
-        team_id: team_ids[i % num_teams],
-        user_id: student_user_ids[i]
+  student_user_ids.each do |student_id|
+    assignment_ids.each do |assignment_id|
+      team_id = team_ids.sample # randomly assign a team for the assignment
+      participant = Participant.create(
+        user_id: student_id,
+        stage_deadline: Time.now + rand(1..10).days,
+        topic: "Topic #{rand(1..5)}",
+        permission_granted: true,
+        current_stage: stages.sample,
+        assignment_id:,
+        team_id:
       )
-      if teams_user.persisted?
-        teams_users_ids << teams_user.id
-        puts "Created TeamsUser with ID: #{teams_user.id}"
+      if participant.persisted?
+        participant_ids << participant.id
       else
-        puts "Failed to create TeamsUser: #{teams_user.errors.full_messages.join(', ')}"
+        puts "Failed to create Participant: #{participant.errors.full_messages.join(', ')}"
       end
     end
-
-    puts "assigning participant to students, teams, courses, and assignments"
-    participant_ids = []
-    num_students.times do |i|
-      participant_ids << Participant.create(
-        user_id: student_user_ids[i],
-        assignment_id: assignment_ids[i%num_assignments],
-        team_id: team_ids[i%num_teams],
-      ).id
-    end
-
-
-
-
-
-
-
-
+  end
 rescue ActiveRecord::RecordInvalid => e
-    puts 'The db has already been seeded'
+  puts 'The db has already been seeded'
 end
