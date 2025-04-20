aa1f1652f1cc39856fbe066952161bc36f91dfe8
10PriyaA <priyaandurkar10@gmail.com>
2025-04-20 12:39:37 -0400

    participants and reviews

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,6 +4,7 @@ class Participant < ApplicationRecord
   belongs_to :assignment, foreign_key: 'assignment_id', inverse_of: false
   has_many   :join_team_requests, dependent: :destroy
   belongs_to :team, optional: true
+  has_many   :reviews, class_name: 'ResponseMap', foreign_key: 'reviewer_id', dependent: :destroy, inverse_of: false
 
   delegate :course, to: :assignment
 
