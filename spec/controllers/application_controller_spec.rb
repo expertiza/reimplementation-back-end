d83462f01365d0bea5b72232f71215f691394beb
Kenil2211 <kenilpatel221102@gmail.com>
2025-04-21 02:12:33 -0400

    Add set_locale before_action for I18n locale resolution and tests

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+<<<<<<< Updated upstream
+  # Define a dummy controller inside which we can invoke set_locale
+=======
+>>>>>>> Stashed changes
+  controller(ActionController::API) do
+    before_action :set_locale
+    def index; head :ok; end
+  end
+
+  it 'uses params[:locale] when valid' do
+    get :index, params: { locale: 'fr' }
+    expect(I18n.locale).to eq(:fr)
+  end
+
+  it 'falls back to Accept-Language header' do
+    request.headers['Accept-Language'] = 'es-ES,es;q=0.9'
+    get :index
+    expect(I18n.locale).to eq(:es)
+  end
+
+  it 'defaults on unknown locale' do
+    get :index, params: { locale: 'xx' }
+    expect(I18n.locale).to eq(I18n.default_locale)
+  end
+end
