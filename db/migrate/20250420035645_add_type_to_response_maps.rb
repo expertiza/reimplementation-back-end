b502ea0cdcd0ec8a154489626cb395d4c2c66f0d
nmaruva <nmaruva@ncsu.edu>
2025-04-22 15:48:40 -0400

    resolved merge errors

--- db/migrate/20250420035645_add_type_to_response_maps.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToResponseMaps < ActiveRecord::Migration[8.0]
-  def change
-    add_column :response_maps, :type, :string
-  end
-end
--- db/migrate/20250420055837_add_topic_id_to_assignment_questionnaires.rb
+++
@@ -1,5 +0,0 @@
-class AddTopicIdToAssignmentQuestionnaires < ActiveRecord::Migration[8.0]
-  def change
-    add_column :assignment_questionnaires, :topic_id, :integer
-  end
-end
