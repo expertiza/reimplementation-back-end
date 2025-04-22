bccfcee95e108d53bbc7a56c47ffa3a533c33f19
Ananya Doshi <doshiananya2002@gmail.com>
2025-04-22 12:32:40 -0400

    made changes in the helper file

--- app/helpers/team_operations_helper.rb
+++ app/helpers/team_operations_helper.rb
@@ -1,4 +1,5 @@
 module TeamOperationsHelper
+   # Validates whether the given user can be part of the specified team based on its type
   def self.validate_team_membership(team, user)
     case team.type
     when 'CourseTeam'
@@ -10,17 +11,24 @@ module TeamOperationsHelper
     end
   end
 
+  # Validates if the given user can be assigned as a mentor to the team
   def self.validate_mentor_assignment(team, user)
     return false unless team.is_a?(MentoredTeam)
     user.mentor?
   end
 
+  # Copies all members from the source team to the target team
   def self.copy_team_members(source_team, target_team)
     source_team.users.each do |user|
       target_team.add_member(user)
     end
   end
 
+  # Returns a hash of basic statistics about the given team:
+  # - Current team size
+  # - Maximum allowed size
+  # - Whether the team is full
+  # - Whether the team is empty
   def self.team_stats(team)
     {
       size: team.team_size,
