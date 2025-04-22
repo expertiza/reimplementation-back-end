60f19f3b04ee4e9c575440f3916d0741345fcc27
Ellie <elliem40499@gmail.com>
2025-04-22 18:20:52 -0400

    teammate_reviewresponse_map

--- spec/models/teammate_review_response_map_spec.rb
+++ spec/models/teammate_review_response_map_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe TeammateReviewResponseMap, type: :model do
   describe '#questionnaire' do
     it 'returns associated questionnaire' do
-      questionnaire = Questionnaire.new
-      assignment = Assignment.new
+      questionnaire = double('Questionnaire')
+      assignment = double('Assignment')
       teammate_review_response_map = TeammateReviewResponseMap.new
       allow(teammate_review_response_map).to receive(:assignment).and_return(assignment)
-      allow(questionnaire).to receive(:where).with(type: 'TeammateReviewQuestionnaire').and_return(questionnaire)
+      allow(assignment).to receive_message_chain(:questionnaires, :find_by).with(type: 'TeammateReviewQuestionnaire').and_return(questionnaire)
       expect(teammate_review_response_map.questionnaire).to eq(questionnaire)
     end
   end
@@ -13,17 +13,21 @@ RSpec.describe TeammateReviewResponseMap, type: :model do
   describe '#questionnaire_by_duty' do
     it 'returns questionnaire specific to a duty' do
       questionnaire = double('Questionnaire')
-      assignment = double('Assignment')
+      assignment = double('Assignment', assignment_id: 1)
       teammate_review_response_map = TeammateReviewResponseMap.new
       allow(teammate_review_response_map).to receive(:assignment).and_return(assignment)
-      allow(questionnaire).to receive(:find).with(assignment_id: 1, duty_id: 1).and_return([questionnaire])
+      allow(assignment).to receive_message_chain(:questionnaires, :find).with(assignment_id: 1, duty_id: 1).and_return([questionnaire])
       expect(teammate_review_response_map.questionnaire_by_duty(1)).to eq(questionnaire)
     end
     it 'returns default questionnaire when no questionnaire is found for duty' do
-      allow(AssignmentQuestionnaire).to receive(:where).with(assignment_id: 1, duty_id: 1).and_return([])
+      questionnaire = double('Questionnaire')
+      assignment = double('Assignment', assignment_id: 1)
+      teammate_review_response_map = TeammateReviewResponseMap.new
+      allow(teammate_review_response_map).to receive(:assignment).and_return(assignment)
       allow(assignment).to receive(:questionnaires).and_return(questionnaire)
-      allow(AssignmentQuestionnaire).to receive(:where).with(type: 'TeammateReviewQuestionnaire').and_return(questionnaire)
-      expect(teammate_review_response_map.questionnaire_by_duty(1)).to eq questionnaire
+      allow(assignment).to receive_message_chain(:questionnaires, :find).with(assignment_id: 1, duty_id: 1).and_return([])
+      allow(assignment).to receive_message_chain(:questionnaires, :find_by).with(type: 'TeammateReviewQuestionnaire').and_return(questionnaire)
+      expect(teammate_review_response_map.questionnaire_by_duty(1)).to eq(questionnaire)
     end
   end
 end
