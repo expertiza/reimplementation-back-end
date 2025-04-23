76d36d4531bb1ffec3bcaa76444cfdcdb24d4e2a
udsray <reachudita@gmail.com>
2025-04-22 23:18:42 -0400

    Add detailed method descriptions in MentoredTeam

--- app/models/mentored_team.rb
+++ app/models/mentored_team.rb
@@ -1,21 +1,30 @@
 class MentoredTeam < AssignmentTeam
   belongs_to :mentor, class_name: 'User'
 
+  #Validates the presence of a mentor in the team
   validates :mentor, presence: true
+
+  #Custom validation to ensure the team type is 'MentoredTeam'
   validate :type_must_be_mentored_team
+
+  #Validates the role of the mentor
   validate :mentor_must_have_mentor_role
 
+  #adds members to the team who are not mentors
   def add_member(user)
     return false if user == mentor
     super(user)
   end
 
+  #Assigning a user as mentor of the team 
+  #Validates if user has the role and permission to be a mentor
   def assign_mentor(user)
     return false unless user.role&.name&.downcase&.include?('mentor')
     self.mentor = user
     save
   end
 
+  #Unassigns mentor from team
   def remove_mentor
     self.mentor = nil
     save
