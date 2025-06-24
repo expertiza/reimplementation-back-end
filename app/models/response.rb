bf50188ea92922fd91caa90b30ef50f03b599dde
Vihar Shah <84446743+vihar2712@users.noreply.github.com>
2025-06-24 13:28:42 +0000

    Pending changes exported from your codespace

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,7 +5,7 @@ class Response < ApplicationRecord
   include MetricHelper
 
   belongs_to :response_map, class_name: 'ResponseMap', foreign_key: 'map_id', inverse_of: false
-  has_many :scores, class_name: 'Answer', foreign_key: 'response_id', dependent: :destroy, inverse_of: false
+  has_one :score, class_name: 'Answer', foreign_key: 'response_id', dependent: :destroy, inverse_of: false
 
   alias map response_map
   delegate :response_assignment, :reviewee, :reviewer, to: :map
