9a5e57f35adaccb4a3cf9234e1c4c1ed5054339d
Dennis Christman <dennis.christman@gmail.com>
2025-07-11 16:08:25 -0400

    E2532. Reimplement Missing ResponseMap Subclasses (#199)
    * Initial repo setup according to instructions
    
    * Model and tests for Survey Response Map
    
    * Added code shared among all subclasses
    
    * Created model file and tests
    
    * Created Course Survey Response Map model and tests
    
    * Created Global Survey Response Map model and tests
    
    * Committing code and test cases for feedback_response_map
    
    * Model and tests for Quiz Response Map
    
    * teammate_review_response_map
    
    * Added module file for constant title values
    
    * Resolved failing test cases and improved code clarity in model
    
    * bookmark_rating_response_map
    
    * teammate_reviewresponse_map
    
    * teammate_reviewresponse_map
    
    * teammate_reviewresponse_map
    
    * reverted gemfile changes for merge with main
    
    * added module for hardcoded titles and corresponding RSpec test
    
    * teammate_reviewresponse_map
    
    * teammate_reviewresponse_map
    
    * Added module file for constant title values
    
    * Committing code and test cases for feedback_response_map
    
    * Resolved failing test cases and improved code clarity in model
    
    * reverted gemfile changes for merge with main
    
    * added module for hardcoded titles and corresponding RSpec test
    
    * bookmark_rating_response_map wip
    
    * bookmark_rating_response_map reimplementation finished
    
    * bookmark_rating_response_map reimplementation finished
    
    ---------
    
    Co-authored-by: Ryan Gallagher <rtgalla2@ncsu.edu>
    Co-authored-by: Ellie <elliem40499@gmail.com>
    Co-authored-by: ellie-not-found <66761832+ellie-not-found@users.noreply.github.com>
    Co-authored-by: Vihar Shah <84446743+vihar2712@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -36,6 +36,7 @@ gem 'lingua'
 # This is a really small gem that can be used to retrieve objects from the database in the order of the list given
 gem 'find_with_order'
 
+
 group :development, :test do
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
---
+++ spec/models/quiz_response_map_spec.rb
@@ -0,0 +1,42 @@
+RSpec.describe QuizResponseMap, type: :model do
+    describe 'associations' do
+      it 'belongs to quiz_questionnaire' do
+        expect(described_class.reflect_on_association(:quiz_questionnaire).macro).to eq(:belongs_to)
+      end
+  
+      it 'belongs to assignment' do
+        expect(described_class.reflect_on_association(:assignment).macro).to eq(:belongs_to)
+      end
+  
+      it 'has many quiz_responses' do
+        expect(described_class.reflect_on_association(:quiz_responses).macro).to eq(:has_many)
+      end
+    end
+  
+    describe '#questionnaire' do
+      it 'returns the associated quiz_questionnaire' do
+        quiz_questionnaire = double('QuizQuestionnaire')
+        quiz_response_map = described_class.new
+        allow(quiz_response_map).to receive(:quiz_questionnaire).and_return(quiz_questionnaire)
+  
+        expect(quiz_response_map.questionnaire).to eq(quiz_questionnaire)
+      end
+    end
+  
+    describe '.mappings_for_reviewer' do
+      it 'returns mappings for a given reviewer' do
+        quiz_response_map = double('QuizResponseMap')
+        allow(QuizResponseMap).to receive(:where).with(reviewer_id: 1).and_return([quiz_response_map])
+  
+        mappings = QuizResponseMap.mappings_for_reviewer(1)
+        expect(mappings).to eq([quiz_response_map])
+      end
+    end
+
+    describe '#get_title' do
+      it 'returns the correct title constant' do
+        quiz_response_map = QuizResponseMap.new
+        expect(quiz_response_map.get_title).to eq(ResponseMapSubclassTitles::QUIZ_RESPONSE_MAP_TITLE)
+      end
+    end
+  end
