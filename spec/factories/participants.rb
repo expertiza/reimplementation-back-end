8b4bf83db65bcb3c87fc2b96d7453e69e0ff05b8
Prithish Samanta <prithishsamanta@gmail.com>
2025-10-08 19:08:24 -0400

    Changed Rails Version (#211)
    * Changed Rails Version
    
    * Update danger.yml
    
    * Update danger_target.yml
    
    * Update TestPR.yml
    
    * Update main.yml
    
    * Update danger_target.yml
    
    * Update main.yml
    
    * Update danger.yml
    
    * Updated ruby version to 3.4.5

--- spec/factories/participants.rb
+++ spec/factories/participants.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-FactoryBot.define do  
+FactoryBot.define do
   factory :assignment_participant, class: 'AssignmentParticipant' do
     association :user
     association :assignment, factory: :assignment
