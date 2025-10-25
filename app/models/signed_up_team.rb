5306624c514219e6ab3d7be46534b879f2ee634e
abhira0 <abhishekr0@protonmail.com>
2025-10-25 19:52:25 -0400

    Squashed commit of the following:
    commit f608218a1f7df58eb31ab452e0b55d99c7b778b2
    Author: dmpatel3 <dmpatel3@ncsu.edu>
    Date:   Wed Apr 16 22:13:20 2025 -0400
    
        Renamed find_team_users() to find_project_topic_team_users() and adjusted SignedUpTeam tests accordingly
    
    commit 615c2b75e0aa9abe9c85890fd377093728f31eda
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Thu Apr 17 06:37:54 2025 +0530
    
        add new tests and comments for project_topic and signed_up_team.
    
    commit 54656ae4a8d50f2ce6d7190ed8fbc4c2a47f2fb5
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Wed Apr 16 10:41:57 2025 +0530
    
        add new function in signed up team and its tests.
    
    commit 4b4eb901f0a2fd12fca96a6d46cd1b7d12e54033
    Merge: a351623 0e067e0
    Author: Adithya Srinivasan <adithya.srini8912@gmail.com>
    Date:   Mon Mar 24 17:07:24 2025 -0400
    
        Merge branch 'main' of https://github.com/RandomOscillations/reimplementation-back-end
    
    commit a351623bbced1a02d868223205f12342d4c95567
    Author: Adithya Srinivasan <adithya.srini8912@gmail.com>
    Date:   Mon Mar 24 17:05:06 2025 -0400
    
        Added JSON parsing in project_topic_controller_spec.rb
    
    commit 0e067e0d66f4df9b34718dcd8136489559276b32
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Tue Mar 25 01:41:31 2025 +0530
    
        fix the code
    
    commit 0faeb334754c5ae12e1572d6ef53d93468b1a0db
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Mon Mar 24 09:07:34 2025 +0530
    
        fixed the routing test file
    
    commit f6a4774d345a39714c5e8a612590385d76098325
    Author: dmpatel3 <dmpatel3@ncsu.edu>
    Date:   Sun Mar 23 21:54:21 2025 -0400
    
        Added and modified tests for the ProjectTopic controller
    
    commit acecf6bfababe6d6cfe3a0e184a1a13bda55785a
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Mon Mar 24 04:58:41 2025 +0530
    
        made necessary changes
    
    commit 27936161d05b15caf4bfc7ebe813ea351cfdfbc9
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Sat Mar 22 05:20:54 2025 +0530
    
        add tests and code for model
    
    commit 8ed7a4aca0f187a9b1f28bc217996dd64125fdf7
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Sat Mar 22 05:20:37 2025 +0530
    
        add tests
    
    commit 6c0dbecb3ee0a74c2bfe6b52f3e489c6fa281e43
    Author: dmpatel3 <dmpatel3@ncsu.edu>
    Date:   Thu Mar 20 18:17:26 2025 -0400
    
        Refactored remaining SignUpTopic-related instances across the project to ProjectTopic-related instances
    
    commit c242e1698c462eb7436329245ead4d86fb68cfde
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Thu Mar 20 05:05:37 2025 +0530
    
        refactored the project_topic model.
    
    commit 39df8eb371cc2168c39f1d29f823d29b032098ea
    Author: smiti <kotharismiti24@gmail.com>
    Date:   Wed Mar 19 07:31:00 2025 +0530
    
        Added migration file and renamed sign_up_topic to project_topic

--- app/models/signed_up_team.rb
+++ app/models/signed_up_team.rb
@@ -1,6 +1,55 @@
 # frozen_string_literal: true
 
 class SignedUpTeam < ApplicationRecord
-  belongs_to :sign_up_topic
+  # Scope to return confirmed signups
+  scope :confirmed, -> { where(is_waitlisted: false) }
+
+  # Scope to return waitlisted signups
+  scope :waitlisted, -> { where(is_waitlisted: true) }
+
+  belongs_to :project_topic
   belongs_to :team
+
+  # Validations for presence and uniqueness of team-topic pairing
+  validates :project_topic, presence: true
+  validates :team, presence: true,
+                   uniqueness: { scope: :project_topic }
+
+  # Calls ProjectTopic's signup_team method to initiate signup
+  def self.signup_for_topic(team, topic)
+    topic.signup_team(team)
+  end
+
+  # Removes all signups (confirmed and waitlisted) for the given team
+  def self.remove_team_signups(team)
+    team.signed_up_teams.includes(:project_topic).each do |sut|
+      sut.project_topic.drop_team(team)
+    end
+  end
+
+  # Returns all users in a given team
+  def self.find_team_participants(team_id)
+    team = Team.find_by(id: team_id)
+    return [] unless team
+
+    team.users.to_a
+  end
+
+  # Returns all users in a given team that's signed up for a topic
+  def self.find_project_topic_team_users(team_id)
+    signed_up_team = SignedUpTeam.find_by(team_id: team_id)
+    return [] unless signed_up_team
+
+    signed_up_team.team.try(:users).to_a
+  end
+
+  # Returns project topic the given user signed up for
+  def self.find_user_project_topic(user_id)
+    user = User.find_by(id: user_id)
+    return [] unless user
+
+    ProjectTopic.joins(:signed_up_teams)
+                .where(signed_up_teams: { team_id: user.teams.pluck(:id) })
+                .distinct.to_a
+  end
 end
