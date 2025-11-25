7c3253d9c8617a0cf5ef54a903932a235f0f07e7
Yu Wang <ywang374@ncsu.edu>
2025-11-24 19:58:26 -0500

    withdraw file changes due to environment setup

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ Bundler.require(*Rails.groups)
 module Reimplementation
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 7.0
     config.active_record.schema_format = :ruby
 
     # Configuration for the application, engines, and railties goes here.
--- db/migrate/20250626161114_add_name_to_teams.rb
+++ db/migrate/20250626161114_add_name_to_teams.rb
@@ -1,7 +1,5 @@
 class AddNameToTeams < ActiveRecord::Migration[8.0]
   def change
-    unless column_exists?(:teams, :name)
-      add_column :teams, :name, :string
-    end
+    add_column :teams, :name, :string
   end
 end
