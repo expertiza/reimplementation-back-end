3876a112e8a64cacb6d7adad2fd46787262f9e4f
crjone24 <crjone24@ncsu.edu>
2025-11-30 01:38:38 -0500

    Started adding the unit tests for the helper file.

---
+++ spec/helpers/import_export_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ImportableExportableHelper, type: :helper do
+  describe 'Create tests for each of the different importable classes' do
+    xit 'Import a class with no headers' do
+
+    end
+
+    xit 'Import a class with headers' do
+
+    end
+
+    xit 'Import a file with multiple records' do
+
+    end
+
+    xit 'Import a class with external lookup and create classe' do
+
+    end
+
+    xit 'Import a class with external create classes can take duplicate headers' do
+
+    end
+  end
+end
