7df5da1d06b8a78c38b9bc46eaa6212a9a32175f
John Weisz <johnmweisz@gmail.com>
2026-03-08 16:37:26 -0400

    make baseurl configurable, align email and template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ApplicationMailer
   default from: "expertizamailer@gmail.com"
 
-  def send_password_reset_email(token)
-    @reset_url = "http://localhost:3000/password_edit/check_reset_url?token=#{token}"
+  def send_password_reset_email(user, token)
+    frontend_url = ENV.fetch('FRONTEND_URL', 'http://localhost:3000')
+    @user = user
+    @reset_url = "#{frontend_url}/password_edit/check_reset_url?token=#{token}"
     mail(to: @user.email, subject: 'Expertiza password reset')
   end
 end
