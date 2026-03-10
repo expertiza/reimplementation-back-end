8f4af85efab0e0253abccdbae3a7cb2f450d314b
John Weisz <johnmweisz@gmail.com>
2026-03-09 20:22:00 -0400

    wrap mailer in conditional

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,19 +55,21 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-    address: ENV['MAILER_SERVER'],
-    port: 587,
-    domain: 'localhost',
-    user_name: ENV['MAILER_USER'],
-    password: ENV['MAILER_PASSWORD'],
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  if ENV['MAILER_USER'].present?
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.smtp_settings = {
+      address: ENV['MAILER_SERVER'],
+      port: ENV['MAILER_SERVER_PORT']&.to_i || 587,
+      domain: ENV['MAILER_DOMAIN'] || 'localhost',
+      user_name: ENV['MAILER_USER'],
+      password: ENV['MAILER_PASSWORD'],
+      authentication: :plain,
+      enable_starttls_auto: true
+    }
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  end
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
