527b058f8401b8f1fb43c03e2237f55ac6cf8028
John Weisz <johnmweisz@gmail.com>
2026-03-09 20:25:12 -0400

    remove misleading config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,6 @@ Rails.application.configure do
       authentication: :plain,
       enable_starttls_auto: true
     }
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   end
 
   # Raises error for missing translations.
