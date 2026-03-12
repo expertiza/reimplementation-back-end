ca472dd998e74ea61dff8d9c6e52941132dbc243
josev814 <josev814@gmail.com>
2026-03-11 20:23:02 -0400

    removing extra code from testing

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -17,19 +17,3 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 end
-
-# describe UserMailer do
-#   describe '#send_password_reset_email' do
-#     let(:user) { create(:password_reset_user) }
-#     let(:token) { user.generate_token_for(:password_reset) }
-#
-#     it 'sends email with correct content' do
-#       email = UserMailer.send_password_reset_email(user, token)
-#
-#       expect(email.to).to include(user.email)
-#       expect(email.subject).to eq('Password Reset Instructions')
-#       expect(email.body.encoded).to include('reset your password')
-#       expect(email.body.encoded).to include(token)
-#     end
-#   end
-# end
