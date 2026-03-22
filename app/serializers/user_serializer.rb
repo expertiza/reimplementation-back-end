dbc6d21a09af5eb2cdbcb6a2250bd869a922ac50
Kiran Nadkarni <kiran.nadk@gmail.com>
2026-03-22 08:50:50 -0500

    Updating user_serializer with more attributes and fields to return. This should fix editing of users and allow them to be manually placed into teams on the frontend.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,38 @@
 # frozen_string_literal: true
 
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :full_name
+  attributes :id, :name, :email, :full_name, :email_on_review, :email_on_submission,
+             :email_on_review_of_review, :date_format_pref, :created_at, :updated_at,
+             :role, :parent, :institution
+
+  def role
+    return nil unless object.role
+
+    {
+      id: object.role.id,
+      name: object.role.name
+    }
+  end
+
+  def parent
+    return { id: nil, name: nil } unless object.parent
+
+    {
+      id: object.parent.id,
+      name: object.parent.name
+    }
+  end
+
+  def institution
+    return { id: nil, name: nil } unless object.institution
+
+    {
+      id: object.institution.id,
+      name: object.institution.name
+    }
+  end
+
+  def date_format_pref
+    nil
+  end
 end 
