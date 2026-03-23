4b6a39d8c8a937e84284621f10b84b3e02a72b3f
Dev Patel <devpatel0732@gmail.com>
2026-03-23 15:29:27 -0400

    Initial commit

---
+++ app/controllers/responses_controller.rb
@@ -0,0 +1,31 @@
+class ResponsesController < ApplicationController
+  before_action :set_response, only: [:show, :edit, :update, :destroy]
+
+  # GET /responses
+  def index
+  end
+
+  # GET /responses/1
+  def show
+  end
+
+  # GET /responses/new
+  def new
+  end
+
+  # GET /responses/1/edit
+  def edit
+  end
+
+  # POST /responses
+  def create
+  end
+
+  # PATCH/PUT /responses/1
+  def update
+  end
+
+  # DELETE /responses/1
+  def destroy
+  end
+end
