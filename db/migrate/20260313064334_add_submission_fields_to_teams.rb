f2e1809bb7ba6b5cd82dd3facc42541aabb1fcc4
Vanaja Agarwal <vanajaagarwal@vagarwa3@ncsu.edu>
2026-03-28 13:54:30 -0400

    Make team submission fields migration resilient

--- db/migrate/20260313064334_add_submission_fields_to_teams.rb
+++ db/migrate/20260313064334_add_submission_fields_to_teams.rb
@@ -1,6 +1,7 @@
 class AddSubmissionFieldsToTeams < ActiveRecord::Migration[8.0]
   def change
-    add_column :teams, :submitted_hyperlinks, :text
-    add_column :teams, :directory_num, :integer
+    add_column :teams, :submitted_hyperlinks, :text unless column_exists?(:teams, :submitted_hyperlinks)
+    add_column :teams, :grade_for_submission, :integer unless column_exists?(:teams, :grade_for_submission)
+    add_column :teams, :comment_for_submission, :string unless column_exists?(:teams, :comment_for_submission)
   end
 end
