e0eca05ab2448e0e3ea647c0006bade6b1f57940
John Weisz <johnmweisz@gmail.com>
2026-03-27 21:41:59 -0400

    fix syntax

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ENV['MAILER_SERVER'] && ENV['MAILER_USER'].present?
+if ENV['MAILER_SERVER'].present? && ENV['MAILER_USER'].present?
   Rails.application.config.action_mailer.delivery_method = :smtp
   Rails.application.config.action_mailer.perform_deliveries = true
   Rails.application.config.action_mailer.smtp_settings = {
