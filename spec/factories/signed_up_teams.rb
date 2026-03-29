6cc2ff4ca03bda9628556848f6b06ea40a07bb07
Vanaja Agarwal <vanajaagarwal@vagarwa3@ncsu.edu>
2026-03-29 11:53:16 -0400

    Revert "Add tests for topic-specific rubric behavior"
    This reverts commit 4b97d6a6120f6f0b98138a30e4cded931d8e69c4.

--- spec/factories/signed_up_teams.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :signed_up_team do
-    association :project_topic
-    association :team, factory: :assignment_team
-    is_waitlisted { false }
-  end
-end
