fcb9a3089ee5bf4bedb12b8d1de5dec7c3dd0f5f
asreeku <asreeku@ncsu.edu>
2026-03-30 15:54:13 -0400

    Add test cases for feedback mailer

--- spec/mailers/feedback_email_mailer_spec.rb
+++ spec/mailers/feedback_email_mailer_spec.rb
@@ -13,20 +13,22 @@ RSpec.describe FeedbackEmailMailer, type: :mailer do
   let(:reviewer_id)        { 123 }
   let(:user_id)            { 456 }
 
-  let(:feedback_map)   { double('FeedbackResponseMap', reviewed_object_id: reviewed_object_id) }
-  let(:assignment)     { double('Assignment', name: 'Cool Project') }
-  let(:original_map)   { double('ResponseMap', reviewer_id: reviewer_id) }
-  let(:participant)    { double('AssignmentParticipant', user_id: user_id) }
-  let(:user)           { double('User', email: 'rev@example.com', name: 'Reviewer Name') }
-  let(:mail_message) { double('MailMessage', deliver: true) }
+  let(:feedback_map)  { double('FeedbackResponseMap', reviewed_object_id: reviewed_object_id) }
+  let(:assignment)    { double('Assignment', name: 'Cool Project') }
+  let(:original_map)  { double('ResponseMap', reviewer_id: reviewer_id) }
+  let(:participant)   { double('AssignmentParticipant', user_id: user_id) }
+  let(:user)          { double('User', email: 'rev@example.com', name: 'Reviewer Name') }
+  let(:mail_message)  { double('MailMessage', deliver: true) }
+  let(:mailer_double) { double('Mailer') }
 
   subject(:service) { described_class.new(feedback_map, assignment) }
 
   before do
+    stub_const('Mailer', mailer_double)
+    allow(mailer_double).to receive(:sync_message).and_return(mail_message)
     allow(ResponseMap).to receive(:find).with(reviewed_object_id).and_return(original_map)
     allow(AssignmentParticipant).to receive(:find).with(reviewer_id).and_return(participant)
     allow(User).to receive(:find).with(user_id).and_return(user)
-    allow(ActionMailer::Parameterized::Mailer).to receive(:sync_message).and_return(mail_message)
   end
 
   describe '#call' do
@@ -46,28 +48,28 @@ RSpec.describe FeedbackEmailMailer, type: :mailer do
     end
 
     it 'sends to the reviewer email address' do
-      expect(ActionMailer::Parameterized::Mailer).to receive(:sync_message) do |defn|
+      expect(mailer_double).to receive(:sync_message) do |defn|
         expect(defn[:to]).to eq('rev@example.com')
       end.and_return(mail_message)
       service.call
     end
 
     it 'sets body type to "Author Feedback"' do
-      expect(ActionMailer::Parameterized::Mailer).to receive(:sync_message) do |defn|
+      expect(mailer_double).to receive(:sync_message) do |defn|
         expect(defn[:body][:type]).to eq('Author Feedback')
       end.and_return(mail_message)
       service.call
     end
 
     it 'sets body name to the user name' do
-      expect(ActionMailer::Parameterized::Mailer).to receive(:sync_message) do |defn|
+      expect(mailer_double).to receive(:sync_message) do |defn|
         expect(defn[:body][:name]).to eq('Reviewer Name')
       end.and_return(mail_message)
       service.call
     end
 
     it 'sets body obj_name to the assignment name' do
-      expect(ActionMailer::Parameterized::Mailer).to receive(:sync_message) do |defn|
+      expect(mailer_double).to receive(:sync_message) do |defn|
         expect(defn[:body][:obj_name]).to eq('Cool Project')
       end.and_return(mail_message)
       service.call
