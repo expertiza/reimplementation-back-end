f88751a86b998a40a7cd6fdbd4f11c565a2c7a22
PriyaGandhi311 <priyagandhi4688@gmail.com>
2026-04-08 11:02:30 -0400

    update the schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_20_011304) do
+ActiveRecord::Schema[8.0].define(version: 2026_03_20_011304) do
   create_table "account_requests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
