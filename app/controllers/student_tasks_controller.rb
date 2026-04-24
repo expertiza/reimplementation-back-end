cd97a31d7f341f22474336d2fb0b8732c4c27357
Arnav Mejari <arnavmejari@gmail.com>
2026-04-24 19:47:14 -0400

    Add student task quiz and review item classes

--- app/controllers/student_tasks_controller.rb
+++ app/controllers/student_tasks_controller.rb
@@ -119,4 +119,102 @@ class StudentTasksController < ApplicationController
     # Build and return the task queue for this participant.
     TaskOrdering::TaskQueue.new(participant.assignment, team_participant)
   end
+
+  class BaseTaskItem
+    attr_reader :assignment, :team_participant, :review_map
+
+    def initialize(assignment:, team_participant:, review_map: nil)
+      @assignment = assignment
+      @team_participant = team_participant
+      @review_map = review_map
+    end
+
+    def participant
+      team_participant.participant
+    end
+
+    def response_map
+      raise NotImplementedError
+    end
+
+    def ensure_response_map!
+      response_map
+    end
+
+    def ensure_response!
+      map = response_map
+      return if map.nil?
+
+      Response.find_or_create_by!(
+        map_id: map.id,
+        round: 1
+      ) do |response|
+        response.is_submitted = false
+      end
+    end
+
+    def completed?
+      map = response_map
+      return false if map.nil?
+
+      Response.where(map_id: map.id, is_submitted: true).exists?
+    end
+
+    def to_h
+      map = response_map
+      {
+        task_type: task_type,
+        assignment_id: assignment.id,
+        response_map_id: map&.id,
+        response_map_type: map&.type,
+        reviewee_id: map&.reviewee_id,
+        team_participant_id: team_participant.id
+      }
+    end
+
+    def to_task_hash
+      to_h
+    end
+  end
+
+  class QuizTaskItem < BaseTaskItem
+    def task_type
+      :quiz
+    end
+
+    def questionnaire
+      assignment.quiz_questionnaire_for_review_flow
+    end
+
+    def response_map
+      return @response_map if @response_map
+
+      existing_map = QuizResponseMap.find_by(
+        reviewer_id: team_participant.participant_id,
+        reviewee_id: review_map&.reviewee_id || 0
+      )
+      return @response_map = existing_map if existing_map
+
+      return nil if questionnaire.nil?
+
+      attributes = {
+        reviewer_id: team_participant.participant_id,
+        reviewee_id: review_map&.reviewee_id || 0,
+        reviewed_object_id: questionnaire.id,
+        type: "QuizResponseMap"
+      }
+
+      @response_map = QuizResponseMap.new(attributes).tap { |map| map.save!(validate: false) }
+    end
+  end
+
+  class ReviewTaskItem < BaseTaskItem
+    def task_type
+      :review
+    end
+
+    def response_map
+      review_map
+    end
+  end
 end
